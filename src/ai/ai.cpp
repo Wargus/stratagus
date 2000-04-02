e7e4b639cf2ec93ec626fd3659ee3800c67957bd
johns <>
2000-04-02 00:32:18 +0000

    *** empty log message ***

--- src/ai/ai.cpp
+++ src/ai/ai.cpp
@@ -16,4 +16,117 @@
 */
 
 #ifdef NEW_AI	// {
+
+/*----------------------------------------------------------------------------
+--	Includes
+----------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "clone.h"
+
+#include "player.h"
+#include "unit.h"
+
+#include "ai_local.h"
+
+/*----------------------------------------------------------------------------
+--	Variables
+----------------------------------------------------------------------------*/
+
+global int AiSleep;			/// Ai sleeps # frames
+global int AiTimeFactor = 100;		/// Adjust the AI build times
+global int AiCostFactor = 100;		/// Adjust the AI costs
+
+global AiType* AiTypes;			/// List of all AI types.
+global AiHelper AiHelpers;		/// AI helper variables
+
+/*----------------------------------------------------------------------------
+--	Functions
+----------------------------------------------------------------------------*/
+
+/**
+**      Setup all at start.
+**
+**      @param player   The player structure pointer.
+*/
+global void AiInit(Player* player)
+{
+}
+
+/*----------------------------------------------------------------------------
+--	Callback Functions
+----------------------------------------------------------------------------*/
+
+/**
+**	Called if a Unit is Attacked
+**
+**	@param unit	Pointer to unit that is being attacked.
+**
+*/
+global void AiHelpMe(Unit* unit)
+{
+    DebugLevel0(__FUNCTION__": %d %d",unit->X,unit->Y);
+}
+
+/**
+**	Called if work complete (Buildings).
+**
+**	@param unit	Pointer to unit what builds the building.
+**	@param what	Pointer to unit building that was build.
+*/
+global void AiWorkComplete(Unit* unit,Unit* what)
+{
+}
+
+/**
+**	Called if building can't be build.
+**
+**	@param unit	Pointer to unit what builds the building.
+**	@param what	Pointer to unit-type.
+*/
+global void AiCanNotBuild(Unit* unit,const UnitType* what)
+{
+}
+
+/**
+**	Called if building place can't be reached.
+**
+**	@param unit	Pointer to unit what builds the building.
+**	@param what	Pointer to unit-type.
+*/
+global void AiCanNotReach(Unit* unit,const UnitType* what)
+{
+}
+
+/**
+**	Called if training of an unit is completed.
+**
+**	@param unit	Pointer to unit.
+**	@param what	Pointer to type.
+*/
+global void AiTrainingComplete(Unit* unit,Unit* what)
+{
+}
+
+/**
+**	This is called for each player, each frame.
+**
+**	@param player	The player structure pointer.
+*/
+global void AiEachFrame(Player* player)
+{
+}
+
+/**
+**	This called for each player, each second.
+**
+**	@param player	The player structure pointer.
+*/
+global void AiEachSecond(Player* player)
+{
+    // FIXME: Not needed, will be removed.
+}
+
 #endif // } NEW_AI
