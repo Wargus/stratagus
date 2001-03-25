c60fbd8791c28bfcb7a30e8e7e7b953894d8ee8c
johns <>
2001-03-25 22:04:42 +0000

    NEW_ORDERS are now only supported, old code removed.

--- action/action_patrol.cpp
+++ action/action_patrol.cpp
@@ -47,17 +47,12 @@ global void HandleActionPatrol(Unit* unit)
 {
     const Unit* goal;
 
-#ifdef NEW_ORDERS
     if( !unit->SubAction ) {		// first entry.
 	NewResetPath(unit);
 	unit->SubAction=1;
     }
-#endif
 
     if( DoActionMove(unit)<0 ) {	// reached end-point or stop
-
-#ifdef NEW_ORDERS
-	//Order order;
 	int tmp;
 
 	unit->Orders[0].Action=UnitActionPatrol;
@@ -65,28 +60,12 @@ global void HandleActionPatrol(Unit* unit)
 	//
 	//	Swap the points.
 	//
-	//order=unit->Orders[0];
 	tmp=(int)unit->Orders[0].Arg1;
 	unit->Orders[0].Arg1=(void*)((unit->Orders[0].X<<16)|unit->Orders[0].Y);
 	unit->Orders[0].X=tmp>>16;
 	unit->Orders[0].Y=tmp&0xFFFF;
-	ResetPath(unit->Orders[0]);
 
 	NewResetPath(unit);
-#else
-	unit->Command.Action=UnitActionPatrol;
-
-	//	Swap the points:
-	unit->Command.Data.Move.SX^=unit->Command.Data.Move.DX;
-	unit->Command.Data.Move.DX^=unit->Command.Data.Move.SX;
-	unit->Command.Data.Move.SX^=unit->Command.Data.Move.DX;
-
-	unit->Command.Data.Move.SY^=unit->Command.Data.Move.DY;
-	unit->Command.Data.Move.DY^=unit->Command.Data.Move.SY;
-	unit->Command.Data.Move.SY^=unit->Command.Data.Move.DY;
-
-	ResetPath(unit->Command);
-#endif
     }
 
     if( unit->Reset ) {
@@ -100,16 +79,10 @@ global void HandleActionPatrol(Unit* unit)
 	    if( goal ) {
 		DebugLevel0("Patrol attack %Zd\n",UnitNumber(goal));
 		// Save current command to come back.
-#ifdef NEW_ORDERS
 		unit->SavedOrder=unit->Orders[0];
 		CommandAttack(unit,goal->X,goal->Y,NULL,FlushCommands);
 		unit->Orders[0].Action=UnitActionStill;
 		unit->SubAction=0;
-#else
-		CommandAttack(unit,goal->X,goal->Y,NULL,FlushCommands);
-		unit->SavedCommand=unit->Command;
-		unit->SavedCommand.Action=UnitActionPatrol;
-#endif
 	    }
 	}
     }
