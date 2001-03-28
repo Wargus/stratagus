36d1f7ae3b7b373342d381c45d63092c117a1f0d
johns <>
2001-03-28 16:36:03 +0000

    Prepared NEW_NAMES.

--- stratagus/trunk/src/stratagus/construct.cpp
+++ stratagus/trunk/src/stratagus/construct.cpp
@@ -34,6 +34,9 @@
 --	Variables
 ----------------------------------------------------------------------------*/
 
+#ifndef laterUSE_CCL
+// FIXME: Make this configurable with CCL and than use only this.
+
 #define DEFAULT	NULL			/// use the first slot as default
 
 /**
@@ -41,10 +44,17 @@
 */
 local Construction Constructions[] = {
 { "construction-0",			// added for bad puds they have 0
+#ifdef NEW_NAMES
+  { "neutral/buildings/land construction site.png",
+  "tilesets/winter/neutral/buildings/land construction site.png",
+  DEFAULT,
+  DEFAULT },
+#else
   { "land construction site (summer,wasteland).png",
   "land construction site (winter).png",
   DEFAULT,
   DEFAULT },
+#endif
 	64,64 },
 { "construction-1",
   { "",
@@ -77,66 +87,138 @@ local Construction Constructions[] = {
   DEFAULT },
 	0,0 },
 { "construction-land",
+#ifdef NEW_NAMES
+  { "neutral/buildings/land construction site.png",
+  "tilesets/winter/neutral/buildings/land construction site.png",
+  DEFAULT,
+  DEFAULT },
+#else
   { "land construction site (summer,wasteland).png",
   "land construction site (winter).png",
   DEFAULT,
   DEFAULT },
+#endif
 	64,64 },
 { "construction-human-shipyard",
+#ifdef NEW_NAMES
+  { "human/buildings/shipyard construction site.png",
+  "tilesets/winter/human/buildings/shipyard construction site.png",
+  DEFAULT,
+  "tilesets/swamp/human/buildings/shipyard construction site.png" },
+#else
   { "human shipyard construction site (summer,wasteland).png",
   "human shipyard construction site (winter).png",
   DEFAULT,
   "human shipyard construction site (swamp).png" },
+#endif
 	96,96 },
 { "construction-orc-shipyard",
+#ifdef NEW_NAMES
+  { "orc/buildings/shipyard construction site.png",
+  "tilesets/winter/orc/buildings/shipyard construction site.png",
+  DEFAULT,
+  "tilesets/swamp/orc/buildings/shipyard construction site.png" },
+#else
   { "orc shipyard construction site (summer,wasteland).png",
   "orc shipyard construction site (winter).png",
   DEFAULT,
   "orc shipyard construction site (swamp).png" },
+#endif
 	96,96 },
 { "construction-human-oil-well",
+#ifdef NEW_NAMES
+  { "tilesets/summer/human/buildings/oil well construction site.png",
+  "tilesets/winter/human/buildings/oil well construction site.png",
+  "tilesets/wasteland/human/buildings/oil well construction site.png",
+  "tilesets/swamp/human/buildings/oil well construction site.png" },
+#else
   { "human oil well construction site (summer).png",
   "human oil well construction site (winter).png",
   "human oil well construction site (wasteland).png",
   "human oil well construction site (swamp).png" },
+#endif
 	96,96 },
 { "construction-orc-oil-well",
-  { "orc oil well construction site (summer).png",
-  "orc oil well construction site (winter).png",
-  "orc oil well construction site (wasteland).png",
-  "orc oil well construction site (swamp).png" },
+#ifdef NEW_NAMES
+  { "tilesets/summer/orc/buildings/oil well construction site.png",
+  "tilesets/winter/orc/buildings/oil well construction site.png",
+  "tilesets/wasteland/orc/buildings/oil well construction site.png",
+  "tilesets/swamp/orc/buildings/oil well construction site.png" },
+#else
+  { "orc/buildings/oil well construction site (summer).png",
+  "orc/buildings/oil well construction site (winter).png",
+  "orc/buildings/oil well construction site (wasteland).png",
+  "orc/buildings/oil well construction site (swamp).png" },
+#endif
 	96,96 },
 { "construction-human-refinery",
+#ifdef NEW_NAMES
+  { "human/buildings/refinery construction site.png",
+  "tilesets/winter/human/buildings/refinery construction site.png",
+  DEFAULT,
+  "tilesets/swamp/human/buildings/refinery construction site.png" },
+#else
   { "human refinery construction site (summer,wasteland).png",
   "human refinery construction site (winter).png",
   DEFAULT,
   "human refinery construction site (swamp).png" },
+#endif
 	96,96 },
 { "construction-orc-refinery",
-  { "orc refinery construction site (summer,wasteland).png",
-  "orc refinery construction site (winter).png",
+#ifdef NEW_NAMES
+  { "orc/buildings/refinery construction site.png",
+  "tilesets/winter/orc/buildings/refinery construction site.png",
   DEFAULT,
-  "orc refinery construction site (swamp).png" },
+  "tilesets/swamp/orc/buildings/refinery construction site.png" },
+#else
+  { "orc/buildings/refinery construction site (summer,wasteland).png",
+  "orc/buildings/refinery construction site (winter).png",
+  DEFAULT,
+  "orc/buildings/refinery construction site (swamp).png" },
+#endif
 	96,96 },
 { "construction-human-foundry",
+#ifdef NEW_NAMES
+  { "human/buildings/foundry construction site.png",
+  "tilesets/winter/human/buildings/foundry construction site.png",
+  DEFAULT,
+  "tilesets/swamp/human/buildings/foundry construction site.png" },
+#else
   { "human foundry construction site (summer,wasteland).png",
   "human foundry construction site (winter).png",
   DEFAULT,
   "human foundry construction site (swamp).png" },
+#endif
 	96,96 },
 { "construction-orc-foundry",
+#ifdef NEW_NAMES
+  { "orc/buildings/foundry construction site.png",
+  "tilesets/winter/orc/buildings/foundry construction site.png",
+  DEFAULT,
+  "tilesets/swamp/orc/buildings/foundry construction site.png" },
+#else
   { "orc foundry construction site (summer,wasteland).png",
   "orc foundry construction site (winter).png",
   DEFAULT,
   "orc foundry construction site (swamp).png" },
+#endif
 	96,96 },
 { "construction-wall",
+#ifdef NEW_NAMES
+  { "tilesets/summer/neutral/buildings/wall construction site.png",
+  "tilesets/winter/neutral/buildings/wall construction site.png",
+  "tilesets/wasteland/neutral/buildings/wall construction site.png",
+  DEFAULT },
+#else
   { "wall construction site (summer).png",
   "wall construction site (winter).png",
   "wall construction site (wasteland).png",
   DEFAULT },
+#endif
 	32,32 },
 };
+
+#endif
 
 /*----------------------------------------------------------------------------
 --	Functions
@@ -162,7 +244,11 @@ global void LoadConstructions(void)
 	    char* buf;
 
 	    buf=alloca(strlen(file)+9+1);
+#ifdef NEW_NAMES
+	    file=strcat(strcpy(buf,"graphics/"),file);
+#else
 	    file=strcat(strcpy(buf,"graphic/"),file);
+#endif
 	    ShowLoadProgress("\tConstruction %s\n",file);
 	    Constructions[i].Sprite=LoadSprite(file
 		    ,Constructions[i].Width,Constructions[i].Height);
