a1fa114a82a57fb95697c409d3c65a3323abf34d
johns <>
2001-03-30 15:15:29 +0000

    MissileType documentation added. Missile speed and range configuration added. Old speed moved to sleep.

--- stratagus/trunk/src/include/missile.h
+++ stratagus/trunk/src/include/missile.h
@@ -20,6 +20,113 @@
 //@{
 
 /*----------------------------------------------------------------------------
+--	Documentation
+----------------------------------------------------------------------------*/
+
+/**
+**	@struct _missile_type_ missile.h
+**
+**	\#include "missile.h"
+**
+**	typedef struct _missile_type_ MissileType;
+**
+**	This structure defines the base type informations of all missiles.
+**	It contains all informations that all missiles of the same type shares.
+**
+**	The missile-type structure members:
+**
+**	MissileType::OType
+**
+**		Object type (future extensions).
+**
+**	MissileType::Ident
+**
+**		Unique identifier of the missile-type, used to reference it in
+**		config files and during startup. Don't use this in game, use
+**		instead the pointer to this structure.
+**
+**	MissileType::File
+**
+**		File containing the image graphics of the missile. The file
+**		can contain multiple frames.
+**		The frames for the different directions are placed in the row.
+**		The different animations steps are placed in the column.
+**
+**	MissileType::Width MissileType::Height
+**
+**		Size (width and height) of a frame in the image. All frames
+**		of the missile-type must have the same size.
+**
+**	MissleType::Frames
+**
+**	@note	This isn't implemented yet.
+**
+**		Number of frames in the graphic image.
+**
+**	MissileType::FiredSound
+**
+**		Sound of the missile, if fired. @note currently not used.
+**
+**	MissileType::ImpactSound
+**
+**		Impact sound for this missile.
+**
+**	MissileType::Class
+**
+**		Class of the missile-type, defines the basic effects of the
+**		missile. Look at the different class identifiers for more
+**		informations (::_missile_class_ ::MissileClassNone ...).
+**
+**	MissileType::Delay
+**
+**	@note	This isn't implemented yet.
+**
+**		Delay after the missile generation, until the missile animation
+**		starts.
+**
+**	MissileType::Sleep
+**
+**		This are the number of frames to wait for the next animation.
+**		This value is for all animations steps the same.  0 is the
+**		fastest and 255 the slowest animation.
+**		@note Perhaps we should later allow animation scripts for
+**		more complex animations.
+**
+**	MissileType::Speed
+**
+**		The speed how fast the missile moves. 0 the missile didn't
+**		move, 1 is the slowest speed and 32 the fastest supported
+**		speed. This is how many pixels the missiles moves with each
+**		animation step.
+**
+**	MissileType::Range
+**
+**	@note	This isn't implemented yet.
+**
+**		Determines the range that a projectile will deal its damage.
+**		A range of 0 will mean that the damage will be limited to only
+**		where the missile was directed towards. So if you shot a
+**		missile at a unit, it would only damage that unit. A value of
+**		1 only effects the field on that the missile is shot. A value
+**		of 2  would mean that for a range of 1 around the impact spot,
+**		the damage for that particular missile would be dealt.
+**
+**	MissileType::ImpactName
+**
+**		The name of the missile-type generated, if the missile
+**		reaches its end point. Can be used to generate chains of
+**		missiles. Used only during configuration and startup.
+**
+**	MissileType::ImpactMissile
+**
+**		Pointer to the impact missile-type. Used during run time.
+**
+**	MissileType::Sprite
+**
+**		Missile sprite loaded from MissileType::File
+*/
+
+/*----------------------------------------------------------------------------
 --	Includes
 ----------------------------------------------------------------------------*/
 
@@ -34,11 +141,11 @@
 ----------------------------------------------------------------------------*/
 
 /*----------------------------------------------------------------------------
---	Missile - type
+--	Missile-type
 ----------------------------------------------------------------------------*/
 
 /**
-**	Missile type typedef.
+**	Missile-type typedef.
 */
 typedef struct _missile_type_ MissileType;
 
@@ -59,8 +166,10 @@ struct _missile_type_ {
     SoundConfig	ImpactSound;		/// impact sound for this missile-type
 
     int		Class;			/// missile class
+    int		Sleep;			/// missile sleep
     int		Speed;			/// missile speed
 
+    int		Range;			/// missile damage range
     char*	ImpactName;		/// Impact missile-type name
     MissileType*ImpactMissile;		/// Missile produces an impact
 
@@ -126,12 +235,12 @@ extern const char* MissileClassNames[];		/// Missile class names
 --	Functions
 ----------------------------------------------------------------------------*/
 
-// 	In ccl_missile.c
+//	In ccl_missile.c
 
     /// register ccl features
 extern void MissileCclRegister(void);
 
-// 	In missile.c
+//	In missile.c
 
     /// load the graphics for the missiles
 extern void LoadMissileSprites(void);
