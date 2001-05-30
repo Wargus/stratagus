e9ac06d947870fb5db43738f8fe12897ad0d913f
johns <>
2001-05-30 18:45:57 +0000

    License changed to GPL.

--- tools/gfx2png.c
+++ tools/gfx2png.c
@@ -14,6 +14,16 @@
 //
 //	(c) Copyright 1998,2000 by Lutz Sammer
 //
+//	FreeCraft is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published
+//	by the Free Software Foundation; either version 2 of the License,
+//	or (at your option) any later version.
+//
+//	FreeCraft is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
 //	$Id$
 
 #include <stdio.h>
