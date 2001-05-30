d0706998facd6666063216ba7969768767e7c8f2
johns <>
2001-05-30 19:45:57 +0000

    License changed to GPL.

--- stratagus/trunk/tools/aledoc.c
+++ stratagus/trunk/tools/aledoc.c
@@ -1,5 +1,15 @@
 //
-//	(c) Copyright Lutz Sammer
+//	(c) Copyright 1999-2001 Lutz Sammer
+//
+//	FreeCraft is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published
+//	by the Free Software Foundation; either version 2 of the License,
+//	or (at your option) any later version.
+//
+//	FreeCraft is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
 //	
 //	This programm improves doc++, for better documenting
 //
