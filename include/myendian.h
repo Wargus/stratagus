f40ab7c61424d41999fb021a1d445a9cb3999535
johns <>
2001-05-31 15:33:25 +0000

    Changed license to GPL.

--- include/myendian.h
+++ include/myendian.h
@@ -9,11 +9,20 @@
 //	   FreeCraft - A free fantasy real time strategy game engine
 //
 /**@name myendian.h	-	The endian-specific headerfile. */
-/*
-**	(c) Copyright 2000 by Lutz Sammer
-**
-**	$Id$
-*/
+//
+//	(c) Copyright 2000,2001 by Lutz Sammer
+//
+//	FreeCraft is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published
+//	by the Free Software Foundation; either version 2 of the License,
+//	or (at your option) any later version.
+//
+//	FreeCraft is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	$Id$
 
 #ifndef __ENDIAN_H__
 #define __ENDIAN_H__
--- include/sound_server.h
+++ include/sound_server.h
@@ -9,11 +9,20 @@
 //	   FreeCraft - A free fantasy real time strategy game engine
 //
 /**@name sound_server.h	-	The sound server header file. */
-/*
-**	(c) Copyright 1998-2000 by Lutz Sammer and Fabrice Rossi
-**
-**	$Id$
-*/
+//
+//	(c) Copyright 1998-2001 by Lutz Sammer and Fabrice Rossi
+//
+//	FreeCraft is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published
+//	by the Free Software Foundation; either version 2 of the License,
+//	or (at your option) any later version.
+//
+//	FreeCraft is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	$Id$
 
 #ifndef __SOUND_SERVER_H__
 #define __SOUND_SERVER_H__
