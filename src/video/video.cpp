e603a2f842dfb8b91788fce423f12a349933edf1
johns <>
2001-06-01 14:54:11 +0000

    Changed license to GPL.

--- src/video/video.cpp
+++ src/video/video.cpp
@@ -12,6 +12,16 @@
 //
 //	(c) Copyright 1999-2001 by Lutz Sammer
 //
+//	FreeCraft is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published
+//	by the Free Software Foundation; either version 2 of the License,
+//	or (at your option) any later version.
+//
+//	FreeCraft is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
 //	$Id$
 
 //@{
