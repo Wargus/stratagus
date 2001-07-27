ad328976e7febe28f82b491371012066b44dbf09
johns <>
2001-07-27 22:37:35 +0000

    sys includes not for MSC.

--- include/etlib/xmalloc.h
+++ include/etlib/xmalloc.h
@@ -1,6 +1,8 @@
 #ifndef ETLIB_XMALLOC_H
 #define ETLIB_XMALLOC_H
+#ifndef _MSC_VER
 #include <sys/types.h>
+#endif
 
 extern void *xmalloc(size_t size);
 extern void *xcalloc(size_t nelem, size_t elem_size);
