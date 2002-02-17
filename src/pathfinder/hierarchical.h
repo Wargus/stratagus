95e9e63a3c7f49dc1f0391f70a4b5446358664d5
johns <>
2002-02-17 22:34:06 +0000

    Doxygen highlites names like coordinates if used as structures. Also added header.

--- src/pathfinder/hierarchical.h
+++ src/pathfinder/hierarchical.h
@@ -1,21 +1,43 @@
+//   ___________		     _________		      _____  __
+//   \_	  _____/______   ____   ____ \_   ___ \____________ _/ ____\/  |_
+//    |    __) \_  __ \_/ __ \_/ __ \/    \  \/\_  __ \__  \\   __\\   __\ 
+//    |     \   |  | \/\  ___/\  ___/\     \____|  | \// __ \|  |   |  |
+//    \___  /   |__|    \___  >\___  >\______  /|__|  (____  /__|   |__|
+//	  \/		    \/	   \/	     \/		   \/
+//  ______________________                           ______________________
+//			  T H E   W A R   B E G I N S
+//	   FreeCraft - A free fantasy real time strategy game engine
+//
+/**@name hierarchical.h	-	The hierarchical pathfinder header file. */
+//
+//      (c) Copyright 2002 by Latimerius.
+//
+//	FreeCraft is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published
+//	by the Free Software Foundation; only version 2 of the License.
+//
+//	FreeCraft is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	 $Id$
 
-/* $Id$ */
+#ifndef __HIERARCHICAL_H__
+#define __HIERARCHICAL_H__
 
-#ifndef HIERARCHICAL_H
-#define HIERARCHICAL_H
-
-typedef enum node_operation {
+typedef enum _node_operation_ {
     SET_REGID
 } NodeOperation;
 
-struct coordinates {
+struct _coordinates_ {
 	short X, Y;
 };
 
-typedef struct coordinates AreaCoords;
-typedef struct coordinates FieldCoords;
+typedef struct _coordinates_ AreaCoords;
+typedef struct _coordinates_ FieldCoords;
 
-typedef enum area_neighborship_type {
+typedef enum _area_neighborship_type_ {
 	AREAS_NONCONNECTED,
 	AREAS_8CONNECTED,
 	AREAS_4CONNECTED
@@ -36,4 +58,4 @@ extern int PfHierComputePath (Unit * , int * , int * );
 
 #endif /* HIERARCHICAL_BACKEND */
 
-#endif /* HIERARCHICAL_H */
+#endif	// __HIERARCHICAL_H__
