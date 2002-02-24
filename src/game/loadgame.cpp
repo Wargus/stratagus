b2ea566e2101ae80acdbbf7e7ac7225971ae22f6
johns <>
2002-02-24 22:34:37 +0000

    Reset SynRandSeed and call to update stats added.

--- src/game/loadgame.cpp
+++ src/game/loadgame.cpp
@@ -10,12 +10,11 @@
 //
 /**@name loadgame.c	-	Load game. */
 //
-//	(c) Copyright 2001 by Lutz Sammer, Andreas Arens
+//	(c) Copyright 2001,2002 by Lutz Sammer, Andreas Arens
 //
 //	FreeCraft is free software; you can redistribute it and/or modify
 //	it under the terms of the GNU General Public License as published
-//	by the Free Software Foundation; either version 2 of the License,
-//	or (at your option) any later version.
+//	by the Free Software Foundation; only version 2 of the License.
 //
 //	FreeCraft is distributed in the hope that it will be useful,
 //	but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -86,6 +85,7 @@ global void CleanModules(void)
 */
 global void InitModules(void)
 {
+    SyncRandSeed=0x87654321;
     InitIcons();
     InitCursors();
     InitUserInterface(RaceWcNames ? RaceWcNames[1] : "oops");
@@ -94,6 +94,7 @@ global void InitModules(void)
     InitMissiles();
     InitConstructions();
     InitUnitTypes();
+    UpdateStats();
     InitUnits();
     InitSelections();
     InitGroups();
