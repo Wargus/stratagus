a3b16196c79abd6fbea91cd66f2ce1b6a8051f2a
johns <>
2002-02-24 01:28:08 +0000

    Added new function StopMusic and (stop-music).

--- stratagus/trunk/src/include/sound.h
+++ stratagus/trunk/src/include/sound.h
@@ -10,12 +10,11 @@
 //
 /**@name sound.h	-	The sound header file. */
 //
-//	(c) Copyright 1998-2001 by Lutz Sammer and Fabrice Rossi
+//	(c) Copyright 1998-2002 by Lutz Sammer and Fabrice Rossi
 //
 //	FreeCraft is free software; you can redistribute it and/or modify
 //	it under the terms of the GNU General Public License as published
-//	by the Free Software Foundation; either version 2 of the License,
-//	or (at your option) any later version.
+//	by the Free Software Foundation; only version 2 of the License.
 //
 //	FreeCraft is distributed in the hope that it will be useful,
 //	but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -119,6 +118,8 @@ extern void InitSoundClient(void);
 
     /// Play a music file
 extern void PlayMusic(const char* name);
+    /// Stop music playing
+extern void StopMusic(void);
 
 #else	// }{ WITH_SOUND
 
@@ -136,6 +137,7 @@ extern void PlayMusic(const char* name);
 #define InitSoundClient()		/// Dummy macro for without sound
 
 #define PlayMusic(v)			/// Dummy macro for without sound
+#define StopMusic(v)			/// Dummy macro for without sound
 
 #endif	// } WITH_SOUND
 
