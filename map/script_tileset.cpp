d01bf8e20e1abec6bb6681b7ad149f4dd8f3e1a1
johns <>
2002-02-25 01:07:09 +0000

    Fixed memory corruption.

--- map/script_tileset.cpp
+++ map/script_tileset.cpp
@@ -14,8 +14,7 @@
 //
 //	FreeCraft is free software; you can redistribute it and/or modify
 //	it under the terms of the GNU General Public License as published
-//	by the Free Software Foundation; either version 2 of the License,
-//	or (at your option) any later version.
+//	by the Free Software Foundation; only version 2 of the License.
 //
 //	FreeCraft is distributed in the hope that it will be useful,
 //	but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -396,7 +395,7 @@ local SCM CclDefineTileset(SCM list)
 	fprintf(stderr,"out of memory.\n");
 	exit(-1);
     }
-    Tilesets[type]=tileset=malloc(sizeof(Tileset));
+    Tilesets[type]=tileset=calloc(sizeof(Tileset),1);
     if( !tileset ) {
 	fprintf(stderr,"out of memory.\n");
 	exit(-1);
