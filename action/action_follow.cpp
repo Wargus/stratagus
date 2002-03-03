0f36b4eb3a388da34c947fb917183dc043027b82
johns <>
2002-03-03 21:07:18 +0000

    Fixed follow of transporter.

--- action/action_follow.cpp
+++ action/action_follow.cpp
@@ -10,12 +10,11 @@
 //
 /**@name action_follow.c -	The follow action. */
 //
-//	(c) Copyright 2001 by Lutz Sammer
+//	(c) Copyright 2001-2002 by Lutz Sammer
 //
 //	FreeCraft is free software; you can redistribute it and/or modify
 //	it under the terms of the GNU General Public License as published
-//	by the Free Software Foundation; either version 2 of the License,
-//	or (at your option) any later version.
+//	by the Free Software Foundation; only version 2 of the License.
 //
 //	FreeCraft is distributed in the hope that it will be useful,
 //	but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -157,6 +156,22 @@ global void HandleActionFollow(Unit* unit)
 	if( !--goal->Refs ) {
 	    ReleaseUnit(goal);
 	}
+	goal=NoUnitP;
+	NewResetPath(unit);
+    }
+    //
+    //	Target removed?
+    //
+    if( unit->Type->Transporter && goal && goal->Removed ) {
+	DebugLevel0Fn("Goal removed\n");
+	unit->Orders[0].X=goal->X+goal->Type->TileWidth/2;
+	unit->Orders[0].Y=goal->Y+goal->Type->TileHeight/2;
+	unit->Orders[0].Goal=NoUnitP;
+	RefsDebugCheck( !goal->Refs );
+	if( !--goal->Refs ) {
+	    ReleaseUnit(goal);
+	}
+	goal=NoUnitP;
 	NewResetPath(unit);
     }
 
