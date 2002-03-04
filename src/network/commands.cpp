4c7e32163635b54a06c9df2ded34ab0907908bf3
johns <>
2002-03-04 09:55:48 +0000

    Fixed compiler warnings.

--- src/network/commands.cpp
+++ src/network/commands.cpp
@@ -10,12 +10,11 @@
 //
 /**@name commands.c	-	Global command handler - network support. */
 //
-//	(c) Copyright 2000,2001 by Lutz Sammer and Andreas Arens.
+//	(c) Copyright 2000,2002 by Lutz Sammer and Andreas Arens.
 //
 //	FreeCraft is free software; you can redistribute it and/or modify
 //	it under the terms of the GNU General Public License as published
-//	by the Free Software Foundation; either version 2 of the License,
-//	or (at your option) any later version.
+//	by the Free Software Foundation; only version 2 of the License.
 //
 //	FreeCraft is distributed in the hope that it will be useful,
 //	but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -78,7 +77,6 @@ local void CommandLog(const char* name,const Unit* unit,int flag,
 	unsigned x,unsigned y,const Unit* dest,const char* value,int num)
 {
     static FILE* logf;
-    extern unsigned SyncRandSeed;
 
     if( !CommandLogEnabled ) {
 	return;
