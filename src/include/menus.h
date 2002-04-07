a26bdf170cc1a9fb27cccc3bc8efb30648d71688
nehalmistry <>
2002-04-07 05:12:36 +0000

    Give an error message if freecraft compiled without CD Audio and user tries to enable it through options.

--- src/include/menus.h
+++ src/include/menus.h
@@ -225,7 +225,8 @@ typedef struct _menus_ {
 #define MENU_OBJECTIVES 14
 #define MENU_END_SCENARIO 15
 #define MENU_GAME_OPTIONS 16
-#define MENU_MAX 16			/// highest available menu id (for ccl)
+#define MENU_CDROM_DISABLED 17
+#define MENU_MAX 17			/// highest available menu id (for ccl)
 
 /// FIXME: FILL IN THIS TABLE!!!!
 
