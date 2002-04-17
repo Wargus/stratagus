c940ed1bd214c654f5501b75595fe586c1ce42bb
johns <>
2002-04-17 13:24:03 +0000

    Fixed compile without hierachic pathfinder.

--- src/pathfinder/script_pathfinder.cpp
+++ src/pathfinder/script_pathfinder.cpp
@@ -10,12 +10,11 @@
 //
 /**@name ccl_pathfinder.c	-	pathfinder ccl functions. */
 //
-//	(c) Copyright 2000,2001 by Lutz Sammer and Fabrice Rossi
+//	(c) Copyright 2000-2002 by Lutz Sammer and Fabrice Rossi
 //
 //	FreeCraft is free software; you can redistribute it and/or modify
 //	it under the terms of the GNU General Public License as published
-//	by the Free Software Foundation; either version 2 of the License,
-//	or (at your option) any later version.
+//	by the Free Software Foundation; only version 2 of the License.
 //
 //	FreeCraft is distributed in the hope that it will be useful,
 //	but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -115,13 +114,17 @@ local SCM CclAStarSetMovingUCC(SCM cost)
 
 local SCM CclPfHierShowRegIds (SCM flag)
 {
+#ifdef HIERARCHIC_PATHFINDER
     PfHierShowRegIds = gh_scm2bool (flag);
+#endif
     return SCM_UNSPECIFIED;
 }
 
 local SCM CclPfHierShowGroupIds (SCM flag)
 {
+#ifdef HIERARCHIC_PATHFINDER
     PfHierShowGroupIds = gh_scm2bool (flag);
+#endif
     return SCM_UNSPECIFIED;
 }
 
