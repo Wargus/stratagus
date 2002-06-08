1c5ca32eaf25470fe1c3f6d72170a593032ecf88
johns <>
2002-06-08 20:47:13 +0000

    Added editloop.c

---
+++ stratagus/trunk/src/editor/editloop.cpp
@@ -0,0 +1,167 @@
+//   ___________		     _________		      _____  __
+//   \_	  _____/______   ____   ____ \_   ___ \____________ _/ ____\/  |_
+//    |    __) \_  __ \_/ __ \_/ __ \/    \  \/\_  __ \__  \\   __\\   __\ 
+//    |     \   |  | \/\  ___/\  ___/\     \____|  | \// __ \|  |   |  |
+//    \___  /   |__|    \___  >\___  >\______  /|__|  (____  /__|   |__|
+//	  \/		    \/	   \/	     \/		   \/
+//  ______________________                           ______________________
+//			  T H E   W A R   B E G I N S
+//	   FreeCraft - A free fantasy real time strategy game engine
+//
+/**@name editloop.c	-	The editor main loop. */
+//
+//	(c) Copyright 2002 by Lutz Sammer
+//
+//	FreeCraft is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published
+//	by the Free Software Foundation; only version 2 of the License.
+//
+//	FreeCraft is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	$Id$
+
+//@{
+
+/*----------------------------------------------------------------------------
+--	Includes
+----------------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+#include "freecraft.h"
+#include "video.h"
+#include "map.h"
+#include "settings.h"
+#include "network.h"
+#include "sound_server.h"
+#include "ui.h"
+#include "interface.h"
+#include "editor.h"
+
+/*----------------------------------------------------------------------------
+--	Variables
+----------------------------------------------------------------------------*/
+
+local char EditorRunning;		/// True editor is running
+
+/*----------------------------------------------------------------------------
+--	Functions
+----------------------------------------------------------------------------*/
+
+/**
+**	Update editor display.
+*/
+local void EditorUpdateDisplay(void)
+{
+    VideoLockScreen();			// { prepare video write
+
+    HideAnyCursor();			// remove cursor (when available)
+
+    DrawMapArea();			// draw the map area
+
+    DrawAnyCursor();
+
+    VideoUnlockScreen();		// } end write access
+
+    // FIXME: For now update everything each frame
+
+    // refresh entire screen, so no further invalidate needed
+    InvalidateAreaAndCheckCursor(0,0,VideoWidth,VideoHeight);
+}
+
+/**
+**	Callback for input.
+*/
+local void EditorCallbackKey(unsigned dummy __attribute__((unused)))
+{
+    DebugLevel3Fn("Pressed %8x %8x\n" _C_ MouseButtons _C_ dummy);
+    EditorRunning=0;
+}
+
+/**
+**	Callback for input.
+*/
+local void EditorCallbackKey2(unsigned dummy1 __attribute__((unused)),
+	unsigned dummy2 __attribute__((unused)))
+{
+    DebugLevel3Fn("Pressed %8x %8x %8x\n" _C_ MouseButtons _C_ dummy1 _C_
+	    dummy2);
+}
+
+/**
+**	Callback for input.
+*/
+local void EditorCallbackKey3(unsigned dummy1 __attribute__((unused)),
+	unsigned dummy2 __attribute__((unused)))
+{
+    DebugLevel3Fn("Repeated %8x %8x %8x\n" _C_ MouseButtons _C_ dummy1 _C_
+	    dummy2);
+}
+
+/**
+**	Callback for input movement of the cursor.
+**
+**	@param x	Screen X position.
+**	@param y	Screen Y position.
+*/
+local void EditorCallbackMouse(int x, int y)
+{
+    DebugLevel2Fn("Moved %d,%d\n" _C_ x _C_ y);
+
+    HandleCursorMove(&x,&y);
+}
+
+/**
+**	Callback for exit.
+*/
+local void EditorCallbackExit(void)
+{
+    DebugLevel3Fn("Exit\n");
+}
+
+/**
+**	Create editor.
+*/
+local void CreateEditor(void)
+{
+    FlagRevealMap=1;
+    CreateGame("default.pud",&TheMap);
+}
+
+/**
+**	Editor main event loop.
+*/
+global void EditorMainLoop(void)
+{
+    EventCallback callbacks;
+
+    CreateEditor();
+
+    SetVideoSync();
+
+    callbacks.ButtonPressed = EditorCallbackKey;
+    callbacks.ButtonReleased = EditorCallbackKey;
+    callbacks.MouseMoved = EditorCallbackMouse;
+    callbacks.MouseExit = EditorCallbackExit;
+    callbacks.KeyPressed = EditorCallbackKey2;
+    callbacks.KeyReleased = EditorCallbackKey2;
+    callbacks.KeyRepeated = EditorCallbackKey3;
+
+    callbacks.NetworkEvent = NetworkEvent;
+    callbacks.SoundReady = WriteSound;
+
+    GameCursor = TheUI.Point.Cursor;
+    InterfaceState = IfaceStateNormal;
+
+    EditorRunning = 1;
+    while (EditorRunning) {
+	EditorUpdateDisplay();
+
+	WaitEventsOneFrame(&callbacks);
+    }
+}
+
+//@}
