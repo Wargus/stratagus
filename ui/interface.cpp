be227ff8e144047b6903f8bf52bb6316af0da406
johns <>
2002-06-12 00:20:20 +0000

    Shouldn't be deleted.

--- ui/interface.cpp
+++ ui/interface.cpp
@@ -871,6 +871,13 @@ local int CommandKey(int key)
 	    break;
 
 #ifdef SPLIT_SCREEN_SUPPORT
+	case 'v':			// ALT+v CTRL+V: Viewport
+	    if (KeyModifiers & ModifierControl) {
+		CycleViewportMode(-1);
+	    } else {
+		CycleViewportMode(1);
+	    }
+	    break;
 #endif /* SPLIT_SCREEN_SUPPORT */
 
 	case KeyCodeUp:
