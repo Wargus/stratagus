0f3a62efe66b71e4019522330bed1681b154c843
johns <>
2002-06-15 22:44:58 +0000

    Empty file for the editor ccl functions.

---
+++ stratagus/trunk/src/editor/script_editor.cpp
@@ -0,0 +1,58 @@
+//   ___________		     _________		      _____  __
+//   \_	  _____/______   ____   ____ \_   ___ \____________ _/ ____\/  |_
+//    |    __) \_  __ \_/ __ \_/ __ \/    \  \/\_  __ \__  \\   __\\   __\ 
+//    |     \   |  | \/\  ___/\  ___/\     \____|  | \// __ \|  |   |  |
+//    \___  /   |__|    \___  >\___  >\______  /|__|  (____  /__|   |__|
+//	  \/		    \/	   \/	     \/		   \/
+//  ______________________                           ______________________
+//			  T H E   W A R   B E G I N S
+//	   FreeCraft - A free fantasy real time strategy game engine
+//
+/**@name ccl_editor.c	-	Editor CCL functions. */
+//
+//	(c) Copyright 2002 by Lutz Sammer
+//
+//	FreeCraft is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published
+//	by the Free Software Foundation; only version 2 of the License.
+//
+//	FreeCraft is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	$Id$
+
+//@{
+
+/*----------------------------------------------------------------------------
+--	Includes
+----------------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+#include "freecraft.h"
+
+/*----------------------------------------------------------------------------
+--	Defines
+----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+--	Variables
+----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+--	Functions
+----------------------------------------------------------------------------*/
+
+/**
+**	Register CCL features for the editor.
+*/
+global void EditorCclRegister(void)
+{
+    //gh_new_procedureN("player",CclPlayer);
+    //gh_new_procedure0_0("get-this-player",CclGetThisPlayer);
+    //gh_new_procedure1_0("set-this-player!",CclSetThisPlayer);
+}
+
+//@}
