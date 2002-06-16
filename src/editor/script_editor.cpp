0a02285c71d57ffe6e13d0fbeb897aa16566306b
johns <>
2002-06-16 23:37:06 +0000

    Better sorted unit list for editor.

--- src/editor/script_editor.cpp
+++ src/editor/script_editor.cpp
@@ -30,8 +30,11 @@
 ----------------------------------------------------------------------------*/
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "freecraft.h"
+#include "editor.h"
+#include "ccl.h"
 
 /*----------------------------------------------------------------------------
 --	Defines
@@ -46,6 +49,38 @@
 ----------------------------------------------------------------------------*/
 
 /**
+**	Define an editor unit-type list.
+**
+**	@param list	List of all names.
+*/
+local SCM CclDefineEditorUnitTypes(SCM list)
+{
+    int i;
+    char** cp;
+
+    if( (cp=EditorUnitTypes) ) {		// Free all old names
+	while( *cp ) {
+	    free(*cp++);
+	}
+	free(EditorUnitTypes);
+    }
+
+    //
+    //	Get new table.
+    //
+    i=gh_length(list);
+    EditorUnitTypes=cp=malloc((i+1)*sizeof(char*));
+    while( i-- ) {
+	*cp++=gh_scm2newstr(gh_car(list),NULL);
+	list=gh_cdr(list);
+    }
+    *cp=NULL;
+
+    return SCM_UNSPECIFIED;
+}
+
+
+/**
 **	Register CCL features for the editor.
 */
 global void EditorCclRegister(void)
@@ -53,6 +88,7 @@ global void EditorCclRegister(void)
     //gh_new_procedureN("player",CclPlayer);
     //gh_new_procedure0_0("get-this-player",CclGetThisPlayer);
     //gh_new_procedure1_0("set-this-player!",CclSetThisPlayer);
+    gh_new_procedureN("define-editor-unittypes",CclDefineEditorUnitTypes);
 }
 
 //@}
