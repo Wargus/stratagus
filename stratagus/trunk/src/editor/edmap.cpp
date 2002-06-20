1209f21130258f554a03534fdcf91f95da7b0518
jsalmon3 <>
2002-06-20 22:02:49 +0000

    Fixed VC++ warnings

--- stratagus/trunk/src/editor/edmap.cpp
+++ stratagus/trunk/src/editor/edmap.cpp
@@ -30,6 +30,8 @@
 ----------------------------------------------------------------------------*/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 
 #include "freecraft.h"
