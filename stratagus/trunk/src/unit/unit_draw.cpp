4bac43c29213f8665962ee0cd33a6e5e44919e78
johns <>
2002-07-21 21:00:42 +0000

    Fixed configurable max mana.

--- stratagus/trunk/src/unit/unit_draw.cpp
+++ stratagus/trunk/src/unit/unit_draw.cpp
@@ -1006,9 +1006,9 @@ local void DrawDecoration(const Unit* unit,const UnitType* type,int x,int y)
     //	Mana bar on right side of unit. FIXME: combine bar and sprite
     //
     if( ShowManaBar ) {
-	    /* s0m3body: mana bar should display man proportionally to unit's max mana (unit->Type->Magic) */
-	if( type->CanCastSpell && !(ShowNoFull && unit->Mana==unit->Type->Magic) ) {
-	    DrawManaBar(x,y,type,unit->Type->Magic,unit->Mana);
+	    /* s0m3body: mana bar should display man proportionally to unit's max mana (unit->Type->_MaxMana) */
+	if( type->CanCastSpell && !(ShowNoFull && unit->Mana==unit->Type->_MaxMana) ) {
+	    DrawManaBar(x,y,type,unit->Type->_MaxMana,unit->Mana);
 	} else if( type->GivesOil || type->GoldMine || type->OilPatch ) {
 	    DrawManaBar(x,y,type,655350,unit->Value);
 	}
@@ -1070,9 +1070,9 @@ local void DrawDecoration(const Unit* unit,const UnitType* type,int x,int y)
     //	Mana dot on right side of unit.
     //
     if( ShowManaDot ) {
-	    /* s0m3body: MaxMana can vary for each unit, it is stored in unit->Type->Magic */	
-	if( type->CanCastSpell && !(ShowNoFull && unit->Mana==unit->Type->Magic) ) {
-	    DrawManaSprite(x,y,type,unit->Type->Magic,unit->Mana);
+	    /* s0m3body: MaxMana can vary for each unit, it is stored in unit->Type->_MaxMana */	
+	if( type->CanCastSpell && !(ShowNoFull && unit->Mana==unit->Type->_MaxMana) ) {
+	    DrawManaSprite(x,y,type,unit->Type->_MaxMana,unit->Mana);
 	} else if( type->GivesOil || type->GoldMine || type->OilPatch ) {
 	    DrawManaSprite(x,y,type,655350,unit->Value);
 	}
