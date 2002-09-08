74365c27f4d8873db91721a3ca97d572e7348a2b
jsalmon3 <>
2002-09-08 22:40:17 +0000

    Check for getenv("HOME") == NULL

--- stratagus/trunk/src/stratagus/iolib.cpp
+++ stratagus/trunk/src/stratagus/iolib.cpp
@@ -420,32 +420,34 @@ global char* LibraryFileName(const char* file,char* buffer)
 #endif	// USE_ZZIPLIB
     }
 
-    //
-    //	In user home directory
-    //
-    sprintf(buffer,"%s/%s/%s",getenv("HOME"),FREECRAFT_HOME_PATH,file);
-    if( !access(buffer,R_OK) ) {
-	return buffer;
-    }
+    if( (s = getenv("HOME")) ) {
+	//
+	//	In user home directory
+	//
+	sprintf(buffer,"%s/%s/%s",s,FREECRAFT_HOME_PATH,file);
+	if( !access(buffer,R_OK) ) {
+	    return buffer;
+	}
 #ifdef USE_ZLIB		// gzip or bzip2 in user home directory
-    sprintf(buffer,"%s/%s/%s.gz",getenv("HOME"),FREECRAFT_HOME_PATH,file);
-    if( !access(buffer,R_OK) ) {
-	return buffer;
-    }
+	sprintf(buffer,"%s/%s/%s.gz",s,FREECRAFT_HOME_PATH,file);
+	if( !access(buffer,R_OK) ) {
+	    return buffer;
+	}
 #endif
 #ifdef USE_BZ2LIB
-    sprintf(buffer,"%s/%s/%s.bz2",getenv("HOME"),FREECRAFT_HOME_PATH,file);
-    if( !access(buffer,R_OK) ) {
-	return buffer;
-    }
+	sprintf(buffer,"%s/%s/%s.bz2",s,FREECRAFT_HOME_PATH,file);
+	if( !access(buffer,R_OK) ) {
+	    return buffer;
+	}
 #endif
 #ifdef USE_ZZIPLIB
-    sprintf(buffer,"%s/%s/%s",getenv("HOME"),FREECRAFT_HOME_PATH,file);
-    if( (zp=zzip_open(buffer,O_RDONLY|O_BINARY)) ) {
-	zzip_close(zp);
-	return buffer;
-    }
+	sprintf(buffer,"%s/%s/%s",s,FREECRAFT_HOME_PATH,file);
+	if( (zp=zzip_open(buffer,O_RDONLY|O_BINARY)) ) {
+	    zzip_close(zp);
+	    return buffer;
+	}
 #endif	// USE_ZZIPLIB
+    }
 
     //
     //	In global shared directory
