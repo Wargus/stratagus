89cd67f9f66f05dd6c0cc4442d0ff5038bda1d2d
nehalmistry <>
2002-12-01 08:06:18 +0000

    fix compiling with -DUSE_OPENGL on mingw

--- src/include/video.h
+++ src/include/video.h
@@ -59,7 +59,11 @@
 ----------------------------------------------------------------------------*/
 
 #ifdef USE_OPENGL
+#define DrawIcon WinDrawIcon
+#define EndMenu WinEndMenu
 #include <SDL_opengl.h>
+#undef EndMenu
+#undef DrawIcon
 #endif
 
 /*----------------------------------------------------------------------------
