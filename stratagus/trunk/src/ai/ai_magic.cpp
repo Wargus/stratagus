5ee1ac96c30911f49dc45ee6d23c5b3a7eaf8c2a
jsalmon3 <>
2003-02-05 06:22:55 +0000

    Spells cleanup.  Ai uses auto cast, ai can use more spells, added check for enemy units in range, renamed Kilrogg to Vision

--- stratagus/trunk/src/ai/ai_magic.cpp
+++ stratagus/trunk/src/ai/ai_magic.cpp
@@ -50,10 +50,16 @@
 local UnitType* AiPaladin;
 local SpellType* AiHolyVision;
 local SpellType* AiHealing;
+local UnitType* AiMage;
+local SpellType* AiSlow;
+local SpellType* AiInvisibility;
 local UnitType* AiOgreMage;
 local SpellType* AiEyeOfVision;
 local SpellType* AiBloodlust;
 local UnitType* AiUnitTypeEyeOfVision;
+local UnitType* AiDeathKnight;
+local SpellType* AiHaste;
+local SpellType* AiUnholyArmor;
 
 /*----------------------------------------------------------------------------
 --	Functions
@@ -69,25 +75,27 @@ local void AiInitMagic(void)
     AiPaladin=UnitTypeByIdent("unit-paladin");
     AiHolyVision=SpellTypeByIdent("spell-holy-vision");
     AiHealing=SpellTypeByIdent("spell-healing");
+    AiMage=UnitTypeByIdent("unit-mage");
+    AiSlow=SpellTypeByIdent("spell-slow");
+    AiInvisibility=SpellTypeByIdent("spell-invisibility");
     AiOgreMage=UnitTypeByIdent("unit-ogre-mage");
     AiEyeOfVision=SpellTypeByIdent("spell-eye-of-vision");
     AiBloodlust=SpellTypeByIdent("spell-bloodlust");
     AiUnitTypeEyeOfVision=UnitTypeByIdent("unit-eye-of-vision");
+    AiDeathKnight=UnitTypeByIdent("unit-death-knight");
+    AiHaste=SpellTypeByIdent("spell-haste");
+    AiUnholyArmor=SpellTypeByIdent("spell-unholy-armor");
 /*
 	"spell-exorcism"
 	"spell-fireball"
-	"spell-slow"
 	"spell-flame-shield"
-	"spell-invisibility"
 	"spell-polymorph"
 	"spell-blizzard"
 
 	"spell-runes"
 	"spell-death-coil"
-	"spell-haste"
 	"spell-raise-dead"
 	"spell-whirlwind"
-	"spell-unholy-armor"
 	"spell-death-and-decay"
 */
 }
@@ -102,67 +110,12 @@ local void AiInitMagic(void)
 **	@param unit	Magic unit.
 **
 **	@return		True, if a spell is casted.
-**
-**	@note This function can also be used for auto bloodlust.
 */
 local int AiBloodlustSpell(Unit* unit)
 {
-    Unit* best;
-    Unit* table[UnitMax];
-    int r;
-    int i;
-    int n;
-
-    if (UpgradeIdentAvailable(AiPlayer->Player, "upgrade-bloodlust")
-	    && unit->Mana > AiBloodlust->ManaCost) {
-
-	r = unit->Type->ReactRangeComputer;
-	n = SelectUnits(unit->X - r, unit->Y - r, unit->X + r + 1,
-	    unit->Y + r + 1, table);
-
-	for (i = 0; i < n; ++i) {
-
-	    // an enemy which can attack
-	    if (!IsEnemy(unit->Player, table[i])
-		    || (!table[i]->Type->CanAttack)) {
-		continue;
-	    }
-	    //
-	    //      We have an enemy in range.
-	    //
-	    best = NoUnitP;
-	    for (i = 0; i < n; ++i) {
-		// FIXME: Spell castable checks must be done global by spells.
-		// not self, not already bloodlust and can attack
-		if (table[i] == unit || table[i]->Bloodlust
-			|| !table[i]->Type->CanAttack) {
-		    continue;
-		}
-		// Allied unit
-		// FIXME: should ally to self
-		if (unit->Player != table[i]->Player
-			&& !IsAllied(unit->Player, table[i])) {
-		    continue;
-		}
-		r = MapDistanceBetweenUnits(unit, table[i]);
-		DebugLevel0Fn("Distance %d\n" _C_ r);
-		if (r <= 1) {
-		    DebugLevel0Fn("`%s' cast bloodlust\n" _C_ unit->Type->
-			Ident);
-		    CommandSpellCast(unit, 0, 0, table[i], AiBloodlust,
-			FlushCommands);
-		    return 1;
-		}
-		if (r == 2) {
-		    best = table[i];
-		}
-	    }
-	    if (best) {
-		CommandSpellCast(unit, 0, 0, best, AiBloodlust,
-		    FlushCommands);
-		return 1;
-	    }
-	    break;
+    if( UpgradeIdentAvailable(AiPlayer->Player,"upgrade-bloodlust") ) {
+	if( AutoCastSpell(unit,AiBloodlust) ) {
+	    return 1;
 	}
     }
     return 0;
@@ -178,29 +131,18 @@ local int AiBloodlustSpell(Unit* unit)
 **	@param unit	Magic unit.
 **
 **	@return		True, if a spell is casted.
-**
-**	@note This function can also be used for auto eye of vision.
 */
 local int AiEyeOfVisionSpell(Unit* unit)
 {
-    int r;
-
-    if( unit->Orders[0].Action==UnitActionStill ) {
-	if( UpgradeIdentAvailable(AiPlayer->Player,"upgrade-eye-of-kilrogg")
-		&& UpgradeIdentAvailable(AiPlayer->Player,
-		    "upgrade-ogre-mage") ) {
-	    r=SyncRand();
-	    /* s0m3body: each unit can have different MaxMana, the original condition is testing MaxMana-10,
-	     * so let's take unit's maxmana * 245/255 as a new treshold */
-	    if( unit->Mana>((unit->Type->_MaxMana * 245 ) / 255) && !(r%32) ) {
-		if( unit->Mana>AiEyeOfVision->ManaCost ) {
-		    DebugLevel0Fn("`%s' cast eye of vision\n"
-			_C_ unit->Type->Ident);
-
-		    CommandSpellCast(unit,unit->X,unit->Y,NoUnitP,
-			AiEyeOfVision,FlushCommands);
-		    return 1;
-		}
+    if( unit->Orders[0].Action==UnitActionStill
+	    && UpgradeIdentAvailable(AiPlayer->Player,"upgrade-eye-of-kilrogg")
+	    && UpgradeIdentAvailable(AiPlayer->Player,"upgrade-ogre-mage") ) {
+	// s0m3body: each unit can have different MaxMana, the original
+	//		condition is testing MaxMana-10, so let's take unit's
+	//		maxmana * 245/255 as a new threshold
+	if( unit->Mana>(unit->Type->_MaxMana*245)/255 && !SyncRand()%32 ) {
+	    if( AutoCastSpell(unit,AiEyeOfVision) ) {
+		return 1;
 	    }
 	}
     }
@@ -221,6 +163,82 @@ local void AiDoOgreMage(Unit* unit)
 }
 
 /**
+**	Check if the unit should cast the "slow" spell.
+**
+**	If the spell is available and the unit has enough mana, the surrounding
+**	of the unit is checked if any enemy units are in sight. If enemy units
+**	are in sight the spell is casted on the units in range.
+**
+**	@param unit	Magic unit.
+**
+**	@return		True, if a spell is casted.
+*/
+local int AiSlowSpell(Unit* unit)
+{
+    if( UpgradeIdentAvailable(AiPlayer->Player,"upgrade-slow") ) {
+	if( !SyncRand()%4 && AutoCastSpell(unit,AiSlow) ) {
+	    return 1;
+	}
+    }
+    return 0;
+}
+
+/**
+**	Check if the unit should cast the "invisibility" spell.
+**
+**	If the spell is available and the unit has enough mana, the surrounding
+**	of the unit is checked if any enemy units are in sight. If enemy units
+**	are in sight the spell is casted on own units in range.
+**
+**	@param unit	Magic unit.
+**
+**	@return		True, if a spell is casted.
+*/
+local int AiInvisibilitySpell(Unit* unit)
+{
+    if( UpgradeIdentAvailable(AiPlayer->Player,"upgrade-invisibility") ) {
+	if( !SyncRand()%4 && AutoCastSpell(unit,AiInvisibility) ) {
+	    return 1;
+	}
+    }
+    return 0;
+}
+
+/**
+**	Do magic for mage.
+*/
+local void AiDoMage(Unit* unit)
+{
+    if (AiSlowSpell(unit)) {
+	return;
+    }
+    if (AiInvisibilitySpell(unit)) {
+	return;
+    }
+}
+
+/**
+**	Check if the unit should cast the "healing" spell.
+**
+**	If the spell is available and the unit has enough mana, the surrounding
+**	of the unit is checked if any ally damaged units are in sight. If ally
+**	damaged units are in sight the spell is casted on own units in range.
+**
+**	@param unit	Magic unit.
+**
+**	@return		True, if a spell is casted.
+*/
+local int AiHealingSpell(Unit* unit)
+{
+    if (UpgradeIdentAvailable(AiPlayer->Player, "upgrade-healing") ) {
+	if( AutoCastSpell(unit,AiHealing) ) {
+	    return 1;
+	}
+    }
+    return 0;
+}
+
+/**
 **	Check if the unit should cast the "holy vision" spell.
 **
 **	If the unit has nothing to do and the spell is available and the unit
@@ -230,115 +248,19 @@ local void AiDoOgreMage(Unit* unit)
 **	@param unit	Magic unit.
 **
 **	@return		True, if a spell is casted.
-**
-**	@note This function can also be used for auto holy vision.
 */
 local int AiHolyVisionSpell(Unit* unit)
 {
-    int r;
-
-    if( unit->Orders[0].Action==UnitActionStill ) {
-	if( UpgradeIdentAvailable(AiPlayer->Player,"upgrade-holy-vision")
-		&& UpgradeIdentAvailable(AiPlayer->Player,
-		    "upgrade-paladin") ) {
-	    r=SyncRand();
-	    // s0m3body: each unit can have different MaxMana, the original
-	    //		condition is testing MaxMana-10, so let's take unit's
-	    //		maxmana * 245/255 as a new treshold
-	    if( unit->Mana>((unit->Type->_MaxMana * 245)/255) && !(r%32) ) {
-		if( unit->Mana>AiHolyVision->ManaCost ) {
-		    int x;
-		    int y;
-
-		    DebugLevel0Fn("`%s' cast holy vision\n"
-			_C_ unit->Type->Ident);
-		    // Look around randomly
-		    r>>=5;
-		    x=r%TheMap.Width;
-		    y=SyncRand()%TheMap.Height;
-		    CommandSpellCast(unit,x,y,NoUnitP,
-			AiHolyVision,FlushCommands);
-		    return 1;
-		}
-	    }
-	}
-    }
-    return 0;
-}
-
-/**
-**	Check if the unit should cast the "heal" spell.
-**
-**	If the spell is available and the unit has enough mana, the surrounding
-**	of the unit is checked if any ally damaged units are in sight. If ally
-**	damaged units are in sight the spell is casted on own units in range.
-**
-**	@param unit	Magic unit.
-**
-**	@return		True, if a spell is casted.
-**
-**	@note This function can also be used for auto heal.
-*/
-local int AiHealingSpell(Unit* unit)
-{
-    Unit* best;
-    Unit* table[UnitMax];
-    int r;
-    int i;
-    int n;
-
-    // FIXME : minimum Mana for Ai Healing should be configurable.
-    if (UpgradeIdentAvailable(AiPlayer->Player, "upgrade-healing")
-	    && unit->Mana > AiHealing->ManaCost+20) {
-
-	r = unit->Type->ReactRangeComputer;
-	n = SelectUnits(unit->X - r, unit->Y - r, unit->X + r + 1,
-	    unit->Y + r + 1, table);
-
-	for (i = 0; i < n; ++i) {
-
-	    // an ally
-	    if (IsEnemy(unit->Player, table[i])) {
-		continue;
-	    }
-	    //
-	    //      We have an ally in range...
-	    //
-	    best = NoUnitP;
-	    for (i = 0; i < n; ++i) {
-		// not self, organic and somewhat in need with healing
-		//	(HP<8/10*max), ...
-		// FIXME: Spell castable checks must be done global by spells.
-		if (table[i] == unit || !table[i]->Type->Organic
-			|| table[i]->HP>=((table[i]->Type->_HitPoints*9)/10)) {
-		    continue;
-		}
-
-		// Allied unit
-		// FIXME: should ally to self
-		if (unit->Player != table[i]->Player
-			&& !IsAllied(unit->Player, table[i])) {
-		    continue;
-		}
-		r = MapDistanceBetweenUnits(unit, table[i]);
-		DebugLevel0Fn("Distance %d\n" _C_ r);
-		if (r <= 1) {
-		    DebugLevel0Fn("`%s' cast healing\n" _C_ unit->Type->
-			Ident);
-		    CommandSpellCast(unit, 0, 0, table[i], AiHealing,
-			FlushCommands);
-		    return 1;
-		}
-		if (r == 2) {
-		    best = table[i];
-		}
-	    }
-	    if (best) {
-		CommandSpellCast(unit, 0, 0, best, AiHealing,
-		    FlushCommands);
+    if( unit->Orders[0].Action==UnitActionStill
+	    && UpgradeIdentAvailable(AiPlayer->Player,"upgrade-holy-vision")
+	    && UpgradeIdentAvailable(AiPlayer->Player,"upgrade-paladin") ) {
+	// s0m3body: each unit can have different MaxMana, the original
+	//		condition is testing MaxMana-10, so let's take unit's
+	//		maxmana * 245/255 as a new threshold
+	if( unit->Mana>(unit->Type->_MaxMana*245)/255 && !SyncRand()%32 ) {
+	    if( AutoCastSpell(unit,AiHolyVision) ) {
 		return 1;
 	    }
-	    break;
 	}
     }
     return 0;
@@ -353,6 +275,61 @@ local void AiDoPaladin(Unit* unit)
 	return;
     }
     if (AiHolyVisionSpell(unit)) {
+	return;
+    }
+}
+
+/**
+**	Check if the unit should cast the "haste" spell.
+**
+**	If the spell is available and the unit has enough mana, the surrounding
+**	of the unit is checked if any ally units are in sight. If ally units
+**	are in sight the spell is casted on the units in range.
+**
+**	@param unit	Magic unit.
+**
+**	@return		True, if a spell is casted.
+*/
+local int AiHasteSpell(Unit* unit)
+{
+    if( UpgradeIdentAvailable(AiPlayer->Player,"upgrade-haste") ) {
+	if( !SyncRand()%4 && AutoCastSpell(unit,AiHaste) ) {
+	    return 1;
+	}
+    }
+    return 0;
+}
+
+/**
+**	Check if the unit should cast the "unholy armor" spell.
+**
+**	If the spell is available and the unit has enough mana, the surrounding
+**	of the unit is checked if any enemy units are in sight. If enemy units
+**	are in sight the spell is casted on own units in range.
+**
+**	@param unit	Magic unit.
+**
+**	@return		True, if a spell is casted.
+*/
+local int AiUnholyArmorSpell(Unit* unit)
+{
+    if( UpgradeIdentAvailable(AiPlayer->Player,"upgrade-unholy-armor") ) {
+	if( !SyncRand()%4 && AutoCastSpell(unit,AiUnholyArmor) ) {
+	    return 1;
+	}
+    }
+    return 0;
+}
+
+/**
+**	Do magic for death knight.
+*/
+local void AiDoDeathKnight(Unit* unit)
+{
+    if (AiHasteSpell(unit)) {
+	return;
+    }
+    if (AiUnholyArmorSpell(unit)) {
 	return;
     }
 }
@@ -378,8 +355,12 @@ global void AiCheckMagic(void)
 	    // FIXME: I hardcode the reactions now
 	    if( unit->Type==AiOgreMage ) {
 		AiDoOgreMage(unit);
+	    } else if( unit->Type==AiMage ) {
+		AiDoMage(unit);
 	    } else if( unit->Type==AiPaladin ) {
 		AiDoPaladin(unit);
+	    } else if( unit->Type==AiDeathKnight ) {
+		AiDoDeathKnight(unit);
 	    }
 	}
 	//
