8d70cdd8961ae824103096d841a3fe3e0d2475e7
nehalmistry <>
2003-02-12 20:27:08 +0000

    use strcmp to check CDMode

--- src/game/intro.cpp
+++ src/game/intro.cpp
@@ -538,7 +538,7 @@ global void ShowIntro(const Intro *intro)
     CallbackMusicOn();
     StopMusic();
     // FIXME: should this be GameMusic?
-    if (CDMode == "off")
+    if (!strcmp(CDMode, "off"))
     {
 	PlayMusic(MenuMusic);
     }
