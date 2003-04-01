ecda6f1d0d439f7587d36d786404129093855a91
jsalmon3 <>
2003-04-01 01:43:16 +0000

    Fixed copyright

--- include/player.h
+++ include/player.h
@@ -10,7 +10,7 @@
 //
 /**@name player.h	-	The player headerfile. */
 //
-//	(c) Copyright 1998-2002 by Lutz Sammer
+//	(c) Copyright 1998-2003 by Lutz Sammer
 //
 //	FreeCraft is free software; you can redistribute it and/or modify
 //	it under the terms of the GNU General Public License as published
