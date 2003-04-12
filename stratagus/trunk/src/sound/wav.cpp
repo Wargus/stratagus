694c8b4164c0d02101a96c79428dea298c4182c2
nehalmistry <>
2003-04-12 05:58:50 +0000

    allow LoadWav to play stereo sounds correctly

--- stratagus/trunk/src/sound/wav.cpp
+++ stratagus/trunk/src/sound/wav.cpp
@@ -221,7 +221,7 @@ global Sample* LoadWav(const char* name, int flags __attribute__((unused)))
 	CLclose(f);
 	ExitFatal(-1);
     }
-    if (wavfmt.SampleSize != 1 && wavfmt.SampleSize != 2) {
+    if (wavfmt.SampleSize != 1 && wavfmt.SampleSize != 2 && wavfmt.Sample != 4) {
 	printf("Unsupported sample size %d\n", wavfmt.SampleSize);
 	CLclose(f);
 	ExitFatal(-1);
@@ -236,7 +236,7 @@ global Sample* LoadWav(const char* name, int flags __attribute__((unused)))
     //
     //  Read sample
     //
-    sample = malloc(sizeof(*sample) + WAV_BUFFER_SIZE);
+    sample = malloc(sizeof(*sample) + WAV_BUFFER_SIZE*wavfmt.Channels);
     sample->Channels = wavfmt.Channels;
     sample->SampleSize = wavfmt.SampleSize * 8;
     sample->Frequency = wavfmt.Frequency;
