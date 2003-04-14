540b50309430d8ce14634ad81ba4426cc40d0369
jsalmon3 <>
2003-04-14 06:30:10 +0000

    More cleanup

--- include/cdaudio.h
+++ include/cdaudio.h
@@ -8,7 +8,7 @@
 //			  T H E   W A R   B E G I N S
 //	   FreeCraft - A free fantasy real time strategy game engine
 //
-/**@name cdaudio.h		-	The cdda format header file. */
+/**@name cdaudio.h		-	cd audio header file. */
 //
 //	(c) Copyright 2003 by Nehal Mistry
 //
@@ -34,6 +34,10 @@
 #error WITH_SOUND must be defined to use cd audio
 #endif
 
+/*----------------------------------------------------------------------------
+--	Includes
+----------------------------------------------------------------------------*/
+
 #if defined(USE_SDLCD) 
 #include "SDL.h" 
 #elif defined(USE_LIBCDA) 
@@ -47,54 +51,56 @@
 
 #include "sound_server.h"
 
+/*----------------------------------------------------------------------------
+--	Declarations
+----------------------------------------------------------------------------*/
+
 typedef enum _cd_modes_ {
-  CDModeStopped = -1,                 /// Stopped
-  CDModeOff,                          /// Off
-  CDModeAll,                          /// All
-  CDModeRandom,                       /// Random
-  CDModeDefined,                      /// Defined
+    CDModeStopped = -1,                 /// Stopped
+    CDModeOff,                          /// Off
+    CDModeAll,                          /// All
+    CDModeRandom,                       /// Random
+    CDModeDefined,                      /// Defined
 } CDModes;
 
+/*----------------------------------------------------------------------------
+--	Variables
+----------------------------------------------------------------------------*/
+
 extern CDModes CDMode;                  /// CD mode
+extern int CDTrack;			/// Current track
+extern int NumCDTracks;			/// Number of tracks on CD
 
-#ifdef USE_CDDA 
-extern Sample* LoadCD(const char* name,int flags);      /// Load a cd track
-extern int CDDrive; 
-extern struct cdrom_tochdr CDchdr; 
-extern struct cdrom_tocentry CDtocentry[64]; 
-extern struct cdrom_read_audio CDdata; 
+#ifdef USE_CDDA
+extern int CDDrive;				/// CDRom device
+extern struct cdrom_tocentry CDtocentry[64];	/// TOC track header struct
+#endif
+
+/*----------------------------------------------------------------------------
+--	Functions
+----------------------------------------------------------------------------*/
+
+#ifdef USE_CDDA
+    /// Load a cd track
+extern Sample* LoadCD(const char* name,int flags);      
 #endif 
-
-    /// Current track
-extern int CDTrack;
-
-    /// Number of tracks on CD
-extern int NumCDTracks;
 
     /// Play CDMode 'name'
 extern int PlayCDRom(int name);
-
     /// Play 'track'
 extern int PlayCDTrack(int track);
-
     /// Is 'track' an audio track?
 extern int IsAudioTrack(int track);
-
     /// Get cd volume (0-255)
 extern int GetCDVolume(void);
-
     /// Set cd volume (0-255)
 extern void SetCDVolume(int vol);
-
     /// Resume CD
 extern void ResumeCD(void);
-
     /// Pause CD
 extern void PauseCD(void);
-
     /// Close CD
 extern void QuitCD(void);
-
     /// Check the cdrom status
 extern int CDRomCheck(void *);
 
