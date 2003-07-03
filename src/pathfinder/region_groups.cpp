2521f284a8c5d46eb2aca658ee31fa9c65faa034
ingo <>
2003-07-03 15:59:56 +0000

    replaced broken copyright with correct one

--- src/pathfinder/region_groups.cpp
+++ src/pathfinder/region_groups.cpp
@@ -13,14 +13,19 @@
 //
 //	(c) Copyright 2002 by Latimerius
 //
-//	Stratagus is free software; you can redistribute it and/or modify
-//	it under the terms of the GNU General Public License as published
-//	by the Free Software Foundation; only version 2 of the License.
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; version 2 dated June, 1991.
 //
-//	Stratagus is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	GNU General Public License for more details.
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//      02111-1307, USA.
 //
 //	$Id$
 
