c54b6c23ef5d3624b8a5b10b628a58ba13fc5a07
jsalmon3 <>
2003-09-28 19:19:55 +0000

    Cleanup

--- video/intern_video.h
+++ video/intern_video.h
@@ -92,36 +92,36 @@ extern int ClipY2;			/// current clipping bottom right
 **	@param height	unsigned int height to display
 **			(return value of width and height can be made smaller)
 */
-#define CLIP_RECTANGLE(x,y,width,height) { \
-  int f;			   \
-  if( x<ClipX1 ) {			   \
-    f=ClipX1-x;				   \
-    if( width<=f ) {			   \
-       return;				   \
-    }					   \
-    width-=f;				   \
-    x=ClipX1;				   \
-  }					   \
-  if( (x+width)>ClipX2+1 ) {		   \
-    if( x>ClipX2 ) {			   \
-      return;				   \
-    }					   \
-    width=ClipX2-x+1;			   \
-  }					   \
-  if( y<ClipY1 ) {			   \
-    f=ClipY1-y;				   \
-    if( height<=f ) {			   \
-      return;				   \
-    }					   \
-    height-=f;				   \
-    y=ClipY1;				   \
-  }					   \
-  if( (y+height)>ClipY2+1 ) {		   \
-    if( y>ClipY2 ) {			   \
-	return;				   \
-    }					   \
-    height=ClipY2-y+1;			   \
-  }					   \
+#define CLIP_RECTANGLE(x, y, width, height) { \
+    int f; \
+    if (x < ClipX1) { \
+	f = ClipX1 - x; \
+	if (width <= f) { \
+	    return; \
+	} \
+	width -= f; \
+	x = ClipX1; \
+    } \
+    if ((x + width) > ClipX2 + 1) { \
+	if (x > ClipX2) { \
+	    return; \
+	} \
+	width = ClipX2 - x + 1; \
+    } \
+    if (y < ClipY1) { \
+	f = ClipY1 - y; \
+	if (height <= f) { \
+	    return; \
+	} \
+	height -= f; \
+	y = ClipY1; \
+    } \
+    if ((y + height) > ClipY2 + 1) { \
+	if (y > ClipY2) { \
+	    return; \
+	} \
+	height = ClipY2 - y + 1; \
+    } \
 }
 
 /**
@@ -147,39 +147,42 @@ extern int ClipY2;			/// current clipping bottom right
 **
 **	@note there was no need for 'endy', as it isn't used to draw sprites..
 */
-#define CLIP_RECTANGLE_OFS(x,y,width,height,ofsx,ofsy,endx) { \
-  if( y<ClipY1 ) {					      \
-    ofsy=ClipY1-y;					      \
-    if( height<=ofsy ) {				      \
-      return;						      \
-    }							      \
-    height-=ofsy;					      \
-    y=ClipY1;						      \
-  }							      \
-  else ofsy=0;						      \
-  if( (y+height)>ClipY2+1 )   {				      \
-    if( y>ClipY2 ) {					      \
-	return;						      \
-    }							      \
-    height=ClipY2-y+1;					      \
-  }							      \
-  if( x<ClipX1 ) {					      \
-    ofsx=ClipX1-x;					      \
-    if( width<=ofsx ) {					      \
-       return;						      \
-    }							      \
-    width-=ofsx;					      \
-    x=ClipX1;						      \
-  }							      \
-  else ofsx=0;						      \
-  if( (x+width)>ClipX2+1 ) {				      \
-    if( x>ClipX2 ) {					      \
-      return;						      \
-    }							      \
-    endx=(x+width)-(ClipX2+1);				      \
-    width=ClipX2-x+1;					      \
-  }							      \
-  else endx=0;						      \
+#define CLIP_RECTANGLE_OFS(x, y, width, height, ofsx, ofsy, endx) { \
+    if (y < ClipY1) { \
+	ofsy = ClipY1 - y; \
+	if (height <= ofsy) { \
+	    return; \
+	} \
+	height -= ofsy; \
+	y = ClipY1; \
+    } else { \
+	ofsy = 0; \
+    } \
+    if ((y + height) > ClipY2 + 1) { \
+	if (y > ClipY2) { \
+	    return; \
+	} \
+	height = ClipY2 - y + 1; \
+    } \
+    if (x < ClipX1) { \
+	ofsx = ClipX1 - x; \
+	if (width <= ofsx) { \
+	    return; \
+	} \
+	width -= ofsx; \
+	x = ClipX1; \
+    } else { \
+	ofsx = 0; \
+    } \
+    if ((x + width) > ClipX2 + 1) { \
+	if (x > ClipX2) { \
+	    return; \
+	} \
+	endx = (x + width) - (ClipX2 + 1); \
+	width = ClipX2 - x + 1; \
+    } else { \
+	endx = 0; \
+    } \
 }
 
 
