2404ca2d8282b28f14642f31b784e679a5253837
jsalmon3 <>
2003-11-20 03:05:48 +0000

    Renamed Screen to TheScreen, merged with USE_SDL_SURFACE

--- stratagus/trunk/src/video/movie.cpp
+++ stratagus/trunk/src/video/movie.cpp
@@ -57,11 +57,7 @@ extern void VPDeInitLibrary(void);
 
 #ifdef USE_SDL				/// Only supported with SDL for now
 
-#ifdef USE_SDL_SURFACE
 extern SDL_Surface* TheScreen;	/// internal screen
-#else
-extern SDL_Surface* Screen;	/// internal screen
-#endif
 
 /*----------------------------------------------------------------------------
 --	Defines
@@ -250,13 +246,8 @@ global int PlayMovie(const char* name, int flags)
     if (flags & PlayMovieFullScreen) {
 	DebugLevel0Fn("FIXME: full screen switch not supported\n");
     }
-#ifdef USE_SDL_SURFACE
     overlay = SDL_CreateYUVOverlay(avi->Width, avi->Height,
 	SDL_YV12_OVERLAY, TheScreen);
-#else
-    overlay = SDL_CreateYUVOverlay(avi->Width, avi->Height,
-	SDL_YV12_OVERLAY, Screen);
-#endif
     if (!overlay) {
 	fprintf(stderr, "Couldn't create overlay: %s\n", SDL_GetError());
 	exit(1);
