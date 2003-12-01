9b9cfe5273313ace5331c29071884c3796c3b555
nehalmistry <>
2003-12-01 20:02:46 +0000

    fixed unit colors with new code

--- stratagus/trunk/src/unit/unit_draw.cpp
+++ stratagus/trunk/src/unit/unit_draw.cpp
@@ -2229,10 +2229,12 @@ global void DrawUnit(const Unit* unit)
 	if (unit->Value) {
 	    if (resinfo->SpriteWhenLoaded) {
 		sprite = resinfo->SpriteWhenLoaded;
+		GraphicUnitPixels(unit, sprite);
 	    }
 	} else {
 	    if (resinfo->SpriteWhenEmpty) {
 		sprite = resinfo->SpriteWhenEmpty;
+		GraphicUnitPixels(unit, sprite);
 	    }
 	}
     }
