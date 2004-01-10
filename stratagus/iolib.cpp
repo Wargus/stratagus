8363e5a96460f978befb1b081775e9a0a95ef960
jsalmon3 <>
2004-01-10 06:13:01 +0000

    Removed zziplib

--- stratagus/iolib.cpp
+++ stratagus/iolib.cpp
@@ -119,7 +119,7 @@ local void bzseek(BZFILE* file, unsigned offset, int whence __attribute__((unuse
 
 #endif		// USE_BZ2LIB
 
-#if defined(USE_ZLIB) || defined(USE_BZ2LIB) || defined(USE_ZZIPLIB)
+#if defined(USE_ZLIB) || defined(USE_BZ2LIB)
 
 /**
 **		CLopen				Library file open
@@ -176,11 +176,6 @@ global CLFile* CLopen(const char* fn, long openflags)
 #ifdef USE_BZ2LIB
 			if ((clf.cl_bz = bzopen(strcat(strcpy(buf, fn), ".bz2"), "rb"))) {
 				clf.cl_type = CLF_TYPE_BZIP2;
-			} else
-#endif
-#ifdef USE_ZZIPLIB
-			if ((clf.cl_zz = zzip_open(strcpy(buf, fn), O_RDONLY | O_BINARY))) {
-				clf.cl_type = CLF_TYPE_ZZIP;
 			} else
 #endif
 			{ }
@@ -260,12 +255,6 @@ global int CLclose(CLFile* file)
 			ret = 0;
 		}
 #endif		// USE_BZ2LIB
-#ifdef USE_ZZIPLIB
-		if (tp == CLF_TYPE_ZZIP) {
-			zzip_close(file->cl_zz);
-			ret = 0;
-		}
-#endif		// USE_ZZIPLIB
 		free(file);
 	} else {
 		errno = EBADF;
@@ -301,11 +290,6 @@ global int CLread(CLFile* file, void* buf, size_t len)
 			ret = bzread(file->cl_bz, buf, len);
 		}
 #endif		// USE_BZ2LIB
-#ifdef USE_ZZIPLIB
-		if (tp == CLF_TYPE_ZZIP) {
-			ret = zzip_read(file->cl_zz, buf, len);
-		}
-#endif		// USE_ZZIPLIB
 	} else {
 		errno = EBADF;
 	}
@@ -379,13 +363,6 @@ global int CLprintf(CLFile* file, char* format, ...)
 			ret = bzwrite(file->cl_bz, p, size);
 		}
 #endif		// USE_BZ2LIB
-#if defined(USE_ZZIPLIB) && 0
-		// FIXME: Support Unknown and Unsupported
-		if (tp == CLF_TYPE_ZZIP) {
-			zzip_write(file->cl_zz, p, size);
-			ret = 0;
-		}
-#endif		// USE_ZZIPLIB
 	} else {
 		errno = EBADF;
 	}
@@ -422,19 +399,13 @@ global int CLseek(CLFile* file, long offset, int whence)
 			ret = 0;
 		}
 #endif		// USE_BZ2LIB
-#ifdef USE_ZZIPLIB
-		if (tp == CLF_TYPE_ZZIP) {
-			zzip_seek(file->cl_zz, offset, whence);
-			ret = 0;
-		}
-#endif		// USE_ZZIPLIB
 	} else {
 		errno = EBADF;
 	}
 	return ret;
 }
 
-#endif		// USE_ZLIB || USE_BZ2LIB || USE_ZZIPLIB
+#endif		// USE_ZLIB || USE_BZ2LIB
 
 /**
 **		Generate a filename into library.
@@ -449,9 +420,6 @@ global int CLseek(CLFile* file, long offset, int whence)
 */
 global char* LibraryFileName(const char* file, char* buffer)
 {
-#ifdef USE_ZZIPLIB
-	ZZIP_FILE* zp;
-#endif
 	char* s;
 
 	//
@@ -473,13 +441,6 @@ global char* LibraryFileName(const char* file, char* buffer)
 		return buffer;
 	}
 #endif
-#ifdef USE_ZZIPLIB
-	strcpy(buffer, file);
-	if ((zp = zzip_open(buffer, O_RDONLY | O_BINARY))) {
-		zzip_close(zp);
-		return buffer;
-	}
-#endif		// USE_ZZIPLIB
 
 	//
 	//		Try in map directory
@@ -521,12 +482,6 @@ global char* LibraryFileName(const char* file, char* buffer)
 		}
 		*strrchr(buffer, '.') = '\0';
 #endif
-#ifdef USE_ZZIPLIB
-		if ((zp = zzip_open(buffer, O_RDONLY | O_BINARY))) {
-			zzip_close(zp);
-			return buffer;
-		}
-#endif		// USE_ZZIPLIB
 	}
 
 	if ((s = getenv("HOME"))) {
@@ -549,13 +504,6 @@ global char* LibraryFileName(const char* file, char* buffer)
 			return buffer;
 		}
 #endif
-#ifdef USE_ZZIPLIB
-		sprintf(buffer, "%s/%s/%s/%s", s, STRATAGUS_HOME_PATH, GameName, file);
-		if ((zp = zzip_open(buffer, O_RDONLY | O_BINARY))) {
-			zzip_close(zp);
-			return buffer;
-		}
-#endif		// USE_ZZIPLIB
 	}
 
 	//
@@ -577,13 +525,6 @@ global char* LibraryFileName(const char* file, char* buffer)
 		return buffer;
 	}
 #endif
-#ifdef USE_ZZIPLIB
-	sprintf(buffer, "%s/%s", StratagusLibPath, file);
-	if ((zp = zzip_open(buffer, O_RDONLY | O_BINARY))) {
-		zzip_close(zp);
-		return buffer;
-	}
-#endif		// USE_ZZIPLIB
 	DebugLevel0Fn("File `%s' not found\n" _C_ file);
 
 	strcpy(buffer, file);
@@ -613,56 +554,6 @@ local int flqcmp(const void* v1, const void* v2)
 	}
 }
 
-#ifdef USE_ZZIPLIB
-/**
- * will attach a .zip extension and tries to open it
- * the with => open(2). This is a helper function for
- * => zzip_dir_open, => zzip_opendir and => zzip_open.
- *
- * copied from zzip_dir.c, as it is a private function
- * and won't be exported in shared lib / dll version!
- */
-local int
-__my_zzip_open_zip(const char* filename, int filemode)
-{
-	auto char file[PATH_MAX];
-	int fd;
-	int len;
-#ifndef USE_WIN32
-	static const char* my_zzip_default_fileext[] =
-	{
-		".zip", ".ZIP", /* common extension */
-		0
-	};
-	const char** ext;
-#endif
-
-	fd = -1;
-	len = strlen(filename);
-#ifdef USE_WIN32
-	if (len + 4 < PATH_MAX) {
-		strcpy(file, filename);
-		strcpy(file + len, ".zip");
-		fd = open(file, filemode);
-	}
-#else
-	ext = my_zzip_default_fileext;
-	if (len + 4 < PATH_MAX) {
-		memcpy(file, filename, len + 1);
-
-		for (; *ext ; ++ext) {
-			strcpy(file + len, *ext);
-			fd = open(file, filemode);
-			if (fd != -1) {
-				break;
-			}
-		}
-	}
-#endif
-	return fd;
-}
-#endif
-
 /**
 **		Generate a list of files within a specified directory
 **
@@ -674,24 +565,11 @@ __my_zzip_open_zip(const char* filename, int filemode)
 */
 global int ReadDataDirectory(const char* dirname, int (*filter)(char*, FileList*), FileList** flp)
 {
-#ifdef USE_ZZIPLIB
-	ZZIP_DIR* dirp = NULL;
-	ZZIP_DIRENT* dp;
-	// ATTENTION: valid until end of file!
-	#define readdir zzip_readdir
-	#define closedir zzip_closedir
-	int i;
-	int entvalid;
-	char zzbasepath[PATH_MAX];
-	struct stat st;
-	char* cp;
-#else
 #ifndef _MSC_VER
 	DIR* dirp;
 	struct dirent* dp;
 #endif
 	struct stat st;
-#endif
 #ifdef _MSC_VER
 	struct _finddata_t fileinfo;
 	long hFile;
@@ -713,61 +591,8 @@ global int ReadDataDirectory(const char* dirname, int (*filter)(char*, FileList*
 	np = buffer + n;
 	n = 0;
 
-#ifdef USE_ZZIPLIB
-	strcpy(zzbasepath, dirname);
-	/* per each slash in filename, check if it there is a zzip around */
-	while ((cp = strrchr(zzbasepath, '/'))) {
-		int fd;
-		zzip_error_t e;
-
-		*cp = '\0'; /* cut at path separator == possible zipfile basename */
-		fd = __my_zzip_open_zip(zzbasepath, O_RDONLY | O_BINARY);
-		if (fd != -1) {
-			/* found zip-file, now open it */
-			dirp = zzip_dir_fdopen(fd, &e);
-			if (e) {
-				errno = zzip_errno(e);
-				close(fd);
-				dirp = NULL;
-			}
-			break;
-		}
-	}
-	if (!dirp) {
-		int fd;
-		zzip_error_t e;
-
-		// this is tricky - we used to simply zzip_opendir(dirname) here, but
-		// zziplib (correctly) prefers real directories over same named zipfiles
-		// and we want it vice versa in this special case. Otherwise it would not
-		// match the path separator backtrace above, which relies on recursive
-		// __zip_open_dir(). __zip_open_dir() only detects zipfiles, not real dirs!
-		fd = __my_zzip_open_zip(dirname, O_RDONLY | O_BINARY);
-		if (fd == -1) {
-			dirp = zzip_opendir(dirname);
-			zzbasepath[0] = 0;
-		} else {
-			dirp = zzip_dir_fdopen(fd, &e);
-			if (e) {
-				errno = zzip_errno(e);
-				close(fd);
-				dirp = NULL;
-			} else {
-				strcpy(zzbasepath, dirname);
-			}
-			DebugLevel3Fn("zzbasepath `%s', dirname `%s'\n" _C_
-				zzbasepath _C_ dirname);
-		}
-	}
-#if !defined(_MSC_VER) && defined(DEBUG)
-	if (!dirp) {
-		DebugLevel0Fn("Dir `%s' not found\n" _C_ dirname);
-	}
-#endif
-#else
 #ifndef _MSC_VER
 	dirp = opendir(dirname);
-#endif
 #endif
 
 #ifndef _MSC_VER
@@ -775,37 +600,11 @@ global int ReadDataDirectory(const char* dirname, int (*filter)(char*, FileList*
 		while ((dp = readdir(dirp)) != NULL) {
 			filename = dp->d_name;
 #else
-#ifdef USE_ZZIPLIB
-	if (!dirp) {
-		strcat(buffer, "*.*");
-		hFile = _findfirst(buffer, &fileinfo);
-	}
-	if (dirp || (!dirp && hFile != -1L)) {
-		int first;
-		first = 1;
-		while (1) {
-			if (!dirp) {
-				if (first) {
-					first = 0;
-				} else {
-					if (_findnext(hFile, &fileinfo) != 0) {
-						break;
-					}
-				}
-				filename = fileinfo.name;
-			} else {
-				if ((dp = readdir(dirp)) == NULL) {
-					break;
-				}
-				filename = dp->d_name;
-			}
-#else
 	strcat(buffer, "*.*");
 	hFile = _findfirst(buffer, &fileinfo);
 	if (hFile != -1L) {
 		do {
 			filename = fileinfo.name;
-#endif
 #endif
 
 			if (strcmp(filename, ".") == 0) {
@@ -816,80 +615,9 @@ global int ReadDataDirectory(const char* dirname, int (*filter)(char*, FileList*
 			}
 
 			strcpy(np, filename);
-#ifdef USE_ZZIPLIB
-			entvalid = 0;
-			if (dirp && zzip_dir_real(dirp)) {
-				if (stat(buffer, &st) == 0) {
-					isdir = S_ISDIR(st.st_mode);
-					if (isdir || S_ISREG(st.st_mode)) {
-						entvalid = 1;
-						if (!isdir) {		// Find out if we have a zip here ..
-							cp = strrchr(buffer, '.');
-							if (cp) {
-								*cp = 0;
-								isdir = __my_zzip_open_zip(buffer, O_RDONLY|O_BINARY);
-								if (isdir != -1) {
-									close(isdir);
-									isdir = 1;
-								} else {
-									isdir = 0;
-									*cp = '.';
-								}
-							}
-						}
-					}
-				}
-			} else if (dirp) {
-				if (zzbasepath[0]) {
-					size_t len;
-					len = strlen(zzbasepath);
-					isdir = 0;
-					if (strlen(dirname) > len) {
-						cp = (char*)dirname + len + 1;
-						len = strlen(cp);
-						if (strlen(filename) >= len && memcmp(filename, cp, len) == 0 &&
-								filename[len] == '/' && filename[len + 1]) {
-							strcpy(np, filename + len + 1);
-							goto zzentry;
-						}
-					} else {
-						strcpy(np, dp->d_name);
-						goto zzentry;
-					}
-				} else {
-zzentry:
-					DebugLevel3Fn("zzip-entry `%s', `%s'\n" _C_ buffer _C_ np);
-					entvalid = 1;
-					cp = strchr(np, '/');
-					if (cp) {
-						isdir = 1;
-						*cp = 0;
-						for (i = 0; i < n; ++i) {
-							if (fl[i].type == 0 && strcmp(fl[i].name, np) == 0) {
-								entvalid = 0;		// already there
-								break;
-							}
-						}
-					}
-				}
-			}
-#ifdef _MSC_VER
-			else {
-				if (stat(buffer, &st) == 0) {
-					isdir = S_ISDIR(st.st_mode);
-					if (isdir || S_ISREG(st.st_mode)) {
-						entvalid = 1;
-					}
-				}
-			}
-#endif
-			{
-				if (entvalid) {
-#else //	}}
 			if (stat(buffer, &st) == 0) {
 				isdir = S_ISDIR(st.st_mode);
 				if (isdir || S_ISREG(st.st_mode)) {
-#endif
 					if (n) {
 						nfl = realloc(fl, sizeof(FileList) * (n + 1));
 						if (nfl) {
@@ -928,17 +656,8 @@ zzentry:
 		}
 		closedir(dirp);
 #else
-#ifdef USE_ZZIPLIB
-		}
-		if (!dirp) {
-			_findclose(hFile);
-		} else {
-			closedir(dirp);
-		}
-#else
 		} while (_findnext(hFile, &fileinfo) == 0);
 		_findclose(hFile);
-#endif
 #endif
 	}
 	if (n == 0) {
