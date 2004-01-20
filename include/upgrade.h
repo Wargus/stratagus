db80955f1a7da446157a97e7574e1f5b6670cb02
jsalmon3 <>
2004-01-20 18:48:14 +0000

    Clean up

--- include/upgrade.h
+++ include/upgrade.h
@@ -5,12 +5,12 @@
 //     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
 //             \/                  \/          \//_____/            \/
 //  ______________________                           ______________________
-//			  T H E   W A R   B E G I N S
-//	   Stratagus - A free fantasy real time strategy game engine
+//                        T H E   W A R   B E G I N S
+//         Stratagus - A free fantasy real time strategy game engine
 //
 /**@name upgrade.h	-	The upgrades headerfile. */
 //
-//	(c) Copyright 1999-2002 by Vladi Belperchinov-Shabanski
+//      (c) Copyright 1999-2004 by Vladi Belperchinov-Shabanski and Jimmy Salmon
 //
 //      This program is free software; you can redistribute it and/or modify
 //      it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@
 //      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //      02111-1307, USA.
 //
-//	$Id$
+//      $Id$
 
 #ifndef __UPGRADE_H__
 #define __UPGRADE_H__
@@ -34,21 +34,21 @@
 //@{
 
 /*----------------------------------------------------------------------------
---		Includes
+--  Includes
 ----------------------------------------------------------------------------*/
 
 #include "upgrade_structs.h"
 
 /*----------------------------------------------------------------------------
---		Declarations
+--  Declarations
 ----------------------------------------------------------------------------*/
 
 /*----------------------------------------------------------------------------
---		Variables
+--  Variables
 ----------------------------------------------------------------------------*/
 
 /*----------------------------------------------------------------------------
---		Functions
+--  Functions
 ----------------------------------------------------------------------------*/
 
 	 /// upgrade of identifier
@@ -59,134 +59,53 @@ extern void InitUpgrades(void);
 	/// save the upgrades
 extern void SaveUpgrades(CLFile*);
 	/// cleanup upgrade module
-extern void CleanUpgrades();
+extern void CleanUpgrades(void);
 
 	/// parse pud alow (upgrade/spell/units allow) table
-extern void ParsePudALOW(const char*,int);
+extern void ParsePudALOW(const char*, int);
 	/// parse pud ugrd (upgrade cost) table
-extern void ParsePudUGRD(const char*,int);
+extern void ParsePudUGRD(const char*, int);
 	/// Register CCL features for upgrades
 extern void UpgradesCclRegister(void);
 
-// CHAOS PUR
-
-/*
-
-  Small help notes -- How to use upgrades -- Demo
-  ----------------------------------------------------------------------------
-  This is just a demo -- perhaps is not usefull IRL
-
-  // start
-
-  UpgradesInit(); // should be called in the beginning
-
-  uid=AddUpgrade( "UpgradeBerserker", 100, 200, 300, 0, IconUpgradeBerserkerId)
-  AddUpgradeModifier( uid,
-		  1, 1,	// more sight
-		  +10, +5, // more damage
-		  -5,	  // less armor
-		  0, 0,	// speed and HP are the same
-		  0, 0, 0, 0, // costs are the same
-				 // allow berserker and forbid axethrower
-		  "A:UnitBerserker,F:UnitAxeThrower",
-				  // allows BerserkerRange1 upgrade
-		  "A:UpgradeBerserkerRange1",
-				  // there are no allow/frobid actions
-		  "",
-				  // apply to Berserker units
-		  "UnitBerserker"
-  );
-
-  UpgradesDone(); // this should be called at the end of the game
-
-  // end
-
-  this is general idea, you can have multiple upgrade modifiers for
-  applying to different units and whatever you like
-
-*/
-
 /*----------------------------------------------------------------------------
---		Init/Done/Add functions
-----------------------------------------------------------------------------*/
-
-// this function is used for define `simple' upgrades
-// with only one modifier
-extern void AddSimpleUpgrade( const char*,
-  const char*,
-  // upgrade costs
-  int*,
-  // upgrade modifiers
-  int, int, int, int, int, int, int, int*,
-  const char*
-  );
-
-/*----------------------------------------------------------------------------
---		General/Map functions
+--  General/Map functions
 ----------------------------------------------------------------------------*/
 
 // AllowStruct and UpgradeTimers will be static in the player so will be
 // load/saved with the player struct
 
-extern int UnitTypeIdByIdent( const char* sid );
-extern int UpgradeIdByIdent( const char* sid );
-extern int ActionIdByIdent( const char* sid );
+extern int UnitTypeIdByIdent(const char* sid);
+extern int UpgradeIdByIdent(const char* sid);
 
 /*----------------------------------------------------------------------------
---		Upgrades
+--  Upgrades
 ----------------------------------------------------------------------------*/
 
-	/// Upgrade will be acquired, called by UpgradeIncTime() when timer reached
-extern void UpgradeAcquire( Player* player,const Upgrade* upgrade );
-
-	/// Increment the upgrade timer.
-extern void UpgradeIncTime( Player* player, int id, int amount );
-extern void UpgradeIncTime2( Player* player, char* sid, int amount ); // by ident string
-
-// this function will mark upgrade done and do all required modifications to
-// unit types and will modify allow/forbid maps
-
+	/// Upgrade will be acquired
+extern void UpgradeAcquire(Player* player, const Upgrade* upgrade);
 
 // for now it will be empty?
-// perhaps acquired upgrade can be lost if ( for example ) a building is lost
-// ( lumber mill? stronghold? )
+// perhaps acquired upgrade can be lost if (for example) a building is lost
+// (lumber mill? stronghold?)
 // this function will apply all modifiers in reverse way
-extern void UpgradeLost( Player* player, int id );
-extern void UpgradeLost2( Player* player, char* sid ); // by ident string
+extern void UpgradeLost(Player* player, int id);
 
 /*----------------------------------------------------------------------------
---		Allow(s)
+--  Allow(s)
 ----------------------------------------------------------------------------*/
 
 // all the following functions are just map handlers, no specific notes
 // id -- unit type id, af -- `A'llow/`F'orbid
-extern void AllowUnitId( Player* player, int id, int units );
-extern void AllowUnitByIdent( Player* player, const char* sid, int units );
 
-extern void AllowActionId( Player* player,  int id, char af );
-extern void AllowActionByIdent( Player* player, const char* sid, char af );
+extern int UnitIdAllowed(const Player* player, int id);
 
-extern void AllowUpgradeId( Player* player,  int id, char af );
-extern void AllowUpgradeByIdent( Player* player, const char* sid, char af );
-
-extern void AllowByIdent( Player* player, const char* sid, int units );
-
-extern int UnitIdAllowed(const Player* player,  int id );
-extern int UnitIdentAllowed(const Player* player,const char* sid );
-
-extern char ActionIdAllowed(const Player* player,  int id );
-extern char ActionIdentAllowed(const Player* player,const char* sid );
-
-extern char UpgradeIdAllowed(const Player* player,  int id );
-extern char UpgradeIdentAllowed(const Player* player,const char* sid );
+extern char UpgradeIdAllowed(const Player* player, int id);
+extern char UpgradeIdentAllowed(const Player* player, const char* sid);
 
 	/// Check if the upgrade is researched.
-extern int UpgradeIdentAvailable(const Player* player,const char* ident);
-
-/*----------------------------------------------------------------------------
---		eof
-----------------------------------------------------------------------------*/
+extern int UpgradeIdentAvailable(const Player* player, const char* ident);
 
 //@}
 
-#endif		// !__UPGRADE_H__
+#endif  // !__UPGRADE_H__
