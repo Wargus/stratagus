f6e1f7f6d58c33e349fb1eb8aedf5b377a7b6cdf
jsalmon3 <>
2004-01-21 20:01:31 +0000

    Fixed shadows

--- stratagus/construct.cpp
+++ stratagus/construct.cpp
@@ -5,12 +5,12 @@
 //     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
 //             \/                  \/          \//_____/            \/
 //  ______________________                           ______________________
-//			  T H E   W A R   B E G I N S
-//	   Stratagus - A free fantasy real time strategy game engine
+//                        T H E   W A R   B E G I N S
+//         Stratagus - A free fantasy real time strategy game engine
 //
-/**@name construct.c	-	The constructions. */
+/**@name construct.c - The constructions. */
 //
-//	(c) Copyright 1998-2003 by Lutz Sammer and Jimmy Salmon
+//      (c) Copyright 1998-2004 by Lutz Sammer and Jimmy Salmon
 //
 //      This program is free software; you can redistribute it and/or modify
 //      it under the terms of the GNU General Public License as published by
@@ -26,12 +26,12 @@
 //      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //      02111-1307, USA.
 //
-//	$Id$
+//      $Id$
 
 //@{
 
 /*----------------------------------------------------------------------------
---		Includes
+--  Includes
 ----------------------------------------------------------------------------*/
 
 #include <stdio.h>
@@ -46,7 +46,7 @@
 #include "script.h"
 
 /*----------------------------------------------------------------------------
---		Variables
+--  Variables
 ----------------------------------------------------------------------------*/
 
 /**
@@ -129,6 +129,7 @@ global void LoadConstructions(void)
 				(*cop)->ShadowSprite = LoadSprite(file,
 					(*cop)->ShadowWidth, (*cop)->ShadowHeight);
 				FlipGraphic((*cop)->ShadowSprite);
+				MakeShadowSprite((*cop)->ShadowSprite);
 			}
 			++cop;
 		}
