bbdb1c7c7d35aadb485ffe4d193bd21683daa266
nobody_ <>
2004-02-01 19:00:25 +0000

    More docs. Fix some stdout junk.

--- pathfinder/splitter_zoneset.cpp
+++ pathfinder/splitter_zoneset.cpp
@@ -398,7 +398,7 @@ global int ZoneSetFindPath(ZoneSet* src,ZoneSet* dst,int * path,int * pathlen)
 		}
 		curdst++;
 		if (!shouldcontinue) {
-			DebugLevel2Fn("No path between zones.\n");
+			DebugLevel3Fn("No path between zones.\n");
 			return 0;
 		}
 	}
