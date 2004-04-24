c147fe4f1a6f7f39b1dfd56d273d8910861286ae
jsalmon3 <>
2004-04-24 03:00:58 +0000

    Fixed license

--- src/action/action_move.cpp
+++ src/action/action_move.cpp
@@ -12,14 +12,19 @@
 //
 //      (c) Copyright 1998-2004 by Lutz Sammer
 //
-//      Stratagus is free software; you can redistribute it and/or modify
-//      it under the terms of the GNU General Public License as published
-//      by the Free Software Foundation; either only 2 of the License.
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; only version 2 of the License.
 //
-//      Stratagus is distributed in the hope that it will be useful,
+//      This program is distributed in the hope that it will be useful,
 //      but WITHOUT ANY WARRANTY; without even the implied warranty of
 //      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //      GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//      02111-1307, USA.
 //
 //      $Id$
 
