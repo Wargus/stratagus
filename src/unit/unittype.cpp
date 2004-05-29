5a29400dafac40e6a8a79679f58bd5934bec39f8
jarod42 <>
2004-05-29 15:50:41 +0000

    Add DefineVariables (with associated stuff) and DefineDecorations

--- src/unit/unittype.cpp
+++ src/unit/unittype.cpp
@@ -927,6 +927,7 @@ global void CleanUnitTypes(void)
 		Assert(type->Name);
 		free(type->Name);
 
+		free(type->Variable);
 		free(type->BoolFlag);
 		free(type->CanTargetFlag);
 
@@ -1016,12 +1017,17 @@ global void CleanUnitTypes(void)
 	}
 	NumUnitTypes = 0;
 
-	for (i = 0; i < NumberBoolFlag; i++) { // User defined flags
-		free(BoolFlagName[i]);
+	for (i = 0; i < UnitTypeVar.NumberBoolFlag; i++) { // User defined flags
+		free(UnitTypeVar.BoolFlagName[i]);
 	}
-	free(BoolFlagName);
-	BoolFlagName = NULL;
-	NumberBoolFlag = 0;
+	for (i = 0; i < UnitTypeVar.NumberVariable; i++) { // User defined variables
+		free(UnitTypeVar.VariableName[i]);
+	}
+	free(UnitTypeVar.BoolFlagName);
+	free(UnitTypeVar.VariableName);
+	free(UnitTypeVar.Variable);
+	free(UnitTypeVar.DecoVar);
+	memset(&UnitTypeVar, 0, sizeof (UnitTypeVar));
 
 	//
 	// Clean hardcoded unit types.
