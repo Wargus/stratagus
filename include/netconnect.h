a5b139b79b8075db390a042b470acf2fc225d180
jsalmon3 <>
2004-06-26 19:34:38 +0000

    Bump version for 2.1pre2

--- include/netconnect.h
+++ include/netconnect.h
@@ -48,7 +48,7 @@
 	/// Network protocol minor version (maximal 99)
 #define NetworkProtocolMinorVersion 9
 	/// Network protocol patch level (maximal 99)
-#define NetworkProtocolPatchLevel   1
+#define NetworkProtocolPatchLevel   2
 	/// Network protocol version (1,2,3) -> 10203
 #define NetworkProtocolVersion \
 	(NetworkProtocolMajorVersion * 10000 + NetworkProtocolMinorVersion * 100 \
