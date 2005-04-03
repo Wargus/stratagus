60705ba7640782bc8dd67441590ec15e7926cd9a
jsalmon3 <>
2005-04-03 16:15:33 +0000

    Added $Id$

--- src/metaserver/cmd.h
+++ src/metaserver/cmd.h
@@ -25,6 +25,8 @@
 //      along with this program; if not, write to the Free Software
 //      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //      02111-1307, USA.
+//
+//      $Id$
 
 #ifndef __CMD_H__
 #define __CMD_H__
