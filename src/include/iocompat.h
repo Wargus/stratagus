a3a60687deb428b183edf69e99082bc80c13af4e
jsalmon3 <>
2005-06-05 12:21:24 +0000

    Fixed VC++ compile error

--- src/include/iocompat.h
+++ src/include/iocompat.h
@@ -10,7 +10,7 @@
 //
 /**@name iocompat.h - IO platform compatibility header file. */
 //
-//      (c) Copyright 2002-2004 by Andreas Arens
+//      (c) Copyright 2002-2005 by Andreas Arens
 //
 //      This program is free software; you can redistribute it and/or modify
 //      it under the terms of the GNU General Public License as published by
@@ -55,6 +55,7 @@
 #include <sys/stat.h>
 #include <direct.h>
 #include <string.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <io.h>
 #include <fcntl.h>
