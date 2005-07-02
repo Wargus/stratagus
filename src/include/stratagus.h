26e0d0b12ae3ccdfd3442b9aef4f49f8baaf5e3a
feber <>
2005-07-02 15:20:35 +0000

    We're working on version 2.2

--- src/include/stratagus.h
+++ src/include/stratagus.h
@@ -176,7 +176,7 @@ typedef struct _missile_type_ MissileType;
 	/// Stratagus major version
 #define StratagusMajorVersion  2
 	/// Stratagus minor version (maximal 99)
-#define StratagusMinorVersion  1
+#define StratagusMinorVersion  2
 	/// Stratagus patch level (maximal 99)
 #define StratagusPatchLevel    90
 	/// Stratagus version (1,2,3) -> 10203
