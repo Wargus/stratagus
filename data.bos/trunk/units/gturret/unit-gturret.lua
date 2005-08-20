49f8293cb9c7d043579568d489ade5c9139fe0cb
feb <>
2005-08-20 17:08:20 +0000

    New Gun Turret graphics (Loïs Taulelle, François Beerten, TimberDragon)

--- data.bos/trunk/units/gturret/unit-gturret.lua
+++ data.bos/trunk/units/gturret/unit-gturret.lua
@@ -1,11 +1,12 @@
---            ____            
---           / __ )____  _____
---          / __  / __ \/ ___/
---         / /_/ / /_/ (__  ) 
---        /_____/\____/____/  
---
---  Invasion - Battle of Survival                  
---   A GPL'd futuristic RTS game
+--       _________ __                 __                               
+--      /   _____//  |_____________ _/  |______     ____  __ __  ______
+--      \_____  \\   __\_  __ \__  \\   __\__  \   / ___\|  |  \/  ___/
+--      /        \|  |  |  | \// __ \|  |  / __ \_/ /_/  >  |  /\___ \ 
+--     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
+--             \/                  \/          \//_____/            \/ 
+--  ______________________                           ______________________
+--			  T H E   W A R   B E G I N S
+--	   Stratagus - A free fantasy real time strategy game engine
 --
 --	unit-gturret.lua	-	Define the gun turret unit.
 --
@@ -28,11 +29,9 @@
 --	$Id$
 
 DefineAnimations("animations-gturret", {
-    Still = {"frame 4", "wait 1", },
-    Attack = {"unbreakable begin", "frame 0", "wait 1", "frame 5", "wait 1",
-        "frame 0", "wait 1", "frame 5", "wait 1", "frame 0", "wait 1", 
-        "frame 5", "sound gturret-attack", "attack", "wait 1", 
-        "frame 0", "unbreakable end", "wait 1", },
+    Still = {"frame 5", "wait 1", },
+    Attack = {"unbreakable begin", "frame 10", "wait 1", "frame 15", "sound gturret-attack", "attack", "wait 1",
+        "frame 20", "unbreakable end", "wait 1", },
     Death = {"unbreakable begin", "frame 10", "unbreakable end", "wait 50", },
     })
 
@@ -46,38 +45,40 @@ DefineIcon({
 	Name = "icon-gturret_b",
 	Size = {46, 38},
 	Frame = 10,
-	File = "elites/units/engineer_icons.png"})
+	File = "units/gturret/gturret_i.png"})
 
 DefineConstruction("construction-gturret", {
 	Constructions = {
-		{Percent = 0, File = "main", Frame = 10},
-		{Percent = 20, File = "main", Frame = 11},
-		{Percent = 40, File = "main", Frame = 12},
-		{Percent = 60, File = "main", Frame = 13},
-		{Percent = 80, File = "main", Frame = 14}
+		{Percent = 0, File = "main", Frame = 0},
+		{Percent = 10, File = "main", Frame = 0},
+		{Percent = 20, File = "main", Frame = 1},
+		{Percent = 30, File = "main", Frame = 2},
+		{Percent = 40, File = "main", Frame = 3},
+		{Percent = 50, File = "main", Frame = 4},
+		{Percent = 60, File = "main", Frame = 5},
+		{Percent = 70, File = "main", Frame = 6},
+		{Percent = 80, File = "main", Frame = 7},
+		{Percent = 90, File = "main", Frame = 7}
 	}
 })
 
 DefineUnitType("unit-gturret", {
 	Name = "Gun Turret",
-	Image = {"file", "units/gturret/gturret.png", "size", {64, 64}},
+	Image = {"file", "units/gturret/gturret.png", "size", {96, 96}},
+	Shadow = {"file", "units/gturret/gturret_s.png", "size", {96, 96}},
 	Animations = "animations-gturret", Icon = "icon-gturret",
-	Costs = {"time", 130, "titanium", 250, "crystal", 50},
+	Costs = {"time", 130, "titanium", 250, "crystal", 50}, 
 	RepairHp = 2, RepairCosts = {"titanium", 2}, Construction = "construction-gturret",
 	Speed = 0, HitPoints = 160, DrawLevel = 25, TileSize  = {2, 2}, BoxSize = {60, 60},
 	SightRange = 6, ComputerReactionRange = 6, PersonReactionRange = 6, Armor = 10,
 	BasicDamage = 4, PiercingDamage = 0, MaxAttackRange = 6, Missile = "missile-none",
 	Priority = 20, AnnoyComputerFactor = 45, Points = 100,
 	ExplodeWhenKilled = "missile-160x128-explosion", RightMouseAction = "attack",
-	CanAttack = true, CanTargetLand = true, NumDirections = 8,
+	CanAttack = true, CanTargetLand = true, NumDirections = 8, Flip = false,
 	Corpse = {"build-dead-body2", 0}, Type = "land",
 	--[[MustBuildOnTop = "unit-plate1", --]] Building = true, BuilderOutside = true,
 	VisibleUnderFog = true,
-	Sounds = {
-		"selected", "gturret-selected",
-		"ready", "gen-ready",
-		"help", "gen-help",
-		"dead", "gen-dead"}
+	Sounds = {"selected", "gturret-selected",}
 })
 
 MakeSound("gturret-selected", "units/gturret/gturret_select.wav")
