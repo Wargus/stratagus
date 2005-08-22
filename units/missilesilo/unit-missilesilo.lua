74b36eb56067c57d8c227f9d1f7a937f7eaf52b6
feb <>
2005-08-22 21:39:40 +0000

    New Missile Silo graphics (p0ng, TimberDragon)

--- units/missilesilo/unit-missilesilo.lua
+++ units/missilesilo/unit-missilesilo.lua
@@ -35,57 +35,52 @@ DefineIcon({
 
 DefineConstruction("construction-msilo", {
 	Constructions = {
-		{Percent = 0, File = "main", Frame = 7},
-		{Percent = 10, File = "main", Frame = 7},
-		{Percent = 20, File = "main", Frame = 8},
-		{Percent = 30, File = "main", Frame = 9},
-		{Percent = 40, File = "main", Frame = 10},
-		{Percent = 50, File = "main", Frame = 11},
-		{Percent = 60, File = "main", Frame = 11},
-		{Percent = 70, File = "main", Frame = 12},
-		{Percent = 80, File = "main", Frame = 13},
-		{Percent = 90, File = "main", Frame = 0}
+		{Percent = 0, File = "main", Frame = 0},
+		{Percent = 10, File = "main", Frame = 1},
+		{Percent = 20, File = "main", Frame = 2},
+		{Percent = 30, File = "main", Frame = 3},
+		{Percent = 40, File = "main", Frame = 4},
+		{Percent = 50, File = "main", Frame = 5},
+		{Percent = 60, File = "main", Frame = 6},
+		{Percent = 70, File = "main", Frame = 7},
+		{Percent = 80, File = "main", Frame = 8},
+		{Percent = 90, File = "main", Frame = 9}
 	}
 })
 DefineAnimations("animations-msilo", {
-    Still = {"frame 0", "wait 1", },
-    Attack = {"unbreakable begin", "frame 1", "wait 1", 
-        "frame 2", "sound msilo-attack", "attack", "wait 1", 
-        "frame 3", "wait 1", "frame 4", "wait 1", "frame 5", "wait 25", 
-        "frame 6", "sound msilo-attack", "attack", "wait 25", 
-        "frame 6", "unbreakable end", "wait 1", },
+    Still = {"frame 15", "wait 5", "frame 16", "wait 5", "frame 17", "wait 5",
+             "frame 18", "wait 5", "frame 19", "wait 5", },
+    Train = {"frame 15", "wait 5", "frame 16", },
+    Attack = {"unbreakable begin", "frame 11", "wait 4",
+        "frame 12", "sound msilo-attack", "wait 2", "attack",
+        "frame 13", "wait 2", "frame 14", "wait 10", "frame 15", 
+        "unbreakable end", "wait 25", },
     })
 DefineUnitType("unit-msilo", {
 	Name = "Missile Silo",
-	Image = {"file", GetCurrentLuaPath().."/missile_silo.png", "size", {128, 128}},
-	Shadow = {"file", GetCurrentLuaPath().."/missile_silo_s.png", "size", {128, 128}},
+	Image = {"file", GetCurrentLuaPath().."/missile_silo.png", "size", {256, 256}},
+	Shadow = {"file", GetCurrentLuaPath().."/missile_silo_s.png", "size", {256, 256}},
 	Animations = "animations-msilo", Icon = "icon-msilo",
 	Costs = {"time", 2000, "titanium", 10000, "crystal", 10000},
 	RepairHp = 2, RepairCosts = {"titanium", 2},
 	Construction = "construction-msilo",
 	Speed = 0, HitPoints = 450, DrawLevel = 25, 
-	TileSize = {4, 4}, BoxSize = {124, 124},
-	SightRange = 1, Armor = 10, BasicDamage = 0, PiercingDamage = 0,
+	TileSize = {5, 5}, BoxSize = {168, 168},
+	SightRange = 3, Armor = 10, BasicDamage = 0, PiercingDamage = 0,
 	Missile = "missile-none", Priority = 20, AnnoyComputerFactor = 45,
 	Points = 100, ExplodeWhenKilled = "missile-160x128-explosion",
 	Corpse = {"build-dead-body4", 0}, Type = land,
 	MaxMana = 1000, CanCastSpell = {"spell-nuke"},
 	Demand = 400, Building = true, BuilderOutside = true,
 	VisibleUnderFog = true,
-	Sounds = {
-		"selected", "gen-selected",
-		"ready", "gen-ready",
-		"help", "gen-help",
-		"dead", "gen-dead"}
 	})
 DefineAnimations("animations-elitebuild7", {
-    Death = {"unbreakable begin", "wait 1", "frame 14", "wait 2000", 
-        "frame 14", "wait 200", "frame 14", "wait 200", "frame 15", "wait 200",
-        "frame 15", "wait 200", "frame 15", "wait 1", "unbreakable end", "wait 1", },
+    Death = {"unbreakable begin", "wait 1", "frame 1", "wait 2000", 
+             "unbreakable end", "wait 1", },
     })
 DefineUnitType("build-dead-body7", {
 	Name = "SiloCrater",
-	Image = {"file", GetCurrentLuaPath().."/missile_silo.png", "size", {128, 128}},
+	Image = {"file", GetCurrentLuaPath().."/missile_silo.png", "size", {256, 256}},
 	Animations = "animations-elitebuild7", Icon = "icon-cancel",
 	Speed = 0, HitPoints = 999, DrawLevel = 10,	TileSize = {4, 4},
 	BoxSize = {124, 124}, SightRange = 1, BasicDamage = 0,
@@ -93,3 +88,4 @@ DefineUnitType("build-dead-body7", {
 	Type = "land" , Building = true, Vanishes = true})
 
 DefineAllow("unit-msilo", "AAAAAAAAAAAAAAAA")
+
