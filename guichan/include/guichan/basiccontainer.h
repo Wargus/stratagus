4ac776e2e61ac55ed6e35ff5050859437d52276d
jsalmon3 <>
2005-10-25 02:33:43 +0000

    Added *.h

---
+++ guichan/include/guichan/basiccontainer.h
@@ -0,0 +1,119 @@
+/*      _______   __   __   __   ______   __   __   _______   __   __                 
+ *     / _____/\ / /\ / /\ / /\ / ____/\ / /\ / /\ / ___  /\ /  |\/ /\                
+ *    / /\____\// / // / // / // /\___\// /_// / // /\_/ / // , |/ / /                 
+ *   / / /__   / / // / // / // / /    / ___  / // ___  / // /| ' / /                  
+ *  / /_// /\ / /_// / // / // /_/_   / / // / // /\_/ / // / |  / /                   
+ * /______/ //______/ //_/ //_____/\ /_/ //_/ //_/ //_/ //_/ /|_/ /                    
+ * \______\/ \______\/ \_\/ \_____\/ \_\/ \_\/ \_\/ \_\/ \_\/ \_\/                      
+ *
+ * Copyright (c) 2004, 2005 darkbits                        Js_./
+ * Per Larsson a.k.a finalman                          _RqZ{a<^_aa
+ * Olof Naessén a.k.a jansem/yakslem                _asww7!uY`>  )\a//
+ *                                                 _Qhm`] _f "'c  1!5m
+ * Visit: http://guichan.darkbits.org             )Qk<P ` _: :+' .'  "{[
+ *                                               .)j(] .d_/ '-(  P .   S
+ * License: (BSD)                                <Td/Z <fP"5(\"??"\a.  .L
+ * Redistribution and use in source and          _dV>ws?a-?'      ._/L  #'
+ * binary forms, with or without                 )4d[#7r, .   '     )d`)[
+ * modification, are permitted provided         _Q-5'5W..j/?'   -?!\)cam'
+ * that the following conditions are met:       j<<WP+k/);.        _W=j f
+ * 1. Redistributions of source code must       .$%w\/]Q  . ."'  .  mj$
+ *    retain the above copyright notice,        ]E.pYY(Q]>.   a     J@\
+ *    this list of conditions and the           j(]1u<sE"L,. .   ./^ ]{a
+ *    following disclaimer.                     4'_uomm\.  )L);-4     (3=
+ * 2. Redistributions in binary form must        )_]X{Z('a_"a7'<a"a,  ]"[
+ *    reproduce the above copyright notice,       #}<]m7`Za??4,P-"'7. ).m
+ *    this list of conditions and the            ]d2e)Q(<Q(  ?94   b-  LQ/
+ *    following disclaimer in the                <B!</]C)d_, '(<' .f. =C+m
+ *    documentation and/or other materials      .Z!=J ]e []('-4f _ ) -.)m]'
+ *    provided with the distribution.          .w[5]' _[ /.)_-"+?   _/ <W"
+ * 3. Neither the name of Guichan nor the      :$we` _! + _/ .        j?
+ *    names of its contributors may be used     =3)= _f  (_yQmWW$#(    "
+ *    to endorse or promote products derived     -   W,  sQQQQmZQ#Wwa]..
+ *    from this software without specific        (js, \[QQW$QWW#?!V"".
+ *    prior written permission.                    ]y:.<\..          .
+ *                                                 -]n w/ '         [.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT       )/ )/           !
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY         <  (; sac    ,    '
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING,               ]^ .-  %
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF            c <   r
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR            aga<  <La
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE          5%  )P'-3L
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR        _bQf` y`..)a
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,          ,J?4P'.P"_(\?d'.,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES               _Pa,)!f/<[]/  ?"
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT      _2-..:. .r+_,.. .
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,     ?a.<%"'  " -'.a_ _,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION)                     ^
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef GCN_BASICCONTAINER_HPP
+#define GCN_BASICCONTAINER_HPP
+
+#include <list>
+
+#include "guichan/platform.h"
+#include "guichan/graphics.h"
+#include "guichan/widget.h"
+
+namespace gcn
+{
+    /**
+     * Containers base class. Contains basic container functions every
+     * container should have. Containers should inherit from this class and
+     * implement it's functions.
+     *
+     * @see Container
+     */
+    class GCN_CORE_DECLSPEC BasicContainer: public Widget
+    {
+    public:
+
+        virtual ~BasicContainer() { }
+
+        /**
+         * Moves a Widget to the top of the BasicContainer. The widget will be
+         * drawn above all other Widgets in the BasicContainer.
+         *
+         * @param widget the Widget to move.
+         */
+        virtual void moveToTop(Widget* widget) = 0;
+
+        /**
+         * Moves a widget to the bottom of the BasicContainer. The Widget will
+         * be drawn below all other Widgets in the BasicContainer.
+         *
+         * @param widget the Widget to move.
+         */
+        virtual void moveToBottom(Widget* widget) = 0;
+
+        /**
+         * Gets the drawing space size a Widget has in the BasicContainer.
+         * It may not be the same size as the Widgets width and height.
+         *
+         * NOTE: Size is not checked recursively all the way back to the
+         *       top Widget. If the BasicContainer itself is clipped,
+         *       the size may be inaccurate.
+         * 
+         * @param width the width the Widget's draw space has.
+         * @param height the height the Widget's draw space has.
+         * @param widget the Widget calling the function.
+         */
+        virtual void getDrawSize(int& width, int& height, Widget* widget) = 0;
+
+        /**
+         * Called when a child of the BasicContainer gets destroyed.
+         *
+         * @param widget the destroyed Widget.
+         */
+        virtual void _announceDeath(Widget *widget) = 0;
+    
+    };  
+}
+
+#endif // end GCN_BASICCONTAINER_HPP
