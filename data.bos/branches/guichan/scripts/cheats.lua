e19f2016266d4a4c8a776fa23eb92d8a8d968336
feb <>
2005-11-01 13:11:35 +0000

    Branch the main Bos trunk in a guichan branch.

---
+++ data.bos/branches/guichan/scripts/cheats.lua
@@ -0,0 +1,137 @@
+--            ____            
+--           / __ )____  _____
+--          / __  / __ \/ ___/
+--         / /_/ / /_/ (__  ) 
+--        /_____/\____/____/  
+--
+--  Invasion - Battle of Survival                  
+--   A GPL'd futuristic RTS game
+--
+--      wc2.lua - WC2 compatibility level
+--
+--      (c) Copyright 2001-2004 by Lutz Sammer and Jimmy Salmon
+--
+--      This program is free software; you can redistribute it and/or modify
+--      it under the terms of the GNU General Public License as published by
+--      the Free Software Foundation; either version 2 of the License, or
+--      (at your option) any later version.
+--  
+--      This program is distributed in the hope that it will be useful,
+--      but WITHOUT ANY WARRANTY; without even the implied warranty of
+--      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--      GNU General Public License for more details.
+--  
+--      You should have received a copy of the GNU General Public License
+--      along with this program; if not, write to the Free Software
+--      Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+--
+--      $Id$
+
+speedcheat = false
+godcheat = false
+
+function HandleCheats(str)
+  local resources = { "titanium", "crystal" }
+
+  if (str == "there is no aliens level") then
+    -- FIXME: no function yet
+    AddMessage("cheat enabled")
+
+  elseif (str == "glittering prizes") then
+    SetPlayerData(GetThisPlayer(), "Resources", "titanium",
+      GetPlayerData(GetThisPlayer(), "Resources", "titanium") + 12000)
+    SetPlayerData(GetThisPlayer(), "Resources", "crystal",
+      GetPlayerData(GetThisPlayer(), "Resources", "crystal") + 5000)
+    AddMessage("!!! :)")
+
+  elseif (str == "on screen") then
+    RevealMap()
+
+  elseif (str == "showpath") then
+    RevealMap()
+
+  elseif (str == "fow on") then
+    SetFogOfWar(true)
+
+  elseif (str == "fow off") then
+    SetFogOfWar(false)
+
+  elseif (str == "fast debug") then
+    for i = 1,table.getn(resources) do
+      SetSpeedResourcesHarvest(resources[i], 10)
+      SetSpeedResourcesReturn(resources[i], 10)
+    end
+    SetSpeedBuild(10)
+    SetSpeedTrain(10)
+    SetSpeedUpgrade(10)
+    SetSpeedResearch(10)
+    AddMessage("FAST DEBUG SPEED")
+
+  elseif (str == "normal debug") then
+    for i = 1,table.getn(resources) do
+      SetSpeedResourcesHarvest(resources[i], 1)
+      SetSpeedResourcesReturn(resources[i], 1)
+    end
+    SetSpeedBuild(1)
+    SetSpeedTrain(1)
+    SetSpeedUpgrade(1)
+    SetSpeedResearch(1)
+    AddMessage("NORMAL DEBUG SPEED")
+
+  elseif (str == "make it so") then
+    if (speedcheat) then
+      speedcheat = false
+      for i = 1,table.getn(resources) do
+        SetSpeedResourcesHarvest(resources[i], 1)
+        SetSpeedResourcesReturn(resources[i], 1)
+      end
+      SetSpeedBuild(1)
+      SetSpeedTrain(1)
+      SetSpeedUpgrade(1)
+      SetSpeedResearch(1)
+      AddMessage("NO SO!")
+    else
+      speedcheat = true
+      for i = 1,table.getn(resources) do
+        SetSpeedResourcesHarvest(resources[i], 10)
+        SetSpeedResourcesReturn(resources[i], 10)
+      end
+      SetSpeedBuild(10)
+      SetSpeedTrain(10)
+      SetSpeedUpgrade(10)
+      SetSpeedResearch(10)
+      for i = 1,table.getn(resources) do
+        SetPlayerData(GetThisPlayer(), "Resources", resources[i],
+          GetPlayerData(GetThisPlayer(), "Resources", resources[i]) + 32000)
+      end
+      AddMessage("SO!")
+    end
+
+  elseif (str == "unite the clans") then
+    ActionVictory()
+
+  elseif (str == "you pitiful worm") then
+    ActionDefeat()
+
+  elseif (str == "it is a good day to die") then
+    if (godcheat) then
+      godcheat = false
+      SetGodMode(false)
+      AddMessage("God Mode OFF")
+    else
+      godcheat = true
+      SetGodMode(true)
+      AddMessage("God Mode ON")
+    end
+
+  elseif (str == "fill mana") then
+    t = GetUnits("this");
+    for i = 1,table.getn(t) do
+      SetUnitMana(t[i], 255)
+    end
+
+  else
+    return false
+  end
+  return true
+end
---
+++ data.bos/branches/guichan/units/radar/unit-radar.lua
@@ -0,0 +1,97 @@
+--            ____            
+--           / __ )____  _____
+--          / __  / __ \/ ___/
+--         / /_/ / /_/ (__  ) 
+--        /_____/\____/____/  
+--
+--  Invasion - Battle of Survival                  
+--   A GPL'd futuristic RTS game
+--
+--	radar.lua	-	Define the radar unit.
+--
+--	(c) Copyright 2004-2005 by François Beerten.
+--
+--      This program is free software; you can redistribute it and/or modify
+--      it under the terms of the GNU General Public License as published by
+--      the Free Software Foundation; either version 2 of the License, or
+--      (at your option) any later version.
+--  
+--      This program is distributed in the hope that it will be useful,
+--      but WITHOUT ANY WARRANTY; without even the implied warranty of
+--      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--      GNU General Public License for more details.
+--  
+--      You should have received a copy of the GNU General Public License
+--      along with this program; if not, write to the Free Software
+--      Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+--
+--	$Id$
+
+DefineAnimations("animations-radar", {
+    Still = {"frame 0", "wait 4", "frame 1", "wait 4", "frame 2", "wait 4",
+        "frame 3", "wait 4", "frame 4", "wait 4", "frame 5", "wait 4", 
+        "frame 6", "wait 4", "frame 7", "wait 4", },
+    Death = {"unbreakable begin", "frame 0", "wait 1", "frame 1", "wait 10",
+        "frame 1", "unbreakable end", "wait 10", },
+    })
+
+DefineIcon({
+	Name = "icon-radar",
+	Size = {46, 38},
+	Frame = 0,
+	File = "units/radar/radar_i.png"})
+
+DefineIcon({
+	Name = "icon-radar_b",
+	Size = {46, 38},
+	Frame = 0,
+	File = "units/radar/radar_b.png"})
+
+MakeSound("radar-selected", GetCurrentLuaPath().."/sfx_rdar.select.wav")
+
+DefineConstruction("construction-radar", {
+	Constructions = {
+		{Percent = 0, File = "main", Frame = 11},
+		{Percent = 50, File = "main", Frame = 10},
+	}
+})
+
+DefineUnitType("unit-radar", {
+	Name = "Radar",
+	Image = {"file", "units/radar/radar.png", "size", {64, 64}},
+	Offset = {12, -16},
+	Shadow = {"file", "units/radar/radar_s.png", "size", {64, 64}},
+	Animations = "animations-radar", Icon = "icon-radar",
+	Costs = {"time", 20, "titanium", 7, "crystal", 35},
+	RepairHp = 1, RepairCosts = {"crystal", 3}, Construction = "construction-radar",
+	Speed = 0, HitPoints = 5, DrawLevel = 25, TileSize  = {1, 1}, BoxSize = {32, 28},
+	SightRange = 3, Armor = 0 , BasicDamage = 0, PiercingDamage = 0,
+	Missile = "missile-none", Priority = 20, AnnoyComputerFactor = 65,
+	Points = 10, Supply = 0, ExplodeWhenKilled = "missile-64x64-explosion",
+	Corpse = {"radar_destroyed", 0}, Type = "land",
+	Building = true, BuilderOutside = true,
+	VisibleUnderFog = true,
+	NumDirections = 1,
+	RadarRange = 40,
+	Sounds = {"selected", "radar-selected"}
+})
+
+DefineUnitType("radar_destroyed", {
+	Name = "RadarWreck",
+	Image = {"file", "units/radar/radar.png", "size", {64, 64}},
+	Animations = "animations-radar", Icon = "icon-cancel",
+	Speed = 0, HitPoints = 999, DrawLevel = 10,
+	TileSize = {1, 1}, BoxSize = {28, 28}, SightRange = 1,
+	BasicDamage = 0, PiercingDamage = 0, Missile = "missile-none",
+	Priority = 0, Type = "land", Building = true, Vanishes = true
+})
+
+DefineAllow("unit-radar", "AAAAAAAAAAAAAAAA")
+
+DefineButton({
+	Pos = 4, Level = 1, Icon = "icon-radar_b", Action = "build",
+	Value = "unit-radar", Key = "r", Hint = "BUILD ~!RADAR",
+	ForUnit = {"unit-engineer"}})
+
+
+
