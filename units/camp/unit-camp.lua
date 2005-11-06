13b90dc422027774ae521691c589a77ab6024bba
gruiick <>
2005-11-06 16:44:52 +0000

    New Camp graphics (Timberdragon, Loïs Taulelle)

--- units/camp/unit-camp.lua
+++ units/camp/unit-camp.lua
@@ -36,21 +36,24 @@ DefineIcon({
 
 DefineConstruction("construction-camp", {
 	Constructions = {
-		{Percent = 0, File = "main", Frame = 5},
-		{Percent = 10, File = "main", Frame = 6},
-		{Percent = 20, File = "main", Frame = 7},
-		{Percent = 30, File = "main", Frame = 8},
-		{Percent = 40, File = "main", Frame = 9},
-		{Percent = 50, File = "main", Frame = 10},
-		{Percent = 60, File = "main", Frame = 11},
-		{Percent = 70, File = "main", Frame = 12},
-		{Percent = 80, File = "main", Frame = 13},
-		{Percent = 90, File = "main", Frame = 14}
+		{Percent = 0, File = "main", Frame = 0},
+		{Percent = 10, File = "main", Frame = 1},
+		{Percent = 20, File = "main", Frame = 2},
+		{Percent = 30, File = "main", Frame = 3},
+		{Percent = 40, File = "main", Frame = 4},
+		{Percent = 50, File = "main", Frame = 6},
+		{Percent = 60, File = "main", Frame = 7},
+		{Percent = 70, File = "main", Frame = 8},
+		{Percent = 80, File = "main", Frame = 9},
+		{Percent = 90, File = "main", Frame = 10}
 	}
     })
 
 DefineAnimations("animations-camp", {
-    Still = {"frame 0", "wait 3", },
+    Still = {"frame 10", "wait 10", "frame 11", "wait 10", "frame 12", "wait 10",
+             "frame 13", "wait 10", "frame 14", "wait 10",},
+    Train = {"frame 15", "wait 3", "frame 16", "wait 5", "frame 17", "wait 5",
+             "frame 18", "wait 5", "frame 19", "wait 3", },
     })
 
 MakeSound("camp-selected", GetCurrentLuaPath().."/sfx_camp.select.wav")
@@ -60,17 +63,17 @@ MakeSound("camp-dead", GetCurrentLuaPath().."/sfx_camp.die.wav")
 
 DefineUnitType("unit-camp", {
 	Name = "Training Camp",
-	Image = {"file", GetCurrentLuaPath().."/training_camp.png", "size", {160, 128}},
-	Shadow = {"file", GetCurrentLuaPath().."/training_camp_s.png", "size", {160, 128}},
+	Image = {"file", GetCurrentLuaPath().."/training_camp.png", "size", {256, 256}},
+	Shadow = {"file", GetCurrentLuaPath().."/training_camp_s.png", "size", {256, 256}},
 	Animations = "animations-camp", Icon = "icon-camp",
 	Costs = {"time", 100, "titanium", 300, "crystal", 150},
 	RepairHp = 2, RepairCosts = {"titanium", 2},
 	Construction = "construction-camp", Speed = 0, HitPoints = 500,
-	DrawLevel = 25, Demand = 125, TileSize = {5, 4}, BoxSize = {156, 124},
+	DrawLevel = 25, Demand = 125, TileSize = {7, 5}, BoxSize = {220, 156},
 	SightRange = 1, Armor = 25, BasicDamage = 0, PiercingDamage = 0,
 	Missile = "missile-none", Priority = 30, AnnoyComputerFactor = 35,
 	Points = 160, ExplodeWhenKilled = "missile-160x128-explosion",
-	Corpse = {"build-dead-body3", 0}, Type = "land",
+	Corpse = {"build-dead-camp", 0}, Type = "land",
 	Building = true, BuilderOutside = true, VisibleUnderFog = true,
 	Sounds = {
 		"selected", "camp-selected",
@@ -79,18 +82,18 @@ DefineUnitType("unit-camp", {
 		"dead", "camp-dead"}
 	})
 
-DefineAnimations("animations-elitebuild3", {
-    Death = {"unbreakable begin", "wait 1", "frame 15", "wait 2000", 
-        "frame 15", "wait 200", "frame 15", "wait 200", "frame 16", "wait 200",
-        "frame 16", "wait 200", "frame 16", "wait 1", "unbreakable end", "wait 1", },
+DefineAnimations("animations-dead-camp", {
+    Death = {"unbreakable begin", "wait 1", "frame 0", "wait 2000", 
+        "frame 0", "wait 200", "frame 0", "wait 200", "frame 0", "wait 200",
+        "frame 0", "wait 200", "frame 0", "wait 1", "unbreakable end", "wait 1", },
     })
 
-DefineUnitType("build-dead-body3", {
+DefineUnitType("build-dead-camp", {
 	Name = "CampCrater",
-	Image = {"file", GetCurrentLuaPath().."/training_camp.png", "size", {160, 128}},
-	Animations = "animations-elitebuild3", Icon = "icon-cancel",
+	Image = {"file", GetCurrentLuaPath().."/training_camp.png", "size", {256, 256}},
+	Animations = "animations-dead-camp", Icon = "icon-cancel",
 	Speed = 0, HitPoints = 999, DrawLevel = 10,
-	TileSize = {5, 4}, BoxSize = {156, 124}, SightRange = 1,
+	TileSize = {7, 5}, BoxSize = {220, 156}, SightRange = 1,
 	BasicDamage = 0, PiercingDamage = 0, Missile = "missile-none",
 	Priority = 0, Type = "land", Building = true, Vanishes = true
 	})
