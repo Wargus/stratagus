b154f3780713f04f84792c188e1dde23b86c29c7
jarod42 <>
2005-11-15 17:07:10 +0000

    Fixed crash when !SoundEnabled.().

--- src/sound/sound.cpp
+++ src/sound/sound.cpp
@@ -125,7 +125,7 @@ static CSample *ChooseSample(CSound *sound, bool selection, Origin &source)
 {
 	CSample *result = NULL;
 
-	if (!sound) {
+	if (!sound || !SoundEnabled()) {
 		return NULL;
 	}
 
