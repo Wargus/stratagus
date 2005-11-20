e7447648fe503a39ec4b511baeba73981a0183cf
jsalmon3 <>
2005-11-20 02:15:20 +0000

    Pass string by reference

--- stratagus/trunk/src/guichan/widgets/radiobutton.cpp
+++ stratagus/trunk/src/guichan/widgets/radiobutton.cpp
@@ -231,7 +231,7 @@ namespace gcn
         return mCaption;
     }
 
-    void RadioButton::setCaption(const std::string caption)
+    void RadioButton::setCaption(const std::string &caption)
     {
         mCaption = caption;
     }
