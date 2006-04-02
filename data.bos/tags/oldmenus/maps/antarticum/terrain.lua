6fb88b8eb5fe7bba12c40defe77c9b2494e24ae8
feb <>
2006-04-02 10:24:26 +0000

    Tag last version of BOS with stratagus old menus.

---
+++ data.bos/tags/oldmenus/maps/antarticum/terrain.lua
@@ -0,0 +1,31 @@
+--            ____            
+--           / __ )____  _____
+--          / __  / __ \/ ___/
+--         / /_/ / /_/ (__  ) 
+--        /_____/\____/____/  
+--
+--  Invasion - Battle of Survival                  
+--   A GPL'd futuristic RTS game
+--
+--	terrain.lua - Define terrrain
+--
+--      (c) Copyright 2005 by François Beerten.
+--
+--      This program is free software; you can redistribute it and/or modify
+--      it under the terms of the GNU General Public License as published by
+--      the Free Software Foundation; either version 2 of the License, or
+--      (at your option) any later version.
+--  
+--      This program is distributed in the hope that it will be useful,
+--      but WITHOUT ANY WARRANTY; without even the implied warranty of
+--      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--      GNU General Public License for more details.
+--  
+--      You should have received a copy of the GNU General Public License
+--      along with this program; if not, write to the Free Software
+--      Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+--
+-- $Id$
+
+DefineImageTerrain(GetCurrentLuaPath().."/terrain.png", 64, 64, 16, 16)
+
---
+++ data.bos/tags/oldmenus/scripts/helpers.lua
@@ -0,0 +1,63 @@
+--            ____            
+--           / __ )____  _____
+--          / __  / __ \/ ___/
+--         / /_/ / /_/ (__  ) 
+--        /_____/\____/____/  
+--
+--  Invasion - Battle of Survival                  
+--   A GPL'd futuristic RTS game
+--
+--	helpers.lua		-	Stratagus helper functions.
+--
+--	(c) Copyright 2004 by François Beerten
+--
+--      This program is free software; you can redistribute it and/or modify
+--      it under the terms of the GNU General Public License as published by
+--      the Free Software Foundation; either version 2 of the License, or
+--      (at your option) any later version.
+--  
+--      This program is distributed in the hope that it will be useful,
+--      but WITHOUT ANY WARRANTY; without even the implied warranty of
+--      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--      GNU General Public License for more details.
+--  
+--      You should have received a copy of the GNU General Public License
+--      along with this program; if not, write to the Free Software
+--      Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+--
+
+if(not helpersInited) then
+  helpersInited = true
+
+  --- 
+  -- Ai helper functions. These function are to be used in coroutines based
+  -- ai scripts.
+
+  OldAiWait=AiWait
+  function AiWait(unit)
+    while OldAiWait(unit) do
+      coroutine.yield()
+    end
+  end
+
+  OldAiSleep = AiSleep
+  function AiSleep(cycles)
+    while OldAiSleep(cycles) do
+      coroutine.yield()
+    end
+  end
+
+  OldAiWaitForce = AiWaitForce
+  function AiWaitForce(forceId)
+    while OldAiWaitForce(forceId) do
+      coroutine.yield()
+    end
+  end
+
+  OldAiTimedWaitForce = AiTimedWaitForce
+  function AiTimedWaitForce(forceId, maxCycles)
+    while OldAiTimedWaitForce(forceId, maxCycles) do
+      coroutine.yield()
+    end
+  end
+end
