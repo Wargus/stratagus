457630aedc7dae8406106e42afa2f97fda5ebb88
feber <>
2006-04-03 19:46:08 +0000

    Fix copyright year.

--- src/stratagus/stratagus.cpp
+++ src/stratagus/stratagus.cpp
@@ -232,7 +232,7 @@ char LocalPlayerName[16];            /// Name of local player
 
 	/// Name, Version, Copyright
 char NameLine[] =
-	"Stratagus V" VERSION ", (c) 1998-2005 by The Stratagus Project.";
+	"Stratagus V" VERSION ", (c) 1998-2006 by The Stratagus Project.";
 
 static char *MapName;                /// Filename of the map to load
 char *CompileOptions;                /// Compile options.
