4724475703c2065039830dd1bf1032e88874af02
jsalmon3 <>
2006-04-05 17:56:40 +0000

    Fixed rgb mask on little endian machines

--- video/graphic.cpp
+++ video/graphic.cpp
@@ -1031,8 +1031,22 @@ void CGraphic::Resize(int w, int h)
 		SDL_UnlockSurface(Surface);
 		SDL_FreeSurface(Surface);
 
+		int Rmask, Gmask, Bmask, Amask;
+		if (SDL_BYTEORDER == SDL_LIL_ENDIAN) {
+			Rmask = 0x000000FF;
+			Gmask = 0x0000FF00;
+			Bmask = 0x00FF0000;
+			Amask = (bpp == 4) ? 0xFF000000 : 0;
+		} else {
+			int s = (bpp == 4) ? 0 : 8;
+			Rmask = 0xFF000000 >> s;
+			Gmask = 0x00FF0000 >> s;
+			Bmask = 0x0000FF00 >> s;
+			Amask = 0x000000FF >> s;
+		}
+
 		Surface = SDL_CreateRGBSurfaceFrom(data, w, h, 8 * bpp, w * bpp,
-			RMASK, GMASK, BMASK, (bpp == 3 ? 0 : AMASK));
+			Rmask, Gmask, Bmask, Amask);
 	}
 
 	Width = GraphicWidth = w;
