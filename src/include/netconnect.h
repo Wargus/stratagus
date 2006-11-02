8be416b9d6d1cbe34f1fc90e857644044276a801
feber <>
2006-11-02 20:45:49 +0000

    Updated version number of the stratagus engine and network protocol.

--- src/include/netconnect.h
+++ src/include/netconnect.h
@@ -42,7 +42,7 @@
 	/// Network protocol minor version (maximal 99)
 #define NetworkProtocolMinorVersion 9
 	/// Network protocol patch level (maximal 99)
-#define NetworkProtocolPatchLevel   2
+#define NetworkProtocolPatchLevel   3
 	/// Network protocol version (1,2,3) -> 10203
 #define NetworkProtocolVersion \
 	(NetworkProtocolMajorVersion * 10000 + NetworkProtocolMinorVersion * 100 + \
