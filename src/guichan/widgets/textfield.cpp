675507e0309b599b52bdafc660770503fd8f32ca
jsalmon3 <>
2006-11-24 21:51:28 +0000

    Added support for pasting

--- src/guichan/widgets/textfield.cpp
+++ src/guichan/widgets/textfield.cpp
@@ -60,6 +60,8 @@
 #include "guichan/mouseinput.h"
 #include "guichan/widgets/textfield.h"
 
+static int GetClipboard(std::string &str);
+
 namespace gcn
 {
     TextField::TextField()
@@ -208,6 +210,16 @@ namespace gcn
             ret = true;
         }
 
+        else if (key.getValue() == 22)
+        {
+			std::string str;
+			if (GetClipboard(str) >= 0) {
+				for (size_t i = 0; i < str.size(); ++i) {
+					keyPress(Key(str[i]));
+				}
+			}
+        }
+
         else if (key.isCharacter())
         {
             mText.insert(mCaretPosition, std::string(1,(char)key.getValue()));
@@ -283,3 +295,105 @@ namespace gcn
         fixScroll();
     }
 }
+
+#ifdef USE_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#elif defined(_XLIB_H_)
+#include <X11/Xlib.h>
+#endif
+
+/**
+** Paste text from the clipboard
+*/
+static int GetClipboard(std::string &str)
+{
+#if defined(USE_WIN32) || defined(_XLIB_H_)
+	int i;
+	unsigned char *clipboard;
+#ifdef USE_WIN32
+	HGLOBAL handle;
+#elif defined(_XLIB_H_)
+	Display *display;
+	Window window;
+	Atom rettype;
+	unsigned long nitem;
+	unsigned long dummy;
+	int retform;
+	XEvent event;
+#endif
+
+#ifdef USE_WIN32
+	if (!IsClipboardFormatAvailable(CF_TEXT) || !OpenClipboard(NULL)) {
+		return -1;
+	}
+	handle = GetClipboardData(CF_TEXT);
+	if (!handle) {
+		CloseClipboard();
+		return -1;
+	}
+	clipboard = (unsigned char *)GlobalLock(handle);
+	if (!clipboard) {
+		CloseClipboard();
+		return -1;
+	}
+#elif defined(_XLIB_H_)
+	if (!(display = XOpenDisplay(NULL))) {
+		return -1;
+	}
+
+	// Creates a non maped temporary X window to hold the selection
+	if (!(window = XCreateSimpleWindow(display,
+			DefaultRootWindow(display), 0, 0, 1, 1, 0, 0, 0))) {
+		XCloseDisplay(display);
+		return -1;
+	}
+
+	XConvertSelection(display, XA_PRIMARY, XA_STRING, XA_STRING,
+		window, CurrentTime);
+
+	XNextEvent(display, &event);
+
+	if (event.type != SelectionNotify ||
+			event.xselection.property != XA_STRING) {
+		return -1;
+	}
+
+	XGetWindowProperty(display, window, XA_STRING, 0, 1024, False,
+		XA_STRING, &rettype, &retform, &nitem, &dummy, &clipboard);
+
+	XDestroyWindow(display, window);
+	XCloseDisplay(display);
+
+	if (rettype != XA_STRING || retform != 8) {
+		if (clipboard != NULL) {
+			XFree(clipboard);
+		}
+		clipboard = NULL;
+	}
+
+	if (clipboard == NULL) {
+		return -1;
+	}
+#endif
+	// Only allow ascii characters
+	for (i = 0; clipboard[i] != '\0'; ++i) {
+		if (clipboard[i] < 32 || clipboard[i] > 126) {
+			return -1;
+		}
+	}
+	str = (char *)clipboard;
+#ifdef USE_WIN32
+	GlobalUnlock(handle);
+	CloseClipboard();
+#elif defined(_XLIB_H_)
+	if (clipboard != NULL) {
+		XFree(clipboard);
+	}
+#endif
+	return 0;
+#else
+	return -1;
+#endif
+}
+
