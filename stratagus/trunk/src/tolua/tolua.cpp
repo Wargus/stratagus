00a9ffb8fe29f54d2648570007535ae16d0f78c7
jsalmon3 <>
2006-12-01 17:21:42 +0000

    Added CPlayer::UnitTypesCount and CUnitType::Slot

--- stratagus/trunk/src/tolua/tolua.cpp
+++ stratagus/trunk/src/tolua/tolua.cpp
@@ -1,6 +1,6 @@
 /*
 ** Lua binding: stratagus
-** Generated automatically by tolua++-1.0.7 on Sun Nov 26 20:51:08 2006.
+** Generated automatically by tolua++-1.0.7 on Fri Dec  1 09:19:21 2006.
 */
 
 #ifndef __cplusplus
@@ -13054,6 +13054,58 @@ static int tolua_get_stratagus_CPlayer_Revenue(lua_State* tolua_S)
 }
 #endif //#ifndef TOLUA_DISABLE
 
+/* get function: UnitTypesCount of class  CPlayer */
+#ifndef TOLUA_DISABLE_tolua_get_stratagus_CPlayer_UnitTypesCount
+static int tolua_get_stratagus_CPlayer_UnitTypesCount(lua_State* tolua_S)
+{
+ int tolua_index;
+  CPlayer* self;
+ lua_pushstring(tolua_S,".self");
+ lua_rawget(tolua_S,1);
+ self = (CPlayer*)  lua_touserdata(tolua_S,-1);
+#ifndef TOLUA_RELEASE
+ {
+ tolua_Error tolua_err;
+ if (!tolua_isnumber(tolua_S,2,0,&tolua_err))
+ tolua_error(tolua_S,"#vinvalid type in array indexing.",&tolua_err);
+ }
+#endif
+ tolua_index = (int)tolua_tonumber(tolua_S,2,0);
+#ifndef TOLUA_RELEASE
+ if (tolua_index<0 || tolua_index>=UnitTypeMax)
+ tolua_error(tolua_S,"array indexing out of range.",NULL);
+#endif
+ tolua_pushnumber(tolua_S,(lua_Number)self->UnitTypesCount[tolua_index]);
+ return 1;
+}
+#endif //#ifndef TOLUA_DISABLE
+
+/* set function: UnitTypesCount of class  CPlayer */
+#ifndef TOLUA_DISABLE_tolua_set_stratagus_CPlayer_UnitTypesCount
+static int tolua_set_stratagus_CPlayer_UnitTypesCount(lua_State* tolua_S)
+{
+ int tolua_index;
+  CPlayer* self;
+ lua_pushstring(tolua_S,".self");
+ lua_rawget(tolua_S,1);
+ self = (CPlayer*)  lua_touserdata(tolua_S,-1);
+#ifndef TOLUA_RELEASE
+ {
+ tolua_Error tolua_err;
+ if (!tolua_isnumber(tolua_S,2,0,&tolua_err))
+ tolua_error(tolua_S,"#vinvalid type in array indexing.",&tolua_err);
+ }
+#endif
+ tolua_index = (int)tolua_tonumber(tolua_S,2,0);
+#ifndef TOLUA_RELEASE
+ if (tolua_index<0 || tolua_index>=UnitTypeMax)
+ tolua_error(tolua_S,"array indexing out of range.",NULL);
+#endif
+  self->UnitTypesCount[tolua_index] = ((int)  tolua_tonumber(tolua_S,3,0));
+ return 0;
+}
+#endif //#ifndef TOLUA_DISABLE
+
 /* get function: Units of class  CPlayer */
 #ifndef TOLUA_DISABLE_tolua_get_stratagus_CPlayer_Units
 static int tolua_get_stratagus_CPlayer_Units(lua_State* tolua_S)
@@ -13956,6 +14008,36 @@ static int tolua_set_CUnitType_Name(lua_State* tolua_S)
  tolua_error(tolua_S,"#vinvalid type in variable assignment.",&tolua_err);
 #endif
   self->Name = ((std::string)  tolua_tocppstring(tolua_S,2,0))
+;
+ return 0;
+}
+#endif //#ifndef TOLUA_DISABLE
+
+/* get function: Slot of class  CUnitType */
+#ifndef TOLUA_DISABLE_tolua_get_CUnitType_Slot
+static int tolua_get_CUnitType_Slot(lua_State* tolua_S)
+{
+  CUnitType* self = (CUnitType*)  tolua_tousertype(tolua_S,1,0);
+#ifndef TOLUA_RELEASE
+ if (!self) tolua_error(tolua_S,"invalid 'self' in accessing variable 'Slot'",NULL);
+#endif
+ tolua_pushnumber(tolua_S,(lua_Number)self->Slot);
+ return 1;
+}
+#endif //#ifndef TOLUA_DISABLE
+
+/* set function: Slot of class  CUnitType */
+#ifndef TOLUA_DISABLE_tolua_set_CUnitType_Slot
+static int tolua_set_CUnitType_Slot(lua_State* tolua_S)
+{
+  CUnitType* self = (CUnitType*)  tolua_tousertype(tolua_S,1,0);
+#ifndef TOLUA_RELEASE
+ tolua_Error tolua_err;
+ if (!self) tolua_error(tolua_S,"invalid 'self' in accessing variable 'Slot'",NULL);
+ if (!tolua_isnumber(tolua_S,2,0,&tolua_err))
+ tolua_error(tolua_S,"#vinvalid type in variable assignment.",&tolua_err);
+#endif
+  self->Slot = ((int)  tolua_tonumber(tolua_S,2,0))
 ;
  return 0;
 }
@@ -16895,6 +16977,7 @@ TOLUA_API int tolua_stratagus_open (lua_State* tolua_S)
   tolua_array(tolua_S,"Resources",tolua_get_stratagus_CPlayer_Resources,tolua_set_stratagus_CPlayer_Resources);
   tolua_array(tolua_S,"Incomes",tolua_get_stratagus_CPlayer_Incomes,tolua_set_stratagus_CPlayer_Incomes);
   tolua_array(tolua_S,"Revenue",tolua_get_stratagus_CPlayer_Revenue,NULL);
+  tolua_array(tolua_S,"UnitTypesCount",tolua_get_stratagus_CPlayer_UnitTypesCount,tolua_set_stratagus_CPlayer_UnitTypesCount);
   tolua_array(tolua_S,"Units",tolua_get_stratagus_CPlayer_Units,tolua_set_stratagus_CPlayer_Units);
   tolua_variable(tolua_S,"TotalNumUnits",tolua_get_CPlayer_TotalNumUnits,tolua_set_CPlayer_TotalNumUnits);
   tolua_variable(tolua_S,"NumBuildings",tolua_get_CPlayer_NumBuildings,tolua_set_CPlayer_NumBuildings);
@@ -16926,6 +17009,7 @@ TOLUA_API int tolua_stratagus_open (lua_State* tolua_S)
  tolua_beginmodule(tolua_S,"CUnitType");
   tolua_variable(tolua_S,"Ident",tolua_get_CUnitType_Ident,tolua_set_CUnitType_Ident);
   tolua_variable(tolua_S,"Name",tolua_get_CUnitType_Name,tolua_set_CUnitType_Name);
+  tolua_variable(tolua_S,"Slot",tolua_get_CUnitType_Slot,tolua_set_CUnitType_Slot);
   tolua_variable(tolua_S,"MinAttackRange",tolua_get_CUnitType_MinAttackRange,tolua_set_CUnitType_MinAttackRange);
   tolua_variable(tolua_S,"ClicksToExplode",tolua_get_CUnitType_ClicksToExplode,tolua_set_CUnitType_ClicksToExplode);
   tolua_variable(tolua_S,"Supply",tolua_get_CUnitType_Supply,tolua_set_CUnitType_Supply);
