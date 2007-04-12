15298e1d08ef29eceb4f7996af38972dc51cddac
gruiick <>
2007-04-12 17:26:46 +0000

    more code; less draft.

--- materials.bos/trunk/voices/make_voices.sh
+++ materials.bos/trunk/voices/make_voices.sh
@@ -3,20 +3,30 @@
 # $Id$
 # GPL v2 or later, see COPYRIGHT.txt from Bos Wars
 #
-# generate voices for Bos Wars, this is only a rapid draft
+# generate building voices for Bos Wars
 
-# create the ~/.festivalrc
-# using 16bits, 22k parameters
-
-echo "" > ~/.festivalrc
-echo "(Parameter.set 'Audio_Method 'linux16audio)" >> ~/.festivalrc
-echo "(Parameter.set 'Audio_Required_Rate 22050)" >> ~/.festivalrc
-echo "(set! voice_default 'voice_us1_mbrola)" >> ~/.festivalrc
+# My ~/.festivalrc :
+#(Parameter.set 'Audio_Method 'linux16audio)
+#(Parameter.set 'Audio_Required_Rate 22050)
+#(set! voice_default 'voice_us1_mbrola)
 
 # TODO
 # text2wave file.txt -o output.wav
-# use a $LIST for units, generate 2 texts : 
-# - "$BUILDING construction completed" or
-# - "$UNIT training completed" and
-# - "$UNIT/$BUILDING under attack!"
+# use a $LIST for buildings, generate 2 texts : 
+# - "$BUILDING construction completed."
+# - "$BUILDING under attack!"
+
+VOICE="voice_us1_mbrola"
+BUILDING="magmapump powerplant"
+
+function makevoice
+{
+  echo "$2" | text2wave -o "$1" -f 22050 -scale 3.0 -eval "($VOICE)"
+}
+
+for i in $BUILDING;
+do
+  makevoice "$i.completed.wav" "$i construction completed."
+  makevoice "$i.underattack.wav" "$i under attack!"
+done
 
