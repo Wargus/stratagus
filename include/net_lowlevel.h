60ebb8dd16195c020adde0ca3d8afb5355601f02
jim4 <>
2007-04-14 04:14:01 +0000

    Removed $Id$

--- include/net_lowlevel.h
+++ include/net_lowlevel.h
@@ -9,7 +9,7 @@
 //
 /**@name net_lowlevel.h - The network low level header file. */
 //
-//      (c) Copyright 1998-2005 by Lutz Sammer and Jimmy Salmon
+//      (c) Copyright 1998-2007 by Lutz Sammer and Jimmy Salmon
 //
 //      This program is free software; you can redistribute it and/or modify
 //      it under the terms of the GNU General Public License as published by
@@ -24,8 +24,6 @@
 //      along with this program; if not, write to the Free Software
 //      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //      02111-1307, USA.
-//
-//      $Id$
 
 #ifndef __NET_LOWLEVEL_H
 #define __NET_LOWLEVEL_H
