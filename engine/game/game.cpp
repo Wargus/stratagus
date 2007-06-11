eff914a0dc069116f3ba22b197e04f0fe21d48b2
jim4 <>
2007-06-11 03:19:52 +0000

    s/AiType/AiName

--- engine/game/game.cpp
+++ engine/game/game.cpp
@@ -240,7 +240,7 @@ int WriteMapSetup(const char *mapsetup, CMap *map, int writeTerrain)
 			f->printf("Players[%d]:SetStartView(%d, %d)\n", i, Players[i].StartX, Players[i].StartY);
 			f->printf("Players[%d].EnergyStored = %d\n", i, Players[i].GetEnergyStored());
 			f->printf("Players[%d].MagmaStored = %d\n", i, Players[i].GetMagmaStored());
-			f->printf("Players[%d].AiType = \"%s\"\n", i, Players[i].AiName.c_str());
+			f->printf("Players[%d].AiName = \"%s\"\n", i, Players[i].AiName.c_str());
 		}
 		f->printf("\n");
 
