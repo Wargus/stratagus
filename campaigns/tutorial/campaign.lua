d9b215f5c588d988f85d5213ab3903210ad40869
feb <>
2007-06-16 19:28:21 +0000

    Fix objective text of the tutorial.

--- campaigns/tutorial/campaign.lua
+++ campaigns/tutorial/campaign.lua
@@ -48,7 +48,7 @@ campaign_steps = {
       "for our base.\n"..
       "-- End of transmission 2. Sergeant TJ --"),
   CreateMapStep("campaigns/tutorial/level03.smp", 
-      "Harvest 200 energy and 100 magma",
+      "Harvest till you have 3000 energy and 1700 magma",
       "News from the Front:\n\n"..
       "The abandoned vault has few resources left. "..
       "For the extension of the base, we need 200 "..
