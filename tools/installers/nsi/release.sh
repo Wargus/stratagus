af59fdb4286998f39b8ec7601698e9298527842b
feb <>
2007-08-12 20:22:12 +0000

    Update release.sh for the move of installers into tools. Remove unecessary files in the package.

--- tools/installers/nsi/release.sh
+++ tools/installers/nsi/release.sh
@@ -2,17 +2,19 @@
 
 echo "Exporting svn"
 svn export . boswars
-rm -rf boswars/tools/installers
+rm -rf boswars/tools
 rm -rf boswars/engine
+rm -f boswars/bos.sln
+rm -f boswars/SConstruct
 
 cp engine/Release/boswars.exe boswars
-cp installers/nsi/SDL.dll boswars
-cp installers/nsi/msvcp80.dll boswars
-cp installers/nsi/msvcr80.dll boswars
-cp installers/nsi/Microsoft.VC80.CRT.manifest boswars
+cp tools/installers/nsi/SDL.dll boswars
+cp tools/installers/nsi/msvcp80.dll boswars
+cp tools/installers/nsi/msvcr80.dll boswars
+cp tools/installers/nsi/Microsoft.VC80.CRT.manifest boswars
 
 echo "Creating installer"
-"c:/Program Files/NSIS/makensis.exe" /NOCD installers/nsi/bos.nsi
+"c:/Program Files/NSIS/makensis.exe" /NOCD tools/installers/nsi/bos.nsi
 
 echo "Cleaning up"
 rm -rf boswars
