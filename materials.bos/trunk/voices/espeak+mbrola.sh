3694fc1cd41ca8dfef138420834a379d1c4c52f8
gruiick <>
2007-10-10 18:53:31 +0000

    (really) Add new script, using espeak TTS and mbrola voices

---
+++ materials.bos/trunk/voices/espeak+mbrola.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# $Id$
+# GPL v2 or later, see COPYRIGHT.txt from Bos Wars
+# This file is part of Bos Wars, tools section
+# 
+# Create voices for boswars units, using espeak TTS with mbrola voices.
+# - need espeak > v1.26, with mbrola translators (from espeak project)
+# - assuming mbrola binary is in /usr/local/bin/ and mbrola voices are
+# in /usr/local/share/mbrola/voices/
+
+echo "THIS IS A WORK IN PROGRESS, DO NOT USE YET !"
+exit 0
+
+VOICE="us1"
+TEXT="text to say"
+UNIT="assault" # for example
+ACTION="completed" # for example
+
+# core code
+espeak -v mb-$VOICE $TEXT | mbrola -e /usr/local/share/mbrola/voices/$VOICE/$VOICE - $UNIT.$ACTION.wav
+
+
