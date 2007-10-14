7dc2b7bed82672d173cc3181a314407189cba73d
gruiick <>
2007-10-14 18:21:16 +0000

    Real script (tested). Need to be tuned for "human feeling".

--- materials.bos/trunk/voices/espeak+mbrola.sh
+++ materials.bos/trunk/voices/espeak+mbrola.sh
@@ -13,13 +13,57 @@ echo "THIS IS A WORK IN PROGRESS, DO NOT USE IT YET !"
 exit 0
 
 VOICE="us1"
-TEXT="unit assault completed" # for example
-UNIT="assault" # for example
-ACTION="completed" # for example
+#TEXT="unit assault ready" # for example
+#UNIT="assault" # for example
+#ACTION="ready" # for example
+MB_BIN="/usr/local/bin/mbrola"
+MB_VOICE_PATH="/usr/local/share/mbrola/voices"
 
 # core "code"
-espeak -v mb-$VOICE $TEXT | mbrola -e /usr/local/share/mbrola/voices/$VOICE/$VOICE - $UNIT.$ACTION.wav
+#espeak -v mb-$VOICE "$TEXT" | $MB_BIN -e $MB_VOICE_PATH/voices/$VOICE/$VOICE - $UNIT.$ACTION.wav
 
 # TODO
-# - play with amplitude, pitch and peed on espeak
+# - play with amplitude, pitch and speed on espeak
+
+# units
+LIST_UNIT="APCS artil assault bazoo bomber buggy chopper dorcoz engineer grenadier harvester jetfighter medic rockettank tank"
+LIST_ACTION="ready underattack"
+for i in $LIST_UNIT;
+do
+  for j in $LIST_ACTION;
+  do
+    UNIT=$i
+    ACTION=$j
+    TEXT="$UNIT unit $ACTION"
+    espeak -v mb-$VOICE "$TEXT" | $MB_BIN -e $MB_VOICE_PATH/voices/$VOICE/$VOICE - $UNIT.$ACTION.wav
+  done
+done
+
+# buildings
+LIST_BUILDING="aircraftfactory camp hospital missilesilo nukeplant vault vehiclefactory"
+LIST_ACTION="completed underattack"
+for i in $LIST_BUILDING;
+do
+  for j in $LIST_ACTION;
+  do
+    UNIT=$i
+    ACTION=$j
+    TEXT="$UNIT building $ACTION"
+    espeak -v mb-$VOICE "$TEXT" | $MB_BIN -e $MB_VOICE_PATH/voices/$VOICE/$VOICE - $UNIT.$ACTION.wav
+  done
+done
+
+# facilities
+LIST_FACILITY="camera cannon gunturret magmapump powerplant radar"
+LIST_ACTION="completed underattack"
+for i in $LIST_FACILITY;
+do
+  for j in $LIST_ACTION;
+  do
+    UNIT=$i
+    ACTION=$j
+    TEXT="$UNIT facility $ACTION"
+    espeak -v mb-$VOICE "$TEXT" | $MB_BIN -e $MB_VOICE_PATH/voices/$VOICE/$VOICE - $UNIT.$ACTION.wav
+  done
+done
 
