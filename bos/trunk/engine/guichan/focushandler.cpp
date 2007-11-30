df0fcf6791ed50903329b27395fc2eaf2a05c58e
feb <>
2007-11-30 19:29:02 +0000

    Fix compilation error in focushandler.cpp. Fixes https://savannah.nongnu.org/bugs/index.php?21606.

--- bos/trunk/engine/guichan/focushandler.cpp
+++ bos/trunk/engine/guichan/focushandler.cpp
@@ -8,7 +8,7 @@
  *
  * Copyright (c) 2004, 2005 darkbits                        Js_./
  * Per Larsson a.k.a finalman                          _RqZ{a<^_aa
- * Olof Naessén a.k.a jansem/yakslem                _asww7!uY`>  )\a//
+ * Olof Naessï¿½n a.k.a jansem/yakslem                _asww7!uY`>  )\a//
  *                                                 _Qhm`] _f "'c  1!5m
  * Visit: http://guichan.darkbits.org             )Qk<P ` _: :+' .'  "{[
  *                                               .)j(] .d_/ '-(  P .   S
@@ -56,6 +56,7 @@
  * For comments regarding functions please see the header file. 
  */
 
+#include <ctype.h>  // for isascii
 #include "guichan/focushandler.h"
 #include "guichan/exception.h"
 
