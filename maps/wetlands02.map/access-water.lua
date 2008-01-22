e3e05122ed6582f1117fcebd060db9df7c168103
llearch <>
2008-01-22 13:21:38 +0000

    Added wetlands02 map. Needs tuning.

---
+++ maps/wetlands02.map/access-water.lua
@@ -0,0 +1,123 @@
+
+--     ____                _       __               
+--    / __ )____  _____   | |     / /___ ___________
+--   / __  / __ \/ ___/   | | /| / / __ `/ ___/ ___/
+--  / /_/ / /_/ (__  )    | |/ |/ / /_/ / /  (__  ) 
+-- /_____/\____/____/     |__/|__/\__,_/_/  /____/  
+--                                              
+--       A futuristic real-time strategy game.
+--          This file is part of Bos Wars.
+--
+--  (c) Copyright 2005-2006 by Francois Beerten et al.
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+-- access file from wetlands02-access-water.png
+-- width:64 height:64
+
+SetTileFlags(232, {"water"})
+SetTileFlags(296, {"water"})
+SetTileFlags(297, {"water"})
+SetTileFlags(427, {"water"})
+SetTileFlags(617, {"water"})
+SetTileFlags(667, {"water"})
+SetTileFlags(668, {"water"})
+SetTileFlags(731, {"water"})
+SetTileFlags(732, {"water"})
+SetTileFlags(796, {"water"})
+SetTileFlags(933, {"water"})
+SetTileFlags(995, {"water"})
+SetTileFlags(996, {"water"})
+SetTileFlags(997, {"water"})
+SetTileFlags(1058, {"water"})
+SetTileFlags(1059, {"water"})
+SetTileFlags(1060, {"water"})
+SetTileFlags(1115, {"water"})
+SetTileFlags(1122, {"water"})
+SetTileFlags(1123, {"water"})
+SetTileFlags(1124, {"water"})
+SetTileFlags(1169, {"water"})
+SetTileFlags(1170, {"water"})
+SetTileFlags(1178, {"water"})
+SetTileFlags(1179, {"water"})
+SetTileFlags(1233, {"water"})
+SetTileFlags(1234, {"water"})
+SetTileFlags(1235, {"water"})
+SetTileFlags(1241, {"water"})
+SetTileFlags(1242, {"water"})
+SetTileFlags(1243, {"water"})
+SetTileFlags(1293, {"water"})
+SetTileFlags(1297, {"water"})
+SetTileFlags(1298, {"water"})
+SetTileFlags(1299, {"water"})
+SetTileFlags(1305, {"water"})
+SetTileFlags(1306, {"water"})
+SetTileFlags(1330, {"water"})
+SetTileFlags(1331, {"water"})
+SetTileFlags(1332, {"water"})
+SetTileFlags(1358, {"water"})
+SetTileFlags(1359, {"water"})
+SetTileFlags(1363, {"water"})
+SetTileFlags(1364, {"water"})
+SetTileFlags(1369, {"water"})
+SetTileFlags(1370, {"water"})
+SetTileFlags(1393, {"water"})
+SetTileFlags(1394, {"water"})
+SetTileFlags(1422, {"water"})
+SetTileFlags(1428, {"water"})
+SetTileFlags(1429, {"water"})
+SetTileFlags(1440, {"water"})
+SetTileFlags(1487, {"water"})
+SetTileFlags(1496, {"water"})
+SetTileFlags(1497, {"water"})
+SetTileFlags(1503, {"water"})
+SetTileFlags(1504, {"water"})
+SetTileFlags(1505, {"water"})
+SetTileFlags(1559, {"water"})
+SetTileFlags(1560, {"water"})
+SetTileFlags(1561, {"water"})
+SetTileFlags(1567, {"water"})
+SetTileFlags(1568, {"water"})
+SetTileFlags(1569, {"water"})
+SetTileFlags(1624, {"water"})
+SetTileFlags(1625, {"water"})
+SetTileFlags(1631, {"water"})
+SetTileFlags(1632, {"water"})
+SetTileFlags(1688, {"water"})
+SetTileFlags(1689, {"water"})
+SetTileFlags(1695, {"water"})
+SetTileFlags(1951, {"water"})
+SetTileFlags(1952, {"water"})
+SetTileFlags(2255, {"water"})
+SetTileFlags(2256, {"water"})
+SetTileFlags(2257, {"water"})
+SetTileFlags(2258, {"water"})
+SetTileFlags(2273, {"water"})
+SetTileFlags(2321, {"water"})
+SetTileFlags(2322, {"water"})
+SetTileFlags(2456, {"water"})
+SetTileFlags(2520, {"water"})
+SetTileFlags(2521, {"water"})
+SetTileFlags(2583, {"water"})
+SetTileFlags(2584, {"water"})
+SetTileFlags(2761, {"water"})
+SetTileFlags(2762, {"water"})
+SetTileFlags(2763, {"water"})
+SetTileFlags(2825, {"water"})
+SetTileFlags(2826, {"water"})
+SetTileFlags(2827, {"water"})
+SetTileFlags(3030, {"water"})
+SetTileFlags(3157, {"water"})
