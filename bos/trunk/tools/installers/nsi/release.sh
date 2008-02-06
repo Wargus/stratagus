51b1ef40b11f637f3a4fee7ac1b6cce6a0cd48b0
feb <>
2008-02-06 20:44:55 +0000

    Update tools/installers/nsi/release.sh to use the zip with the dlls.
    The download happens only when the local zip is out of date.

--- bos/trunk/tools/installers/nsi/release.sh
+++ bos/trunk/tools/installers/nsi/release.sh
@@ -7,10 +7,12 @@ rm -rf boswars/engine
 rm -f boswars/bos.sln
 rm -f boswars/SConstruct
 
+wget -N http://www.boswars.org/dependencies/win32/windows-runtime-libs.zip
+unzip windows-runtime-libs.zip
+mv windows-runtime-libs/*.dll boswars/
+rmdir windows-runtime-libs
+
 cp engine/Release/boswars.exe boswars
-cp tools/installers/nsi/SDL.dll boswars
-cp tools/installers/nsi/msvcp80.dll boswars
-cp tools/installers/nsi/msvcr80.dll boswars
 cp tools/installers/nsi/Microsoft.VC80.CRT.manifest boswars
 
 echo "Creating installer"
