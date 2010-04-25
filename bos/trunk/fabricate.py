266e5b8ebffd04d3d644dbdbca8c9f2d0cb5a0f9
feb <>
2010-04-25 11:51:56 +0000

    Embed the licence text in the file fabricate.py.

--- bos/trunk/fabricate.py
+++ bos/trunk/fabricate.py
@@ -10,10 +10,30 @@ Windows as well as Linux.
 Read more about how to use it and how it works on the project page:
     http://code.google.com/p/fabricate/
 
-Like memoize, fabricate is released under a "New BSD license". fabricate is
-copyright (c) 2009 Brush Technology. Full text of the license is here:
-    http://code.google.com/p/fabricate/wiki/License
-
+ Copyright (c) 2009,
+    Brush Technology.
+ 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+3. The names of the contributors may not be used to endorse or promote
+    products derived from this software without specific prior written
+    permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 """
 
 # so you can do "from fabricate import *" to simplify your build script
