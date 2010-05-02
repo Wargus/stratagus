8a73bee71b648d8fe738b8f0b2eb730ea75a4e5e
kon <>
2010-05-02 11:40:43 +0000

    Make updatepo.sh update pt-??.po and bos-pt-??.po too

--- bos/trunk/languages/updatepo.sh
+++ bos/trunk/languages/updatepo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-for i in bos-??.po; do msgmerge -U  $i bos.pot; done
-for i in ??.po; do msgmerge -U  $i engine.pot; done
+for i in bos-??.po bos-??-*.po; do msgmerge -U  $i bos.pot; done
+for i in ??.po ??-*.po; do msgmerge -U  $i engine.pot; done
