cc055436bd9898ffc4e40606500d55a286c59ede
kon <>
2010-05-16 20:24:06 +0000

    Show terrain flags in status line of map editor
    In the map editor, if the user points the mouse cursor at a patch
    in the map, the editor displays the type and position of the patch
    in the status line.  Make it also describe the flags of the map
    field at which the user is pointing.
    
    languages/genpot.sh now uses --add-comments and presumably requires
    GNU gettext.  I don't know whether it worked with others before.

--- bos/trunk/languages/genpot.sh
+++ bos/trunk/languages/genpot.sh
@@ -1,4 +1,4 @@
 cd ..
 xgettext -d bos -k_ -o languages/bos.pot `find -name "*.lua" | sort`
 cd engine
-xgettext -d engine -C -k_ -o ../languages/engine.pot `find -name "*.cpp" -or -name "*.h" | sort`
+xgettext -d engine -C -k_ --add-comments=TRANSLATORS -o ../languages/engine.pot `find -name "*.cpp" -or -name "*.h" | sort`
