892249aec0bd249a986cd2c0457e3f873aac9ad6
kon <>
2010-05-18 20:53:28 +0000

    etags.sh: Also allow later than GPLv2
    And remove the Bos Wars logo because that one was probably the most
    copyrightable thing in the whole file.

--- tools/etags.sh
+++ tools/etags.sh
@@ -1,25 +1,23 @@
 #! /bin/sh -e
-#     ____                _       __
-#    / __ )____  _____   | |     / /___ ___________
-#   / __  / __ \/ ___/   | | /| / / __ `/ ___/ ___/
-#  / /_/ / /_/ (__  )    | |/ |/ / /_/ / /  (__  )
-# /_____/\____/____/     |__/|__/\__,_/_/  /____/
 #
-#       A futuristic real-time strategy game.
-#          This file is part of Bos Wars.
+#  Script that generates a TAGS file for GNU Emacs.
 #
-#      Script that generates a TAGS file for GNU Emacs.
-#      (c) Copyright 2010 by Kalle Olavi Niemitalo
-#      (unless this is too simple to be copyrightable)
+#  (c) Copyright 2010 by Kalle Olavi Niemitalo.
+#  (unless this is too simple to be copyrightable)
 #
-#      Bos Wars is free software; you can redistribute it and/or modify
-#      it under the terms of the GNU General Public License as published
-#      by the Free Software Foundation; only version 2 of the License.
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
 #
-#      Bos Wars is distributed in the hope that it will be useful,
-#      but WITHOUT ANY WARRANTY; without even the implied warranty of
-#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#      GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 # The etags program does not recognize "class GCN_CORE_DECLSPEC DropDown"
 # and "class GCN_EXTENSION_DECLSPEC SDLGraphics" unless we help it
