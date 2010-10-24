3fb0dbc9255fbf82e37ab254d5025913ee52059b
Pali Rohár <pali.rohar@gmail.com>
2010-10-24 14:09:16 +0200

    Updated build-win32-cross.sh, use new mingw-w64

--- build-win32-cross.sh
+++ build-win32-cross.sh
@@ -2,7 +2,7 @@
 
 make distclean || exit 1
 ./autogen.sh || exit 1
-./configure --enable-win32 --host=amd64-mingw32msvc --prefix=/usr/amd64-mingw32msvc --enable-static --without-bzip2 --disable-stdio-redirect || exit 1
+./configure --enable-win32 --host=x86_64-w64-mingw32 --prefix=/usr/x86_64-w64-mingw32 --enable-static --without-bzip2 --disable-stdio-redirect || exit 1
 make || exit 1
 make strip || exit 1
 #upx -9 stratagus.exe || exit 1
@@ -10,9 +10,8 @@ makensis -DAMD64 stratagus.nsi || exit 1
 
 make distclean || exit 1
 ./autogen.sh || exit 1
-./configure --enable-win32 --host=i586-mingw32msvc --prefix=/usr/i586-mingw32msvc --enable-static --without-bzip2 --disable-stdio-redirect || exit 1
+./configure --enable-win32 --host=i686-w64-mingw32 --prefix=/usr/i686-w64-mingw32 --enable-static --without-bzip2 --disable-stdio-redirect || exit 1
 make || exit 1
 make strip || exit 1
 upx -9 stratagus.exe || exit 1
 makensis stratagus.nsi || exit 1
-
