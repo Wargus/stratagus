62ab6aad39da910aca1aeace546defb5c6169023
mvw <>
2010-11-26 10:57:52 +0000

    Move the specific AI and patch from the map "multiple-fronts-total-war" to the location for common ones. This makes this map editable in the map editor.

--- scripts/ais/attackdefense.lua
+++ scripts/ais/attackdefense.lua
@@ -7,12 +7,12 @@
 --       A futuristic real-time strategy game.
 --          This file is part of Bos Wars.
 --
---	attackdefense.lua	- Define a hard to beat AI 
+--      attackdefense.lua	- Define a hard to beat AI 
 --      that needs many resources and enough space,
 --      takes care of a good defense,
 --      and attacks with everything available.
 --
---	(c) Copyright 2009-2010 by Michiel van der Wulp
+--      (c) Copyright 2009-2010 by Michiel van der Wulp
 --
 --      This program is free software; you can redistribute it and/or modify
 --      it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@
 --      along with this program; if not, write to the Free Software
 --      Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 --
---	$Id: attackdefense.lua  $
+--      $Id: attackdefense.lua  $
 --
 
 local player
@@ -59,14 +59,14 @@ local ai_loop_funcs = {
 
   -- Attack wave
   function() 
-    print("=== Ai " .. player .. " is starting loop.");
-    print("Ai " .. player .. " is making 6 bazoos.");
+    -- print("=== Ai " .. player .. " is starting loop.");
+    -- print("Ai " .. player .. " is making 6 bazoos.");
     return AiForce(4, {"unit-bazoo", 6}) 
   end,
   function() return AiWaitForce(4) end,
   function() return AiSleep((SyncRand(100)+40)*GameSettings.Difficulty) end,
   function()
-    print("Ai " .. player .. " is attacking with force 4 (6 bazoos).");
+    -- print("Ai " .. player .. " is attacking with force 4 (6 bazoos).");
     return AiAttackWithForce(4)
   end,
 
