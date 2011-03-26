8bdb1a3f5266d80bed2dcfede1612d0c3c0ab232
Pali Rohár <pali.rohar@gmail.com>
2011-03-26 23:25:36 +0100

    Removed tolua++ files from Stratagus tree Use system tolua++ library

--- cmake/modules/FindTolua++.cmake
+++ cmake/modules/FindTolua++.cmake
@@ -1,9 +1,8 @@
 # - Try to find the tolua++
 # Once done this will define
 #
-#  TOLUA++_FOUND - system has tolua++ program
-#  TOLUA++_LIB_FOUND - system has tolua++ library
-#  TOLUA++_APP -the tolua++ program
+#  TOLUA++_FOUND - system has tolua++
+#  TOLUA++_APP - the tolua++ program
 #  TOLUA++_INLUDE_DIR - the tolua++ include directory
 #  TOLUA++_LIBRARY - the tolua++ library
 
@@ -20,20 +19,13 @@ else()
 	find_library(TOLUA++_LIBRARY NAMES tolua++ tolua++5.1)
 	find_program(TOLUA++_APP NAMES tolua++ tolua++5.1)
 
-	if(TOLUA++_INCLUDE_DIR AND TOLUA++_APP)
+	if(TOLUA++_INCLUDE_DIR AND TOLUA++_LIBRARY AND TOLUA++_APP)
 		set(TOLUA++_FOUND true)
 		message(STATUS "Found program tolua++: ${TOLUA++_APP}")
-	else()
-		set(TOLUA++_FOUND false)
-		message(FATAL_ERROR "Could not find program tolua++")
-	endif()
-
-	if(TOLUA++_INCLUDE_DIR AND TOLUA++_LIBRARY)
-		set(TOLUA++_LIB_FOUND true)
 		message(STATUS "Found library tolua++: ${TOLUA++_LIBRARY}")
 	else()
-		set(TOLUA++_LIB_FOUND false)
-		message(STATUS "Could not find library tolua++")
+		set(TOLUA++_FOUND false)
+		message(FATAL_ERROR "Could not find library or program tolua++")
 	endif()
 
 	mark_as_advanced(TOLUA++_INCLUDE_DIR AND TOLUA++_LIBRARY AND TOLUA++_APP)
