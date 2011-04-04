a4e3de3c1e2ea40a2c2e825feef4c6381cf1bda3
Pali Rohár <pali.rohar@gmail.com>
2011-04-04 22:30:41 +0200

    Better checking Maemo in debian/rules

--- debian/maemo-check.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-if pkg-config maemo-version 1>/dev/null 2>&1 || test -f "/etc/maemo_version" || osso-product-info -q OSSO_PRODUCT_INFO_RELEASE_NAME 2>/dev/null | grep -q -i maemo; then echo maemo; exit 0; else exit 1; fi
