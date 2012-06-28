294c9ab192bf3aa173be49ffc4971e0619c955a3
Joris <joris.dauphin@gmail.com>
2012-06-28 14:30:08 +0200

    Fix warning.

--- src/unit/unittype.cpp
+++ src/unit/unittype.cpp
@@ -134,9 +134,9 @@ extern int ParseAnimInt(CUnit *unit, const char *parseint);
 CUnitType::CUnitType() :
 	Slot(0), Width(0), Height(0), OffsetX(0), OffsetY(0), DrawLevel(0),
 	ShadowWidth(0), ShadowHeight(0), ShadowOffsetX(0), ShadowOffsetY(0),
-	Animations(NULL), StillFrame(0), OnEachCycle(NULL), OnEachSecond(NULL),
-	DeathExplosion(NULL), OnHit(NULL), CorpseType(NULL),
-	Construction(NULL), RepairHP(0), TileWidth(0), TileHeight(0),
+	Animations(NULL), StillFrame(0),
+	DeathExplosion(NULL), OnHit(NULL), OnEachCycle(NULL), OnEachSecond(NULL),
+	CorpseType(NULL), Construction(NULL), RepairHP(0), TileWidth(0), TileHeight(0),
 	BoxWidth(0), BoxHeight(0), NumDirections(0), MinAttackRange(0),
 	ReactRangeComputer(0), ReactRangePerson(0), Priority(0),
 	BurnPercent(0), BurnDamageRate(0), RepairRange(0),
