a92eea4fec628ca5646720f69ff3e200f6e1fd0f
cybermind <iddqd_mail@mail.ru>
2013-10-06 16:06:59 +0600

    [*] Show upgrade name in notify if available

--- src/action/action_research.cpp
+++ src/action/action_research.cpp
@@ -133,7 +133,11 @@
 	CPlayer &player = *unit.Player;
 	player.UpgradeTimers.Upgrades[upgrade.ID] += std::max(1, player.SpeedResearch / SPEEDUP_FACTOR);
 	if (player.UpgradeTimers.Upgrades[upgrade.ID] >= upgrade.Costs[TimeCost]) {
-		player.Notify(NotifyGreen, unit.tilePos, _("%s: research complete"), type.Name.c_str());
+		if (upgrade.Name.empty()) {
+			player.Notify(NotifyGreen, unit.tilePos, _("%s: research complete"), type.Name.c_str());
+		} else {
+			player.Notify(NotifyGreen, unit.tilePos, _("%s: research complete"), upgrade.Name.c_str());
+		}
 		if (&player == ThisPlayer) {
 			CSound *sound = GameSounds.ResearchComplete[player.Race].Sound;
 
