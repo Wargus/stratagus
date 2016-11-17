036e0bbb4774944509fd54df8f34f50ae1e87175
mimooh <mimoohowy@gmail.com>
2016-11-17 22:40:58 +0100

    Disable lua checksum for now. s/uint16_t/uint32_t/ doesn't fix the issue as I hoped.

--- src/stratagus/script.cpp
+++ src/stratagus/script.cpp
@@ -221,7 +221,9 @@ int LuaLoadFile(const std::string &file, const std::string &strArg)
 		// First, remove '\r' characters from the input. These are
 		// added, for example, by Windows Git, and should be ignored
 		content.erase(std::remove(content.begin(), content.end(), '\r'), content.end());
-		FileChecksums ^= fletcher32(content);
+		// FileChecksums ^= fletcher32(content);
+		// https://github.com/Wargus/stratagus/issues/196, disable for now.
+		FileChecksums ^= 0;
 	}
 	const int status = luaL_loadbuffer(Lua, content.c_str(), content.size(), file.c_str());
 
