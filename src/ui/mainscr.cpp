f51a9b9f692fea42f1c8acc7a61d9b87d5a9633a
mxdamien <mxdamien@gmx.de>
2020-02-13 22:07:21 +0100

    Increase buffersize due to possible overflow

--- src/ui/mainscr.cpp
+++ src/ui/mainscr.cpp
@@ -1149,7 +1149,7 @@ static void InfoPanel_draw_multiple_selection()
 		}
 	}
 	if (Selected.size() > UI.SelectedButtons.size()) {
-		char buf[5];
+		char buf[22];
 
 		sprintf(buf, "+%lu", (long unsigned int)(Selected.size() - UI.SelectedButtons.size()));
 		CLabel(*UI.MaxSelectedFont).Draw(UI.MaxSelectedTextX, UI.MaxSelectedTextY, buf);
