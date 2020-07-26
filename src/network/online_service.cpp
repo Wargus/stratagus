4c1227ee402dd5a332a904c3f008e5fb577305d2
Tim Felgentreff <timfelgentreff@gmail.com>
2020-07-26 14:20:46 +0200

    get into chat

--- src/network/online_service.cpp
+++ src/network/online_service.cpp
@@ -2,10 +2,12 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <ctime>
 #include <iostream>
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <tuple>
 #include <unistd.h>
 #include <vector>
 
@@ -15,6 +17,8 @@
 #include <netinet/in.h>
 #endif
 
+#include "game.h"
+#include "parameters.h"
 #include "assert.h"
 #include "network.h"
 #include "network/netsockets.h"
@@ -22,6 +26,7 @@
 #include "version.h"
 
 #include "./xsha1.h"
+
 
 class BNCSInputStream {
 public:
@@ -257,7 +262,6 @@ public:
         this->host = new CHost("127.0.0.1", 6112); // TODO: parameterize
         this->clientToken = MyRand();
         this->username = "";
-        this->password = "";
         this->info = "";
     }
 
@@ -278,9 +282,30 @@ public:
 
     void setUsername(std::string arg) { username = arg; }
 
-    std::string getPassword() { return password; }
+    uint32_t* getPassword2() {
+        // we assume that any valid password has at least 1 non-null word hash
+        for (int i = 0; i < 5; i++) {
+            if (password[i] != 0) {
+                return password2;
+            }
+        }
+        return NULL;
+    }
 
-    void setPassword(std::string arg) { password = arg; }
+    uint32_t* getPassword1() {
+        // we assume that any valid password has at least 1 non-null word hash
+        for (int i = 0; i < 5; i++) {
+            if (password[i] != 0) {
+                return password;
+            }
+        }
+        return NULL;
+    }
+
+    void setPassword(std::string pw) {
+        xsha1_calcHashBuf(pw.c_str(), pw.length(), password);
+        xsha1_calcHashDat(password, password2);
+    }
 
     CHost *getHost() { return host; }
 
@@ -319,7 +344,8 @@ private:
 
     std::string info;
     std::string username;
-    std::string password;
+    uint32_t password[5]; // xsha1 hash of password
+    uint32_t password2[5]; // xsha1 hash of password hash
 };
 
 int NetworkState::send(Context *ctx, BNCSOutputStream *buf) {
@@ -346,8 +372,289 @@ private:
     std::string message;
 };
 
-class IN_CHAT : public NetworkState {
-    virtual void doOneStep(Context *ctx);
+/* needed
+C>S 0x02 SID_STOPADV
+
+C>S 0x09 SID_GETADVLISTEX
+S>C 0x09 SID_GETADVLISTEX
+
+C>S 0x0E SID_CHATCOMMAND
+
+S>C 0x0F SID_CHATEVENT
+
+C>S 0x1C SID_STARTADVEX3
+S>C 0x1C SID_STARTADVEX3
+
+C>S 0x22 SID_NOTIFYJOIN
+
+C>S 0x3D SID_CREATEACCOUNT2
+S>C 0x3D SID_CREATEACCOUNT2
+
+C>S 0x2C SID_GAMERESULT
+
+C>S 0x2F SID_FINDLADDERUSER
+S>C 0x2F SID_FINDLADDERUSER
+
+C>S 0x26 SID_READUSERDATA
+S>C 0x26 SID_READUSERDATA
+
+C>S 0x31 SID_CHANGEPASSWORD
+S>C 0x31 SID_CHANGEPASSWORD
+
+C>S 0x5A SID_RESETPASSWORD
+
+C>S 0x65 SID_FRIENDSLIST
+S>C 0x65 SID_FRIENDSLIST
+
+*/
+
+class Game {
+public:
+    Game(uint32_t settings, uint16_t port, uint32_t host, uint32_t status, uint32_t time, std::string name, std::string pw, std::string stats) {
+        this->gameSettings = settings;
+        this->host = CHost(host, port);
+        this->gameStatus = status;
+        this->elapsedTime = time;
+        this->gameName = name;
+        this->gamePassword = pw;
+        this->gameStatstring = stats;
+    }
+
+    CHost getHost() { return host; }
+
+    
+
+    std::string getGameStatus() {
+        switch (gameStatus) {
+        case 0x0:
+            return "OK";
+        case 0x1:
+            return "No such game";
+        case 0x2:
+            return "Wrong password";
+        case 0x3:
+            return "Game full";
+        case 0x4:
+            return "Game already started";
+        case 0x5:
+            return "Spawned CD-Key not allowed";
+        case 0x6:
+            return "Too many server requests";
+        }
+        return "Unknown status";
+    }
+
+    std::string getGameType() {
+        switch (gameSettings & 0xff) {
+        case 0x02:
+            return "Melee";
+        case 0x03:
+            return "Free 4 All";
+        case 0x04:
+            return "One vs One";
+        case 0x05:
+            return "CTF";
+        case 0x06:
+            switch (gameSettings & 0xffff0000) {
+            case 0x00010000:
+                return "Greed 2500 resources";
+            case 0x00020000:
+                return "Greed 5000 resources";
+            case 0x00030000:
+                return "Greed 7500 resources";
+            case 0x00040000:
+                return "Greed 10000 resources";
+            }
+            return "Greed";
+        case 0x07:
+            switch (gameSettings & 0xffff0000) {
+            case 0x00010000:
+                return "Slaughter 15 minutes";
+            case 0x00020000:
+                return "Slaughter 30 minutes";
+            case 0x00030000:
+                return "Slaughter 45 minutes";
+            case 0x00040000:
+                return "Slaughter 60 minutes";
+            }
+            return "Slaughter";
+        case 0x08:
+            return "Sudden Death";
+        case 0x09:
+            switch (gameSettings & 0xffff0000) {
+            case 0x00000000:
+                return "Ladder (Disconnect is not loss)";
+            case 0x00010000:
+                return "Ladder (Loss on Disconnect)";
+            }
+            return "Ladder";
+        case 0x0A:
+            return "Use Map Settings";
+        case 0x0B:
+        case 0x0C:
+        case 0x0D:
+            std::string sub("");
+            switch (gameSettings & 0xffff0000) {
+            case 0x00010000:
+                sub += " (2 teams)";
+                break;
+            case 0x00020000:
+                sub += " (3 teams)";
+                break;
+            case 0x00030000:
+                sub += " (4 teams)";
+                break;                
+            }
+            switch (gameSettings & 0xff) {
+            case 0x0B:
+                return "Team Melee" + sub;
+            case 0x0C:
+                return "Team Free 4 All" + sub;
+            case 0x0D:
+                return "Team CTF" + sub;
+            }
+        case 0x0F:
+            switch (gameSettings & 0xffff0000) {
+            case 0x00010000:
+                return "Top vs Bottom (1v7)";
+            case 0x00020000:
+                return "Top vs Bottom (2v6)";
+            case 0x00030000:
+                return "Top vs Bottom (3v5)";
+            case 0x00040000:
+                return "Top vs Bottom (4v4)";
+            case 0x00050000:
+                return "Top vs Bottom (5v3)";
+            case 0x00060000:
+                return "Top vs Bottom (6v2)";
+            case 0x00070000:
+                return "Top vs Bottom (7v1)";
+            }
+            return "Top vs Bottom";
+        case 0x10:
+            return "Iron Man Ladder";
+        default:
+            return "Unknown";
+        }
+    }
+
+private:
+    uint32_t gameSettings;
+    uint32_t languageId;
+    CHost host;
+    uint32_t gameStatus;
+    uint32_t elapsedTime;
+    std::string gameName;
+    std::string gamePassword;
+    std::string gameStatstring;
+}
+
+class S2C_CHATEVENT : public NetworkState {
+public:
+    S2C_CHATEVENT() {
+        this->ticks = 0;
+    }
+
+    virtual void doOneStep(Context *ctx) {
+        ticks++;
+        if ((ticks % 5000) == 0) {
+            // C>S 0x07 PKT_KEEPALIVE
+            // ~5000 frames @ ~50fps == 100 seconds
+            BNCSOutputStream keepalive(0x07);
+            keepalive.serialize32(ticks);
+            keepalive.flush(ctx->getUDPSocket(), ctx->getHost());
+        }
+
+        if (ctx->getTCPSocket()->HasDataToRead(0)) {
+            uint8_t msg = ctx->getMsgIStream()->readMessageId();
+            if (msg == 0xff) {
+                // try again next time
+                return;
+            }
+
+            switch (msg) {
+            case 0x00: // SID_NULL
+                break;
+            case 0x0f: // CHATEVENT
+                handleChatevent(ctx);
+                break;
+            case 0x09:
+                // S>C 0x09 SID_GETADVLISTEX
+                handleGamelist(ctx);
+            default:
+                // S>C 0x68 SID_FRIENDSREMOVE
+                // S>C 0x67 SID_FRIENDSADD
+                std::cout << "Unhandled message ID: " << std::hex << msg << std::endl;
+            }
+        }
+    }
+
+private:
+    void handleGamelist(Context *ctx) {
+        uint32_t cnt = ctx->getMsgIStream()->read32();
+        std::vector<std::tuple<CHost, std::string, uint32_t, std::string, std::string, std::string>> games();
+        if (cnt == 0) {
+            ctx->setGamelist(games);
+            return;
+        }
+    }
+
+    void handleChatevent(Context *ctx) {
+        uint32_t eventId = ctx->getMsgIStream()->read32();
+        uint32_t userFlags = ctx->getMsgIStream()->read32();
+        uint32_t ping = ctx->getMsgIStream()->read32();
+        uint32_t ip = ctx->getMsgIStream()->read32();
+        uint32_t acn = ctx->getMsgIStream()->read32();
+        uint32_t reg = ctx->getMsgIStream()->read32();
+        std::string username = ctx->getMsgIStream()->readString();
+        std::string text = ctx->getMsgIStream()->readString();
+        switch (eventId) {
+        case 0x01: // sent for user that is already in channel
+            ctx->addUser(username);
+            break;
+        case 0x02: // user joined channel
+            ctx->addUser(username);
+            ctx->showInfo(username " joined");
+            break;
+        case 0x03: // user left channel
+            ctx->rmUser(username);
+            ctx->showInfo(username " left");
+        case 0x04: // recv whisper
+            ctx->showChat(username + " whispers: " + text);
+            break;
+        case 0x05: // recv chat
+            ctx->showChat(username + ": " + text);
+            break;
+        case 0x06: // recv broadcast
+            ctx->showChat("[BROADCAST]: " + text);
+            break;
+        case 0x07: // channel info
+            break;
+        case 0x09: // user flags update
+            break;
+        case 0x0a: // sent whisper
+            break;
+        case 0x0d: // channel full
+            ctx->showInfo("Channel full");
+            break;
+        case 0x0e: // channel does not exist
+            ctx->showInfo("Channel does not exist");
+            break;
+        case 0x0f: // channel is restricted
+            ctx->showInfo("Channel restricted");
+            break;
+        case 0x12: // general info text
+            ctx->showInfo("[INFO]: " + text);
+            break;
+        case 0x13: // error message
+            ctx->showError("[ERROR]: " + text);
+            break;
+        case 0x17: // emote
+            break;
+        }
+    }
+
+    uint32_t ticks;
 };
 
 class S2C_GETCHANNELLIST : public NetworkState {
@@ -363,10 +670,22 @@ class S2C_GETCHANNELLIST : public NetworkState {
                 error += std::to_string(msg);
                 ctx->setState(new DisconnectedState(error));
             }
-
+            
             std::vector<std::string> channels = ctx->getMsgIStream()->readStringlist();
+            ctx->setChannels(channels);
 
-            ctx->setState(new IN_CHAT());
+            BNCSOutputStream getadvlistex(0x09);
+            getadvlistex.serialize16(0x00); // all games
+            getadvlistex.serialize16(0x01); // no sub game type
+            getadvlistex.serialize32(0xff80); // show all games
+            getadvlistex.serialize32(0x00); // reserved field
+            getadvlistex.serialize32(0xff); // return all games
+            getadvlistex.serialize(""); // no game name
+            getadvlistex.serialize(""); // no game pw
+            getadvlistex.serialize(""); // no game statstring
+            getadvlistex.flush(ctx->getTCPSocket());
+
+            ctx->setState(new S2C_CHATEVENT());
         }
     }
 };
@@ -422,7 +741,7 @@ class C2S_ENTERCHAT : public NetworkState {
 
 class S2C_PKT_SERVERPING : public NetworkState {
 public:
-    S2C_PKT_SERVERPING(std::string message) {
+    S2C_PKT_SERVERPING() {
         this->retries = 0;
     };
 
@@ -478,7 +797,8 @@ class S2C_LOGONRESPONSE2 : public NetworkState {
 
             switch (status) {
             case 0x00:
-                // success - TODO - channel list, join chat
+                // success. we need to send SID_UDPPINGRESPONSE before entering chat
+                ctx->setState(new S2C_PKT_SERVERPING());
                 return;
             case 0x01:
             case 0x02:
@@ -488,7 +808,6 @@ class S2C_LOGONRESPONSE2 : public NetworkState {
                 return;
             case 0x06:
                 ctx->setInfo("Account closed: " + ctx->getMsgIStream()->readString());
-                ctx->setPassword("");
                 ctx->setState(new C2S_LOGONRESPONSE2());
                 return;
             default:
@@ -500,19 +819,30 @@ class S2C_LOGONRESPONSE2 : public NetworkState {
 
 void C2S_LOGONRESPONSE2::doOneStep(Context *ctx) {
     std::string user = ctx->getUsername();
-    std::string pw = ctx->getPassword();
-    if (!user.empty() && !pw.empty()) {
-        // C2S SID_LOGONRESPONSE2
+    uint32_t *pw = ctx->getPassword1(); // single-hashed for SID_LOGONRESPONSE2
+    if (!user.empty() && pw) {
         BNCSOutputStream logon(0x3a);
         logon.serialize32(ctx->clientToken);
         logon.serialize32(ctx->serverToken);
 
-        uint32_t pwhash[5];
-        xsha1_calcHashBuf(pw.c_str(), pw.length(), pwhash);
-        xsha1_calcHashDat(pwhash, pwhash);
-        // Uint8[20] password hash
+        // Battle.net password hashes are hashed twice using XSHA-1. First, the
+        // password is hashed by itself, then the following data is hashed again
+        // and sent to Battle.net:
+        // (UINT32) Client Token
+        // (UINT32) Server Token
+        // (UINT32)[5] First password hash
+        uint32_t data[7];
+        data[0] = ctx->clientToken;
+        data[1] = ctx->serverToken;
+        data[2] = pw[0];
+        data[3] = pw[1];
+        data[4] = pw[2];
+        data[5] = pw[3];
+        data[6] = pw[4];
+        uint32_t sendHash[5];
+        xsha1_calcHashDat(data, sendHash);
         for (int i = 0; i < 20; i++) {
-            logon.serialize8(reinterpret_cast<uint8_t*>(pwhash)[i]);
+            logon.serialize8(reinterpret_cast<uint8_t*>(sendHash)[i]);
         }
         logon.serialize(user.c_str());
         logon.flush(ctx->getTCPSocket());
@@ -623,7 +953,16 @@ class S2C_SID_AUTH_INFO : public NetworkState {
             check.serialize32(0);
             check.serialize32(0);
             // EXE information
-            std::string exeInfo("stratagus.exe ");
+            std::string exeInfo("");
+            if (!FullGameName.empty()) {
+                exeInfo += FullGameName;
+            } else {
+                if (!GameName.empty()) {
+                    exeInfo += GameName;
+                } else {
+                    exeInfo += Parameters::Instance.applicationName;
+                }
+            }
             exeInfo += StratagusLastModifiedDate;
             exeInfo += " ";
             exeInfo += StratagusLastModifiedTime;
@@ -655,7 +994,7 @@ class S2C_SID_PING : public NetworkState {
             }
             uint32_t pingValue = ctx->getMsgIStream()->read32();
 
-            // immediately respond
+            // immediately respond with C2S_SID_PING
             BNCSOutputStream buffer(0x25);
             buffer.serialize32(pingValue);
             send(ctx, &buffer);
@@ -665,16 +1004,31 @@ class S2C_SID_PING : public NetworkState {
     }
 };
 
-class C2S_SID_AUTH_INFO : public NetworkState {
+class ConnectState : public NetworkState {
     virtual void doOneStep(Context *ctx) {
+        // Connect
+        ctx->getTCPSocket()->Open(*ctx->getHost());
+        if (ctx->getTCPSocket()->IsValid() == false) {
+            ctx->setState(new DisconnectedState("TCP open failed"));
+            return;
+        }
+        if (ctx->getTCPSocket()->Connect(*ctx->getHost()) == false) {
+            ctx->setState(new DisconnectedState("TCP connect failed"));
+            return;
+        }
+        // Send proto byte
+        ctx->getTCPSocket()->Send("\x1", 1);
+
+        // C>S SID_AUTH_INFO
         BNCSOutputStream buffer(0x50);
         // (UINT32) Protocol ID
         buffer.serialize32(0x00);
         // (UINT32) Platform code
         buffer.serialize("IX86", 4);
-        // (UINT32) Product code
-        buffer.serialize("WAGS", 4);
-        // (UINT32) Version byte
+        // (UINT32) Product code - we'll just use W2BN and encode what we are in
+        // the version
+        buffer.serialize("W2BN", 4);
+        // (UINT32) Version byte - just use the last from W2BN
         buffer.serialize32(0x4f);
         // (UINT32) Language code
         buffer.serialize32(0x00);
@@ -705,7 +1059,22 @@ class C2S_SID_AUTH_INFO : public NetworkState {
             }
         }
         // (UINT32) Time zone bias
-        buffer.serialize32(0x00);
+        uint32_t bias = 0;
+        std::time_t systemtime = std::time(NULL);
+        struct std::tm *utc = std::gmtime(&systemtime);
+        if (utc != NULL) {
+            std::time_t utctime_since_epoch = std::mktime(utc);
+            if (utctime_since_epoch != -1) {
+                struct std::tm *localtime = std::localtime(&systemtime);
+                if (localtime != 0) {
+                    std::time_t localtime_since_epoch = std::mktime(localtime);
+                    if (localtime_since_epoch != -1) {
+                        bias = std::difftime(utctime_since_epoch, localtime_since_epoch) / 60;
+                    }
+                }
+            }
+        }
+        buffer.serialize32(bias);
         // (UINT32) MPQ locale ID
         buffer.serialize32(0x00);
         // (UINT32) User language ID
@@ -717,28 +1086,6 @@ class C2S_SID_AUTH_INFO : public NetworkState {
 
         send(ctx, &buffer);
         ctx->setState(new S2C_SID_PING());
-    }
-};
-
-class ProtoByteState : public NetworkState {
-    virtual void doOneStep(Context *ctx) {
-        ctx->getTCPSocket()->Send("\x1", 1); // use game protocol
-        ctx->setState(new C2S_SID_AUTH_INFO());
-    }
-};
-
-class ConnectState : public NetworkState {
-    virtual void doOneStep(Context *ctx) {
-        ctx->getTCPSocket()->Open(*ctx->getHost());
-        if (ctx->getTCPSocket()->IsValid() == false) {
-            ctx->setState(new DisconnectedState("TCP open failed"));
-            return;
-        }
-        if (ctx->getTCPSocket()->Connect(*ctx->getHost()) == false) {
-            ctx->setState(new DisconnectedState("TCP connect failed"));
-            return;
-        }
-        ctx->setState(new ProtoByteState());
     }
 };
 
