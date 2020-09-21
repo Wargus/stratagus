72f8ea9e7d34ac6ae3cb1f1619b90c678a846be0
Tim Felgentreff <timfelgentreff@gmail.com>
2020-09-21 09:28:52 +0200

    improve task for auto-download of related projects and dependencies

--- .vscode/scripts/download.ps1
+++ .vscode/scripts/download.ps1
@@ -1,4 +1,16 @@
-curl https://github.com/Wargus/win32-stratagus-dependencies/releases/download/master-builds/dependencies.zip -OutFile "$args[0]/../dependencies.zip"
-Expand-Archive "$args[0]/../dependencies.zip"
-move "$args[0]/../dependencies/dependencies/"* "$args[0]/../dependencies/"
-curl https://github.com/Wargus/stratagus/releases/download/2015-30-11/ffmpeg.exe -OutFile "$args[0]/../dependencies/bin/ffmpeg.exe"
+if (-not (Test-Path "$args[0]/../dependencies")) {
+    if (-not (Test-Path "$args[0]/../dependencies.zip")) {
+        Invoke-WebRequest https://github.com/Wargus/win32-stratagus-dependencies/releases/download/master-builds/dependencies.zip -OutFile "$args[0]/../dependencies.zip"
+    }
+    Expand-Archive "$args[0]/../dependencies.zip"
+    Move-Item "$args[0]/../dependencies/dependencies/"* "$args[0]/../dependencies/"
+}
+if (-not (Test-Path "$args[0]/../dependencies/bin/ffmpeg.exe")) {
+    Invoke-WebRequest https://github.com/Wargus/stratagus/releases/download/2015-30-11/ffmpeg.exe -OutFile "$args[0]/../dependencies/bin/ffmpeg.exe"
+}
+if (-not (Test-Path "$args[0]/../wargus")) {
+    git clone https://github.com/Wargus/wargus "$args[0]/../wargus"
+}
+if (-not (Test-Path "$args[0]/../war1gus")) {
+    git clone https://github.com/Wargus/wargus "$args[0]/../war1gus"
+}
