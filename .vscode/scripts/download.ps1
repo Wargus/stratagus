98a2e511a742167898763a8c1bfbe7bd302d40aa
Tim Felgentreff <timfelgentreff@gmail.com>
2020-12-02 09:34:05 +0100

    Update download.ps1

--- .vscode/scripts/download.ps1
+++ .vscode/scripts/download.ps1
@@ -1,16 +1,17 @@
-if (-not (Test-Path "$args[0]/../dependencies")) {
-    if (-not (Test-Path "$args[0]/../dependencies.zip")) {
-        Invoke-WebRequest https://github.com/Wargus/win32-stratagus-dependencies/releases/download/master-builds/dependencies.zip -OutFile "$args[0]/../dependencies.zip"
+$stratagus_folder = $args[0]
+if (-not (Test-Path "$stratagus_folder\\..\\dependencies")) {
+    if (-not (Test-Path "$stratagus_folder\\..\\dependencies.zip")) {
+        Invoke-WebRequest https://github.com/Wargus/win32-stratagus-dependencies/releases/download/master-builds/dependencies.zip -OutFile "$stratagus_folder\\..\\dependencies.zip"
     }
-    Expand-Archive "$args[0]/../dependencies.zip"
-    Move-Item "$args[0]/../dependencies/dependencies/"* "$args[0]/../dependencies/"
+    Expand-Archive "$stratagus_folder\\..\\dependencies.zip" -DestinationPath "$stratagus_folder\\..\\dependencies"
+    Move-Item "$stratagus_folder\\..\\dependencies\\dependencies\\*" "$stratagus_folder\\..\\dependencies\\"
 }
-if (-not (Test-Path "$args[0]/../dependencies/bin/ffmpeg.exe")) {
-    Invoke-WebRequest https://github.com/Wargus/stratagus/releases/download/2015-30-11/ffmpeg.exe -OutFile "$args[0]/../dependencies/bin/ffmpeg.exe"
+if (-not (Test-Path "$stratagus_folder\\..\\dependencies\\bin\\ffmpeg.exe")) {
+    Invoke-WebRequest https://github.com/Wargus/stratagus/releases/download/2015-30-11/ffmpeg.exe -OutFile "$stratagus_folder\\..\\dependencies\\bin\\ffmpeg.exe"
 }
-if (-not (Test-Path "$args[0]/../wargus")) {
-    git clone https://github.com/Wargus/wargus "$args[0]/../wargus"
+if (-not (Test-Path "$stratagus_folder\\..\\wargus")) {
+    git clone https://github.com/Wargus/wargus "$stratagus_folder\\..\\wargus"
 }
-if (-not (Test-Path "$args[0]/../war1gus")) {
-    git clone https://github.com/Wargus/wargus "$args[0]/../war1gus"
+if (-not (Test-Path "$stratagus_folder\\..\\war1gus")) {
+    git clone https://github.com/Wargus/wargus "$stratagus_folder\\..\\war1gus"
 }
