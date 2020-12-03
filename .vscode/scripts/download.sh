4a790f98e81aa34f51879fca348561659d8f90cd
Tim Felgentreff <timfelgentreff@gmail.com>
2020-12-03 11:57:28 +0100

    keep data extracted for development local

--- .vscode/scripts/download.sh
+++ .vscode/scripts/download.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
-
-# directory names should be uppercase for launch config to work
-
 stratagus_folder="$1"
-if [ ! -d "${stratagus_folder}/../Wargus" ]; then
-    if [ -d "${stratagus_folder}/../wargus" ]; then
-        pushd "${stratagus_folder}/../wargus"
-        target="$(pwd)"
-        popd
-        ln -s "${target}" "${stratagus_folder}/../Wargus"
-    else
-        git clone https://github.com/Wargus/wargus "${stratagus_folder}/../Wargus"
-    fi
+if [ ! -d "${stratagus_folder}/../wargus" ]; then
+    git clone https://github.com/Wargus/wargus "${stratagus_folder}/../wargus"
 fi
-if [ ! -d "$stratagus_folder/../War1gus" ]; then
-    if [ -d "${stratagus_folder}/../war1gus" ]; then
-        pushd "${stratagus_folder}/../war1gus"
-        target="$(pwd)"
-        popd
-        ln -s "${target}" "${stratagus_folder}/../War1gus"
-    else
-        git clone https://github.com/Wargus/wargus "${stratagus_folder}/../War1gus"
-    fi
+if [ ! -d "$stratagus_folder/../war1gus" ]; then
+    git clone https://github.com/Wargus/wargus "${stratagus_folder}/../war1gus"
 fi
