d9c6339d7428ecf5a14ce245d1ac38d1cc1c07f0
Tim Felgentreff <timfelgentreff@gmail.com>
2020-12-04 12:06:19 +0100

    let settings.json be local, just seed with an initial version

--- .vscode/scripts/download.ps1
+++ .vscode/scripts/download.ps1
@@ -9,9 +9,27 @@ if (-not (Test-Path "$stratagus_folder\\..\\dependencies")) {
 if (-not (Test-Path "$stratagus_folder\\..\\dependencies\\bin\\ffmpeg.exe")) {
     Invoke-WebRequest https://github.com/Wargus/stratagus/releases/download/2015-30-11/ffmpeg.exe -OutFile "$stratagus_folder\\..\\dependencies\\bin\\ffmpeg.exe"
 }
+
+if (-not (Test-Path "$stratagus_folder\\.vscode\\settings.json")) {
+    Copy-Item "$stratagus_folder\\.vscode\\settings.windows.json" "$stratagus_folder\\.vscode\\settings.json"
+}
+
 if (-not (Test-Path "$stratagus_folder\\..\\wargus")) {
     git clone https://github.com/Wargus/wargus "$stratagus_folder\\..\\wargus"
 }
+if (-not (Test-Path "$stratagus_folder\\..\\wargus\\.vscode\\settings.json")) {
+    Copy-Item "$stratagus_folder\\.vscode\\settings.windows.wargus.json" "$stratagus_folder\\..\\wargus\\.vscode\\settings.json"
+}
+if (-not (Test-Path "$stratagus_folder\\..\\wargus\\.vscode\\launch.json")) {
+    Copy-Item "$stratagus_folder\\.vscode\\launch.wargus.json" "$stratagus_folder\\..\\wargus\\.vscode\\launch.json"
+}
+
 if (-not (Test-Path "$stratagus_folder\\..\\war1gus")) {
     git clone https://github.com/Wargus/war1gus "$stratagus_folder\\..\\war1gus"
+}
+if (-not (Test-Path "$stratagus_folder\\..\\war1gus\\.vscode\\settings.json")) {
+    Copy-Item "$stratagus_folder\\.vscode\\settings.windows.war1gus.json" "$stratagus_folder\\..\\war1gus\\.vscode\\settings.json"
+}
+if (-not (Test-Path "$stratagus_folder\\..\\war1gus\\.vscode\\launch.json")) {
+    Copy-Item "$stratagus_folder\\.vscode\\launch.war1gus.json" "$stratagus_folder\\..\\war1gus\\.vscode\\launch.json"
 }
--- .vscode/scripts/download.sh
+++ .vscode/scripts/download.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 stratagus_folder="$1"
+
 if [ ! -d "${stratagus_folder}/../wargus" ]; then
     git clone https://github.com/Wargus/wargus "${stratagus_folder}/../wargus"
 fi
-if [ ! -d "$stratagus_folder/../war1gus" ]; then
-    git clone https://github.com/Wargus/wargus "${stratagus_folder}/../war1gus"
+if [ ! -e "${stratagus_folder}/../wargus/.vscode/settings.json" ]; then
+    cp "${stratagus_folder}/.vscode/settings.linux.wargus.json" "${stratagus_folder}/../wargus/.vscode/settings.json"
+fi
+if [ ! -e "${stratagus_folder}/../wargus/.vscode/launch.json" ]; then
+    cp "${stratagus_folder}/.vscode/launch.wargus.json" "${stratagus_folder}/../wargus/.vscode/launch.json"
+fi
+
+if [ ! -d "${stratagus_folder}/../war1gus" ]; then
+    git clone https://github.com/Wargus/war1gus "${stratagus_folder}/../war1gus"
+fi
+if [ ! -e "${stratagus_folder}/../war1gus/.vscode/settings.json" ]; then
+    cp "${stratagus_folder}/.vscode/settings.linux.war1gus.json" "${stratagus_folder}/../war1gus/.vscode/settings.json"
+fi
+if [ ! -e "${stratagus_folder}/../war1gus/.vscode/launch.json" ]; then
+    cp "${stratagus_folder}/.vscode/launch.war1gus.json" "${stratagus_folder}/../war1gus/.vscode/launch.json"
 fi
