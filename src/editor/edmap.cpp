6b3ea99e4a8730609cd310c8886ed8d2c8f1c578
Tim Felgentreff <timfelgentreff@gmail.com>
2022-02-16 10:04:42 +0100

    better random for random maps

--- src/editor/edmap.cpp
+++ src/editor/edmap.cpp
@@ -33,6 +33,10 @@
 --  Includes
 ----------------------------------------------------------------------------*/
 
+#include <chrono>
+#include <random>
+#include <cmath>
+
 #include "stratagus.h"
 #include "editor.h"
 #include "map.h"
@@ -295,6 +299,11 @@ static void TileFill(const Vec2i &pos, int tile, int size)
 	}
 }
 
+static std::mt19937 MersenneTwister(std::chrono::steady_clock::now().time_since_epoch().count());
+static int rng() {
+	return std::abs((int)MersenneTwister());
+}
+
 /**
 **  Randomize tiles and fill in map
 **
@@ -307,11 +316,11 @@ static void EditorRandomizeTile(int tile, int count, int max_size)
 	const Vec2i mpos(Map.Info.MapWidth - 1, Map.Info.MapHeight - 1);
 
 	for (int i = 0; i < count; ++i) {
-		const Vec2i rpos(rand() % ((1 + mpos.x) / 2), rand() % ((1 + mpos.y) / 2));
+		const Vec2i rpos(rng() % ((1 + mpos.x) / 2), rng() % ((1 + mpos.y) / 2));
 		const Vec2i mirror = mpos - rpos;
 		const Vec2i mirrorh(rpos.x, mirror.y);
 		const Vec2i mirrorv(mirror.x, rpos.y);
-		const int rz = rand() % max_size + 1;
+		const int rz = rng() % max_size + 1;
 
 		TileFill(rpos, tile, rz);
 		TileFill(mirrorh, tile, rz);
@@ -339,7 +348,7 @@ static void EditorRandomizeUnit(const char *unit_type, int count, int value, int
 	const Vec2i tpos(type.TileWidth, type.TileHeight);
 
 	for (int i = 0; i < count; ++i) {
-		const Vec2i rpos(rand() % (mpos.x / 2 - tpos.x + 1), rand() % (mpos.y / 2 - tpos.y + 1));
+		const Vec2i rpos(rng() % (mpos.x / 2 - tpos.x + 1), rng() % (mpos.y / 2 - tpos.y + 1));
 		const Vec2i mirror(mpos.x - rpos.x - 1, mpos.y - rpos.y - 1);
 		const Vec2i mirrorh(rpos.x, mirror.y);
 		const Vec2i mirrorv(mirror.x, rpos.y);
@@ -409,9 +418,9 @@ static void RandomizeTransition(int x, int y)
 	int baseTileIndex = tileset.tiles[tileset.findTileIndexByTile(mf.getGraphicTile())].tileinfo.BaseTerrain;
 	int mixTerrainIdx = tileset.tiles[tileset.findTileIndexByTile(mf.getGraphicTile())].tileinfo.MixTerrain;
 	if (mixTerrainIdx != 0) {
-		if (rand() % 8 == 0) {
+		if (rng() % 8 == 0) {
 			// change only in ~12% of cases
-			const int tileIdx = tileset.findTileIndex(rand() % 2 ? baseTileIndex : mixTerrainIdx, 0);
+			const int tileIdx = tileset.findTileIndex(rng() % 2 ? baseTileIndex : mixTerrainIdx, 0);
 			EditorChangeTile(Vec2i(x, y), tileIdx, Vec2i(x, y), true);
 		}
 	}
