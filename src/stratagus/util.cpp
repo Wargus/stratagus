e5ca6aa17f931e6e21dfeb0f79772fd9c613da9a
Tim Felgentreff <timfelgentreff@gmail.com>
2023-01-07 16:44:59 +0100

    clarify expected bit width of our sync rand numbers

--- src/stratagus/util.cpp
+++ src/stratagus/util.cpp
@@ -27,6 +27,7 @@
 //      02111-1307, USA.
 //
 
+#include <cstdint>
 #include <random>
 
 #include "stratagus.h"
@@ -60,7 +61,7 @@
 --  Random
 ----------------------------------------------------------------------------*/
 
-unsigned SyncRandSeed;               /// sync random seed value.
+uint32_t SyncRandSeed;               /// sync random seed value.
 uint32_t FileChecksums = 0;              /// checksums of all loaded lua files
 
 /**
