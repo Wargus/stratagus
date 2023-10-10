f5c73fe4212c4cb10a348a637847bb0082c815d7
Jarod42 <Jarod42@users.noreply.github.com>
2023-10-10 11:11:32 +0200

    clang-format network/online_service.cpp and include/online_service.h

--- src/include/online_service.h
+++ src/include/online_service.h
@@ -1,34 +1,35 @@
 #ifndef __ONLINE_SERVICE_H__
 #define __ONLINE_SERVICE_H__
 
-#include <string>
-
 #include "network/netsockets.h"
 
-class OnlineContext {
+#include <string>
+
+class OnlineContext
+{
 public:
 	virtual ~OnlineContext() = default;
 
-    // called in the sdl event loop
-    virtual bool handleUDP(const unsigned char *buffer, int len, CHost host) = 0;
+	// called in the sdl event loop
+	virtual bool handleUDP(const unsigned char *buffer, int len, CHost host) = 0;
 
-    // called in the sdl event loop
-    virtual void doOneStep() = 0;
+	// called in the sdl event loop
+	virtual void doOneStep() = 0;
 
-    // called when joining a network game
-    virtual void joinGame(std::string hostPlayerName, std::string pw) = 0;
+	// called when joining a network game
+	virtual void joinGame(std::string hostPlayerName, std::string pw) = 0;
 
-    // called when leaving a network game
-    virtual void leaveGame() = 0;
+	// called when leaving a network game
+	virtual void leaveGame() = 0;
 
-    // called when advertised game is starting (just reports the game as in-progress)
-    virtual void startAdvertising(bool isStarted = false) = 0;
+	// called when advertised game is starting (just reports the game as in-progress)
+	virtual void startAdvertising(bool isStarted = false) = 0;
 
-    // called when advertised game is left by the server
-    virtual void stopAdvertising() = 0;
+	// called when advertised game is left by the server
+	virtual void stopAdvertising() = 0;
 
-    // called when network game ends
-    virtual void reportGameResult() = 0;
+	// called when network game ends
+	virtual void reportGameResult() = 0;
 };
 
 extern OnlineContext *OnlineContextHandler;
