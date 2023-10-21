b9d4311176c233694ef329164e672937aa609a5f
Jarod42 <Jarod42@users.noreply.github.com>
2023-10-21 09:47:05 +0200

    Move private types from unit/depend.h into unit/depend.cpp

--- src/unit/depend.cpp
+++ src/unit/depend.cpp
@@ -49,6 +49,83 @@
 #include "upgrade.h"
 
 /*----------------------------------------------------------------------------
+--  Documentation
+----------------------------------------------------------------------------*/
+
+/**
+**  @struct DependRule depend.h
+**
+**  \#include "depend.h"
+**
+**  This structure is used define the requirements of upgrades or
+**  unit-types. The structure is used to define the base (the wanted)
+**  upgrade or unit-type and the requirements upgrades or unit-types.
+**  The requirements could be combination of and-rules and or-rules.
+**
+**  This structure is very complex because nearly everything has two
+**  meanings.
+**
+**  The depend-rule structure members:
+**
+**  DependRule::Next
+**
+**    Next rule in hash chain for the base upgrade/unit-type.
+**    Next and-rule for the requirements.
+**
+**  DependRule::Count
+**
+**    If DependRule::Type is DependRuleUnitType, the counter is
+**    how many units of the unit-type are required, if zero no unit
+**    of this unit-type is allowed. if DependRule::Type is
+**    DependRuleUpgrade, for a non-zero counter the upgrade must be
+**    researched, for a zero counter the upgrade must be unresearched.
+**
+**  DependRule::Type
+**
+**    Type of the rule, DependRuleUnitType for an unit-type,
+**    DependRuleUpgrade for an upgrade.
+**
+**  DependRule::Kind
+**
+**    Contains the element of rule. Depending on DependRule::Type.
+**
+**  DependRule::Kind::UnitType
+**
+**    An unit-type pointer.
+**
+**  DependRule::Kind::Upgrade
+**
+**    An upgrade pointer.
+**
+**  DependRule::Rule
+**
+**    For the base upgrade/unit-type the rules which must be meet.
+**    For the requirements alternative or-rules.
+**
+*/
+
+enum
+{
+	DependRuleUnitType, /// Kind is an unit-type
+	DependRuleUpgrade /// Kind is an upgrade
+};
+
+/// Dependency rule
+class DependRule
+{
+public:
+	DependRule *Next; /// next hash chain, or rules
+	unsigned char Count; /// how many required
+	char Type; /// an unit-type or upgrade
+	union
+	{
+		const CUnitType *UnitType; /// unit-type pointer
+		const CUpgrade *Upgrade; /// upgrade pointer
+	} Kind; /// required object
+	DependRule *Rule; /// requirements, and rule
+};
+
+/*----------------------------------------------------------------------------
 --  Variables
 ----------------------------------------------------------------------------*/
 
