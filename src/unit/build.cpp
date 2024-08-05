Wargus/stratagus

commit 936b1dc09c7360072b21544cc599756bba0fa906

Fix regression in editor to place units.

src/unit/build.cpp
@@ -416,11 +416,30 @@ bool CBuildRestrictionOnTop::Check(const CUnit *builder, const CUnitType &, cons
 	return false;
 }
 
+static bool HasAtLeastOneCoastTile(const Vec2i &pos, const CUnitType &type)
+{
+	const int width = std::min<int>(type.TileWidth, Map.Info.MapWidth - pos.x);
+	const int height = std::min<int>(type.TileHeight, Map.Info.MapHeight - pos.y);
+	unsigned int index = Map.getIndex(pos);
+
+	for (int h = 0; h != height; ++h) {
+		const CMapField *mf = Map.Field(index);
+		for (int w = 0; w != width; ++w) {
+			if (mf->CoastOnMap()) {
+				return true;
+			}
+			++mf;
+		}
+		index += Map.Info.MapWidth;
+	}
+	return false;
+}
 
 /**
 **  Can build unit here.
 **  Hall too near to goldmine.
 **  Refinery or shipyard too near to oil patch.
+**  Does NOT check terrain flag (allow to test later valid/invalid tile)
 **
 **  @param unit  Unit doing the building
 **  @param type  unit-type to be checked.
@@ -431,54 +450,24 @@ bool CBuildRestrictionOnTop::Check(const CUnit *builder, const CUnitType &, cons
 std::optional<CUnit *> CanBuildHere(const CUnit *unit, const CUnitType &type, const Vec2i &pos)
 {
 	//  Can't build outside the map
-	if (pos.x + type.TileWidth > Map.Info.MapWidth) {
-		return std::nullopt;
-	}
-	if (pos.y + type.TileHeight > Map.Info.MapHeight) {
+	if (pos.x + type.TileWidth > Map.Info.MapWidth
+	    || pos.y + type.TileHeight > Map.Info.MapHeight) {
 		return std::nullopt;
 	}
 
 	// Must be checked before oil!
 	if (type.BoolFlag[SHOREBUILDING_INDEX].value) {
-		const int width = type.TileWidth;
-		int h = type.TileHeight;
-		bool success = false;
-
-		// Need at least one coast tile
-		unsigned int index = Map.getIndex(pos);
-		do {
-			const CMapField *mf = Map.Field(index);
-			int w = width;
-			do {
-				if (mf->CoastOnMap()) {
-					success = true;
-				}
-				++mf;
-			} while (!success && --w);
-			index += Map.Info.MapWidth;
-		} while (!success && --h);
-		if (!success) {
+		if (!HasAtLeastOneCoastTile(pos, type)) {
 			return std::nullopt;
 		}
 	}
 
 	// Check special rules for AI players
 	if (unit && unit->Player->AiEnabled) {
-		bool aiChecked = true;
-		if (!type.AiBuildingRules.empty()) {
-			CUnit *ontoptarget = nullptr;
-			for (auto &rule : type.AiBuildingRules) {
-				// All checks processed, did we really have success
-				if (rule->Check(unit, type, pos, ontoptarget)) {
-					// We passed a full ruleset
-					aiChecked = true;
-					break;
-				} else {
-					aiChecked = false;
-				}
-			}
-		}
-		if (aiChecked == false) {
+		if (ranges::none_of(type.AiBuildingRules, [&](const auto &rule) {
+				CUnit *ontoptarget = nullptr;
+				return rule->Check(unit, type, pos, ontoptarget);
+			})) {
 			return std::nullopt;
 		}
 	}
@@ -520,15 +509,15 @@ bool CanBuildOn(const Vec2i &pos, int mask)
 **  @param pos   tile map position.
 **  @param real  Really build, or just placement
 **
-**  @return      OnTop, parent unit, builder on true, nullptr false.
-**
+**  @return      OnTop, parent unit, builder on true, nullopt false.
 */
-CUnit *CanBuildUnitType(const CUnit *unit, const CUnitType &type, const Vec2i &pos, int real)
+std::optional<CUnit *>
+CanBuildUnitType(const CUnit *unit, const CUnitType &type, const Vec2i &pos, int real)
 {
 	// Terrain Flags don't matter if building on top of a unit.
 	std::optional<CUnit *> ontop = CanBuildHere(unit, type, pos);
 	if (ontop == std::nullopt) {
-		return nullptr;
+		return std::nullopt;
 	}
 	if (*ontop != nullptr && *ontop != unit) {
 		return *ontop;
@@ -548,12 +537,6 @@ CUnit *CanBuildUnitType(const CUnit *unit, const CUnitType &type, const Vec2i &p
 	unsigned int index = pos.y * Map.Info.MapWidth;
 	for (int h = 0; h < type.TileHeight; ++h) {
 		for (int w = type.TileWidth; w--;) {
-			/* first part of if (!CanBuildOn(x + w, y + h, testmask)) */
-			if (!Map.Info.IsPointOnMap(pos.x + w, pos.y + h)) {
-				h = type.TileHeight;
-				*ontop = nullptr;
-				break;
-			}
 			if (player && !real) {
 				//testmask = MapFogFilterFlags(player, x + w, y + h, type.MovementMask);
 				testmask = MapFogFilterFlags(*player,
@@ -561,16 +544,16 @@ CUnit *CanBuildUnitType(const CUnit *unit, const CUnitType &type, const Vec2i &p
 			} else {
 				testmask = type.MovementMask;
 			}
-			/*secound part of if (!CanBuildOn(x + w, y + h, testmask)) */
+			/*second part of if (!CanBuildOn(x + w, y + h, testmask)) */
 			const CMapField &mf = *Map.Field(index + pos.x + w);
 			if (mf.CheckMask(testmask)) {
 				h = type.TileHeight;
-				*ontop = nullptr;
+				ontop = std::nullopt;
 				break;
 			}
 			if (player && !mf.playerInfo.IsExplored(*player)) {
 				h = type.TileHeight;
-				*ontop = nullptr;
+				ontop = std::nullopt;
 				break;
 			}
 		}
@@ -579,8 +562,7 @@ CUnit *CanBuildUnitType(const CUnit *unit, const CUnitType &type, const Vec2i &p
 	if (unit) {
 		MarkUnitFieldFlags(*unit);
 	}
-	// We can build here: check distance to gold mine/oil patch!
-	return *ontop;
+	return ontop;
 }
 
 //@}

src/action/action_build.cpp
@@ -280,7 +280,7 @@ CUnit *COrder_Build::CheckCanBuild(CUnit &unit)
 
 	// Check if the building could be built there.
 
-	CUnit *ontop = CanBuildUnitType(&unit, type, pos, 1);
+	CUnit *ontop = CanBuildUnitType(&unit, type, pos, 1).value_or(nullptr);
 
 	if (ontop != nullptr) {
 		return ontop;

src/include/unit.h
@@ -597,7 +597,8 @@ CanBuildHere(const CUnit *unit, const CUnitType &type, const Vec2i &pos);
 /// @todo more docu
 extern bool CanBuildOn(const Vec2i &pos, int mask);
 /// FIXME: more docu
-extern CUnit *CanBuildUnitType(const CUnit *unit, const CUnitType &type, const Vec2i &pos, int real);
+extern std::optional<CUnit *>
+CanBuildUnitType(const CUnit *unit, const CUnitType &type, const Vec2i &pos, int real);
 /// Get the suitable animation frame depends of unit's damaged type.
 extern int ExtraDeathIndex(std::string_view death);
 

src/video/cursor.cpp
@@ -239,8 +239,6 @@ static void DrawBuildingCursor()
 	const Vec2i mpos = vp.ScreenToTilePos(CursorScreenPos);
 	const PixelPos screenPos = vp.TilePosToScreen_TopLeft(mpos);
 
-	CUnit *ontop = nullptr;
-
 	//
 	//  Draw building
 	//
@@ -263,16 +261,17 @@ static void DrawBuildingCursor()
 	//
 	//  Draw the allow overlay
 	//
-	bool f;
+	std::optional<CUnit *> ontop = std::nullopt;
+
 	if (!Selected.empty()) {
-		f = true;
+		bool f = true;
 		for (size_t i = 0; f && i < Selected.size(); ++i) {
 			f = ((ontop = CanBuildHere(Selected[i], *CursorBuilding, mpos).value_or(nullptr)) != nullptr);
 			// Assign ontop or nullptr
 			ontop = (ontop == Selected[i] ? nullptr : ontop);
 		}
 	} else {
-		f = ((ontop = CanBuildHere(nullptr, *CursorBuilding, mpos).value_or(nullptr)) != nullptr);
+		ontop = CanBuildHere(nullptr, *CursorBuilding, mpos);
 		if (!Editor.Running) {
 			ontop = nullptr;
 		}
@@ -291,7 +290,7 @@ static void DrawBuildingCursor()
 			const Vec2i posIt(mpos.x + w, mpos.y + h);
 			Uint32 color;
 
-			if (f && (ontop ||
+			if (ontop && (*ontop ||
 					  CanBuildOn(posIt, MapFogFilterFlags(*ThisPlayer, posIt,
 														  mask & ((!Selected.empty() && Selected[0]->tilePos == posIt) ?
 																  ~(MapFieldLandUnit | MapFieldSeaUnit) : -1))))

commit 48ab3b66b6ec0a1aca3637adc81a2c4e0683940a

[fix] Fix *correctly* regression introduced by commit 936b1dc09c7360072b21544cc599756bba0fa906 (Fix regression in editor to place units.) (and not fixed by 59d30da09aaa35448fe0fe404298799f72c01802) #closes https://github.com/Wargus/wargus/issues/449 Allow AI to build building. Thanks to MamiyaOtaru for investigation and fix.

src/unit/build.cpp
@@ -464,10 +464,11 @@ std::optional<CUnit *> CanBuildHere(const CUnit *unit, const CUnitType &type, co
 
 	// Check special rules for AI players
 	if (unit && unit->Player->AiEnabled) {
-		if (!ranges::any_of(type.AiBuildingRules, [&](const auto &rule) {
-				CUnit *ontoptarget = nullptr;
-				return rule->Check(unit, type, pos, ontoptarget);
-			})) {
+		if (!type.AiBuildingRules.empty()
+		    && ranges::none_of(type.AiBuildingRules, [&](const auto &rule) {
+				   CUnit *ontoptarget = nullptr;
+				   return rule->Check(unit, type, pos, ontoptarget);
+			   })) {
 			return std::nullopt;
 		}
 	}